741552791f0ec488ee9bcc460d2bca5902dbd61e
Valentin <valio990210@gmail.com>
2022-06-07 19:16:41 +0100

    update

---
+++ letter.js
@@ -0,0 +1,20 @@
+function letters(input){
+
+let word = input [0];
+let total = 0;
+for(let i = 0; i < word.length; i++){
+    let ch = word[i];
+    switch(ch){
+        case "a": total += 1; break;
+        case "e": total += 2; break;
+        case "i": total += 3; break;
+        case "o": total += 4; break;
+        case "u": total += 5; break;
+        }
+        }
+        console.log(total);
+
+
+}
+
+letters(["bamboo"])
