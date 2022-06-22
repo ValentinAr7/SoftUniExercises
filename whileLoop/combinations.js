42e0a33aee03ba514253e4055e5e53fc7892e299
Valentin <valio990210@gmail.com>
2022-06-22 18:51:26 +0100

    final combination

--- whileLoop/combinations.js
+++ whileLoop/combinations.js
@@ -4,13 +4,17 @@ function combination (input){
         let endNumber = Number(input[1]);
         let magicNumber = Number(input[2]);
 
+        let counter = 0;
+
         isFinished = false
 
     for(let x = startNumber; x <= endNumber; x++){
         for(let y = startNumber; y <= endNumber; y++){
 
+            counter++
+
             if(x + y === magicNumber){
-                console.log(x + y);
+                console.log(`Combination ${counter}: ${x} + ${y} = ${magicNumber}`)
                 isFinished = true;
                 break;
             }
