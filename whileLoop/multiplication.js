b4aeb0c5d037a148cd73f828c8fbeaa19067988a
Valentin <valio990210@gmail.com>
2022-06-22 18:12:38 +0100

    formula

--- whileLoop/multiplication.js
+++ whileLoop/multiplication.js
@@ -1,7 +1,11 @@
 function multiplication(){
 
     for(let x = 1; x <= 10; x++){
-        
+        for (let y = 1; y <=10; y++){
+            console.log(`${x * y} =`);
+
+        }
+
     }
 }
 multiplication
