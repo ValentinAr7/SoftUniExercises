048c1bf2a6b43efcc1ca1e4665b12bbe2f3cbd8a
Valentin <valio990210@gmail.com>
2022-06-22 18:43:03 +0100

    for cicle

--- whileLoop/combinations.js
+++ whileLoop/combinations.js
@@ -4,7 +4,12 @@ function combination (input){
         let endNumber = Number(input[1]);
         let magicNumber = Number(input[2]);
 
+    for(let x = startNumber; x <= endNumber; x++){
+        for(let y = startNumber; y <= endNumber; y++){
 
+            
+        }
+    }
 
 }       
     combination(["1", "10", "5"])
