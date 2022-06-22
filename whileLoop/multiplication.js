9d574a679d7e855345a54920d8ec9a910c9c1da2
Valentin <valio990210@gmail.com>
2022-06-22 18:24:16 +0100

    boolean

--- whileLoop/multiplication.js
+++ whileLoop/multiplication.js
@@ -1,12 +1,18 @@
 function multiplication(){
-
+    let isFinished = false;
     for(let x = 1; x <= 10; x++){
-        
+
         for (let y = 1; y <=10; y++){
             console.log(`${x} * ${y} = ${x * y}`);
+            if(y === 5){
+                isFinished = true;
+                break;
+            }
 
         }
-
+        if(isFinished){
+            break
+        }
     }
 }
 multiplication
