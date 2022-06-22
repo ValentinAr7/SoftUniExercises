55482995de09c55fc9cd6f2ae1f0c11a91fe023a
Valentin <valio990210@gmail.com>
2022-06-22 18:46:59 +0100

    calculation

--- whileLoop/combinations.js
+++ whileLoop/combinations.js
@@ -4,10 +4,19 @@ function combination (input){
         let endNumber = Number(input[1]);
         let magicNumber = Number(input[2]);
 
+        isFinished = false
+
     for(let x = startNumber; x <= endNumber; x++){
         for(let y = startNumber; y <= endNumber; y++){
 
-            
+            if(x + y === magicNumber){
+                console.log(x + y);
+                isFinished = true;
+                break;
+            }
+        }
+        if(isFinished){
+            break;
         }
     }
 
