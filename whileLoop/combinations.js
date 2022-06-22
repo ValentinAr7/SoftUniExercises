c911944990255fafffb2ae7a44b97ae4f7a497a6
Valentin <valio990210@gmail.com>
2022-06-22 18:35:52 +0100

    combination variables

---
+++ whileLoop/combinations.js
@@ -0,0 +1,10 @@
+function combination (input){
+
+        let startNumber = Number(input[0]);
+        let endNumber = Number(input[1]);
+        let magicNumber = Number(input[2]);
+
+
+
+}       
+    combination(["1", "10", "5"])
