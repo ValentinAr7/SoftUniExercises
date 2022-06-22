de4cfa808041171d5ec4db3cf0da03681eb93ddd
Valentin <valio990210@gmail.com>
2022-06-22 17:46:34 +0100

    income#

---
+++ whileLoop/school.js
@@ -0,0 +1,42 @@
+function school(input){
+
+    let goodGrade = 0;
+    let badGrade = 0;
+    let sum = 0;
+
+    index = 1
+    let command = input[index];
+    index++
+
+
+    while(command !== "Enough"){
+        console.log(command);
+
+        command = input[index]
+        index++ 
+        
+        if(command[index] = "Enough"){
+            avgScore = 
+
+
+
+            "Average score: {средна оценка}"
+
+o "Number of problems: {броя на всички задачи}"
+
+o "Last problem: {името на последната задача}"
+        }
+    }
+
+
+}
+
+school (["2",
+"Income",
+"3",
+"Game Info",
+"6",
+"Best Player",
+"4"])
+
+
