b342631a7156c5bee2c73af8543fca6daa4577b0
Valentin <valio990210@gmail.com>
2022-06-23 19:35:51 +0100

    football finish

--- whileLoop/footballResult.js
+++ whileLoop/footballResult.js
@@ -19,5 +19,35 @@ function footballResult (input){
         drawCount++
     }
 
+    teamA = Number(socndGame[0]);
+    teamB = Number(socndGame[2]);
+
+    
+    if(teamA > teamB){
+        winCount++
+    } else if(teamA< teamB){
+        lostCount++
+    } else{
+        drawCount++
+    }
+
+
+    teamA = Number(thirdGame[0]);
+    teamB = Number(thirdGame[2]);
+
+    
+    if(teamA > teamB){
+        winCount++
+    } else if(teamA< teamB){
+        lostCount++
+    } else{
+        drawCount++
+    }
+
+    console.log(`Team won ${winCount} games.`);
+    console.log(`Team lost ${lostCount} games.`);
+    console.log(`Team draw ${drawCount} games.`);
 
 }
+
+footballResult(["3:1", "1:1", "2:2"])
