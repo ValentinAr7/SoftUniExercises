371da1e214a5622c44d86f71ca8baabfc56691d5
Valentin <valio990210@gmail.com>
2022-06-23 19:32:09 +0100

    if and counter

--- whileLoop/footballResult.js
+++ whileLoop/footballResult.js
@@ -4,4 +4,20 @@ function footballResult (input){
     let socndGame = input[1];
     let thirdGame = input[2];
 
+    let winCount = 0;
+    let lostCount = 0;
+    let drawCount = 0;
+
+    let teamA = Number(firstGame[0]);
+    let teamB = Number(firstGame[2]);
+
+    if(teamA > teamB){
+        winCount++
+    } else if(teamA< teamB){
+        lostCount++
+    } else{
+        drawCount++
+    }
+
+
 }
