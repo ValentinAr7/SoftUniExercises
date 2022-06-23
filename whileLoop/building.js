bb72a43a0ae7679e6bfc54695af3d4ffd0cc449b
Valentin <valio990210@gmail.com>
2022-06-23 18:58:00 +0100

    second FOR

--- whileLoop/building.js
+++ whileLoop/building.js
@@ -3,7 +3,12 @@ function building (input){
     let y = Number(input[0]);
     let x = Number(input[1]);
 
-    for(let i = y; i >0; i--)
+    for(let i = y; i >0; i--){
+
+        for(let z = 0; z < x; z++){
+            
+        }
+    }
 
 }
 
