a4a5fa7dfacff92ede7326c7853a3a4b03fe406b
Valentin <valio990210@gmail.com>
2022-06-24 19:02:32 +0100

    variables and for cicle

---
+++ whileLoop/easterCompetitions.js
@@ -0,0 +1,14 @@
+function easterCompetitions(input){
+
+    let index = 0;
+    let easterBread = Number(input[index]);
+    index++
+
+    for(let i = 0; i <easterBread; i++){
+        let name = input[index];
+        index++
+        let command = input[index];
+        index++
+        
+    }
+}
