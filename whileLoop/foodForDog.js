7a56fdce1cb749217dcd7ceea78529fc7b8cc2ae
Valentin <valio990210@gmail.com>
2022-06-24 21:59:18 +0100

    variables dog food

---
+++ whileLoop/foodForDog.js
@@ -0,0 +1,22 @@
+function foodForDog(input){
+
+    let foodKg = Number[input[0]];
+    
+    let index = 1;
+    let dogFood = input[index];
+    index++
+
+    let foodConvert = dogFood * 1000
+    
+    
+
+
+    while(inputIndex !== "Adopted"){
+        inputIndex += 
+
+        inputIndex = input[index]
+        index++
+
+    }
+
+}
