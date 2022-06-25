36de953ea44f4419adc5504e5aec5ef21e688d28
Valentin <valio990210@gmail.com>
2022-06-25 20:46:32 +0100

    variabls

---
+++ whileLoop/balls.js
@@ -0,0 +1,13 @@
+function balls(input){
+
+    let index = 0;
+    let n = Number(input[index]);
+
+    let totalPoints = 0;
+    let redBalls = 0;
+    let orangeBalls = 0;
+    let yelloBalls = 0;
+    let whiteBalls = 0;
+    let blackBalls = 0;
+    
+}
