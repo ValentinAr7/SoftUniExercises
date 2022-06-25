357990323c42e67a29e0b7d0c99630d65d5e0f6d
Valentin <valio990210@gmail.com>
2022-06-25 21:11:34 +0100

    double switch case

--- whileLoop/paintingEggs.js
+++ whileLoop/paintingEggs.js
@@ -1,9 +1,34 @@
-function eggs (input){
+function eggs(input) {
 
     let eggsSize = input[0];
-    let eggsColor = input [1];
+    let eggsColor = input[1];
     let count = Number[input[2]];
 
-    
+    switch (eggsSize) {
+
+        case "Large":
+            switch (eggsColor) {
+                case "Red": break;
+                case "Green": break;
+                case "Yellow": break;
+            }
+            break;
+
+        case "Medium":
+            switch (eggsColor) {
+                case "Red": break;
+                case "Green": break;
+                case "Yellow": break;
+            }
+            break;
+
+        case "Small":
+            switch (eggsColor) {
+                case "Red": break;
+                case "Green": break;
+                case "Yellow": break;
+            }
+            break;
+    }
 
 }
