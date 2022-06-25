b7faa1d79e22f388ef55781f1da5353deadd9ca1
Valentin <valio990210@gmail.com>
2022-06-25 21:16:41 +0100

    data in siwtch case

--- whileLoop/paintingEggs.js
+++ whileLoop/paintingEggs.js
@@ -4,31 +4,56 @@ function eggs(input) {
     let eggsColor = input[1];
     let count = Number[input[2]];
 
+    let price = 0;
+
     switch (eggsSize) {
 
         case "Large":
             switch (eggsColor) {
-                case "Red": break;
-                case "Green": break;
-                case "Yellow": break;
+                case "Red": 
+                price = count * 16;
+                break;
+                case "Green": 
+                price = count * 12;
+                break;
+                case "Yellow": 
+                price = count * 9;
+                break;
             }
             break;
 
         case "Medium":
             switch (eggsColor) {
-                case "Red": break;
-                case "Green": break;
-                case "Yellow": break;
+                case "Red": 
+                price = count * 13;
+                break;
+
+                case "Green": 
+                price = count * 9;
+                break;
+
+                case "Yellow": 
+                price = count * 7;
+                break;
             }
             break;
 
         case "Small":
             switch (eggsColor) {
-                case "Red": break;
-                case "Green": break;
-                case "Yellow": break;
+                case "Red": 
+                price = count * 9;
+                break;
+
+                case "Green": 
+                price = count * 8
+                break;
+
+                case "Yellow": 
+                price = count * 5;
+                break;
             }
             break;
     }
+
 
 }
