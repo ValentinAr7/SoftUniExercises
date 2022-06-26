aa53b93ac1f7dc1d44a359278b92cac6b4b68ff8
Valentin <valio990210@gmail.com>
2022-06-26 08:20:53 +0100

    variables

--- whileLoop/MarinControl.js
+++ whileLoop/MarinControl.js
@@ -1,25 +1,4 @@
-function marinControl (input){
-
-let minutes = Number(input[0]);
-let seconds = Number(input[1]);
-let lenght = Number(input[2]);
-let secondsFor100 = Number(input[3]);
-
-let timeInSeconds = minutes * 60 + seconds
-
-let loseTime = lenght / 120
-let totalLoseTime = loseTime * 2.5
-
-let MarinTime = (lenght / 100) * secondsFor100 - totalLoseTime
-
-if(MarinTime < timeInSeconds){
-    console.log("Marin Bangiev won an Olympic quota!");
-    console.log(`His time is ${(MarinTime).toFixed(3)}.`);
-} else {
-    console.log(`No, Marin failed! He was ${(MarinTime - timeInSeconds).toFixed(3)} second slower.`)
-}
  
-}
 
 marinControl (["2",
 
---
+++ whileLoop/miningRig.js
@@ -0,0 +1,18 @@
+function miningRing (input){
+
+    let videoCardPrice = Nuber(input[0]);
+    let prehodnik = Number(input[1]);
+    let electricityPerDay = Number(input[2]);
+    let profitForDay = Number(input[3]);
+
+    let totalPriceCards = videoCardPrice * 13
+    let totalPricePrehodnice = prehodnik * 13
+
+    console.log();
+
+}
+
+miningRing(["700",
+"15",
+"0.20",
+"2"])
