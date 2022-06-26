da0943a8e4b2ab1511fbeff73366ef879a61cd7d
Valentin <valio990210@gmail.com>
2022-06-26 10:45:41 +0100

    updates

--- whileLoop/CatFood.js
+++ whileLoop/CatFood.js
@@ -6,26 +6,30 @@ function catFood (input){
     let group2 = 0;
     let group3 = 0;
 
-    let foodGrams = 0;
+    let totalPrice = 0;
 
     for(i = 1; i < input.length; i++){
-        if(foodGrams >=100 && foodGrams < 200){
+        let foodGrams = input[i];
+        if(foodGrams >= 100 && foodGrams < 200){
             group1++
-        } else if(foodGrams < 300){
+        } else if (foodGrams < 300){
             group2++
         } else if (foodGrams < 400){
             group3++
         }
-    }
 
-    console.log(`Group 1: ${group1} cats.`);
-    console.log(`Group 2: ${group2} cats.`);
-    console.log(`Group 3: ${group3} cats.`);
-
+        totalPrice += input[i]
 }
 
+
+
+console.log(`Group 1: ${group1} cats.`);
+console.log(`Group 2: ${group2} cats.`);
+console.log(`Group 3: ${group3} cats.`);
+console.log(totalPrice);
+}
 catFood(["6",
-"102",
+"108",
 "236",
 "123",
 "399",
