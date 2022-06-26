579cde7ca2a2184a27f39d24d445541ddb63330d
Valentin <valio990210@gmail.com>
2022-06-26 11:15:56 +0100

    update

--- whileLoop/CatFood.js
+++ whileLoop/CatFood.js
@@ -6,10 +6,12 @@ function catFood (input){
     let group2 = 0;
     let group3 = 0;
 
-    let totalPrice = 0;
+        let price = 0;
+
+
 
     for(i = 1; i < input.length; i++){
-        let foodGrams = input[i];
+        let foodGrams = Number(input[i]);
         if(foodGrams >= 100 && foodGrams < 200){
             group1++
         } else if (foodGrams < 300){
@@ -17,16 +19,17 @@ function catFood (input){
         } else if (foodGrams < 400){
             group3++
         }
-
-        totalPrice += input[i]
+        price = (price + Number(input[i]))
 }
 
-
+let grams = price / 1000
+let price2 = grams * 12.45
 
 console.log(`Group 1: ${group1} cats.`);
 console.log(`Group 2: ${group2} cats.`);
 console.log(`Group 3: ${group3} cats.`);
-console.log(totalPrice);
+console.log(`Price for food per day: ${(price2).toFixed(2)} lv.`);
+
 }
 catFood(["6",
 "108",
