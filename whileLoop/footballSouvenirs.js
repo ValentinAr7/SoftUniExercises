af4674f204dcc48923daa9a633f8b545878b64ae
Valentin <valio990210@gmail.com>
2022-06-26 09:01:42 +0100

    if statemnts

--- whileLoop/footballSouvenirs.js
+++ whileLoop/footballSouvenirs.js
@@ -22,22 +22,79 @@ function footballSouvenirs(input){
             } else if (souvenir === "caps"){
                 totalPrice += 7.20
                 totalPrice++
-            } else if ("poster"){
-                
-
+            } else if (souvenir === "poster"){
+                totalPrice += 5.10
+                totalPrice++
+            } else if  (souvenir === "stickers"){
+                totalPrice += 1.25
+                totalPrice++
+            } else {
+                console.log("Invalid stock");
             }
-            
-            
             break;
 
 
-            case "Brazil": break;
-            case "Croatia": break;
-            case "Denmark": break;
-                
+            case "Brazil": 
+            if(souvenir === "flags"){
+                totalPrice += 4.20
+                totalPrice++
+            } else if (souvenir === "caps"){
+                totalPrice += 8.50
+                totalPrice++
+            } else if (souvenir === "poster"){
+                totalPrice += 5.35
+                totalPrice++
+            } else if  (souvenir === "stickers"){
+                totalPrice += 1.20
+                totalPrice++
+            } else {
+                console.log("Invalid stock");
+            }
+            break;
 
+
+            case "Croatia": 
+            if(souvenir === "flags"){
+                totalPrice += 2.75
+                totalPrice++
+            } else if (souvenir === "caps"){
+                totalPrice += 6.90
+                totalPrice++
+            } else if (souvenir === "poster"){
+                totalPrice += 4.95
+                totalPrice++
+            } else if  (souvenir === "stickers"){
+                totalPrice += 1.10
+                totalPrice++
+            } else {
+                console.log("Invalid stock");
+            }
+            break;
+            
+            
+            
+            case "Denmark": 
+            if(souvenir === "flags"){
+                totalPrice += 3.10
+                totalPrice++
+            } else if (souvenir === "caps"){
+                totalPrice += 6.50
+                totalPrice++
+            } else if (souvenir === "poster"){
+                totalPrice += 4.80
+                totalPrice++
+            } else if  (souvenir === "stickers"){
+                totalPrice += 0.90
+                totalPrice++
+            } else {
+                console.log("Invalid stock");
+            }
+            break;
+                
         }
     }
+
+    console.log(`Pepi bought ${numberSouvenier} ${souvenir} of ${team} for ${totalPrice} lv.`);
 
 }
 
