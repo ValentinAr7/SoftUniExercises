79054a784f0ad44e0145dc83c1da8795a4deacbb
Valentin <valio990210@gmail.com>
2022-07-13 20:13:08 +0100

    calc

--- udemy/liveInWeeks.js
+++ udemy/liveInWeeks.js
@@ -1,4 +1,8 @@
 function lifeInWeeks(){
 
-    var yearsRemaining = 
+    var yearsRemaining = 90 - age 
+    var days = yearsRemaining * 365
+    var weeks = yearsRemaining * 52
+    var months = yearsRemaining * 12
 }
+
