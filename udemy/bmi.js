5530de3a3cc0c12fd39758c1f02355d5b33b64b5
Valentin <valio990210@gmail.com>
2022-07-14 21:06:26 +0100

    bmi

--- udemy/bmi.js
+++ udemy/bmi.js
@@ -3,5 +3,50 @@ function bmi(){
     let weight = 65
     let hight = 1.68
 
-    bmi = hight / weight * weight
+    bmi1 = hight / (weight * weight)
+    console.log(bmi1);
 }
+
+<div id="shopify-section-1585210042882" class="shopify-section index-section featured-images-aftrbnr"><div class="">
+  
+  <div class="images-after-banner">
+    <ul>
+      
+      <li>
+      <img src="//cdn.shopify.com/s/files/1/0108/6206/7812/files/groom_journal_bd6763ba-0e24-4ff9-9c28-cccb650d2581_1024x1024.png?v=1596128803">
+      
+           
+        <div class="content"></div>
+      </li>
+      
+      
+      <li>
+      <img src="//cdn.shopify.com/s/files/1/0108/6206/7812/files/799px-VOGUE_revista_-_logo_1024x1024.png?v=1650007340">
+      
+           
+        <div class="content"></div>
+      </li>
+      
+      
+      <li>
+      <img src="//cdn.shopify.com/s/files/1/0108/6206/7812/files/gq_logo_1024x1024.png?v=1586963190">
+      
+           
+        <div class="content"></div>
+      </li>
+      
+      
+      <li>
+      <img src="//cdn.shopify.com/s/files/1/0108/6206/7812/files/Conde_Nast_logo_82eedeaf-6d2d-4a43-a0f5-2bc3e30fec64_1024x1024.png?v=1650007780">
+      
+           
+        <div class="content"></div>
+      </li>
+      
+    </ul>
+  </div>
+</div>
+
+
+
+</div>
