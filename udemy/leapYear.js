f28eb548746104d340e2cbeb783a0a92b38035e1
Valentin <valio990210@gmail.com>
2022-07-14 21:28:34 +0100

    leap year

---
+++ udemy/leapYear.js
@@ -0,0 +1,11 @@
+function leapYear(input){
+
+    year = Number(input[0])
+
+    if(year % 4 == 0 ){
+        console.log("leap year");        
+    } else if(year % 100 == )
+
+}
+
+leapYear([2012])
