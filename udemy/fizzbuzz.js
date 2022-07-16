ab683ffbc1cce3e0d2c4a223fc693f81718b8bb7
Valentin <valio990210@gmail.com>
2022-07-16 21:17:52 +0100

    output

--- udemy/fizzbuzz.js
+++ udemy/fizzbuzz.js
@@ -1,11 +1,8 @@
 function fizbuzz(){
-
-let count = 0;
-count++
+let output = []
+let count = 1;
 
 while (count => 100){
-
-
     if(count % 3 == 0 && count % 5 == 0){
         console.log("fizbuzz");
     } else if ( count % 3 == 0){
@@ -13,8 +10,7 @@ while (count => 100){
     } else if (count % 5 == 0){
         console.log("fuzz");
     }
-
     count++
 }
-
+console.log(output);
 }
