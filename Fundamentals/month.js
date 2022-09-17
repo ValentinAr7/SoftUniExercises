cb0ff2b529e8a47348b3134c721c8fa59bcfa51d
Valentin <valio990210@gmail.com>
2022-09-17 18:08:31 +0100

    months

---
+++ Fundamentals/month.js
@@ -0,0 +1,24 @@
+function month(input){
+
+    num = Number(input[0]);
+
+    if(num > 12 && num < 1){
+        switch (m) {
+            case 1:
+                console.log("January");
+                break;
+
+            case 2:
+                    console.log("February");
+                    break;
+
+            case 1:
+                    console.log("March");
+                    break;
+        
+            default:
+                break;
+        }
+
+    }
+}
