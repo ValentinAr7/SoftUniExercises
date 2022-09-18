899d6a38f8bda84bce15626c57e053190adcf419
Valentin <valio990210@gmail.com>
2022-09-18 10:36:34 +0100

    folder hanges

--- indexCheck.js
+++
@@ -1,9 +0,0 @@
-function indexCheck(input){
-
-    let index = input[0];
-
-    index.slice(1, 4)
-    console.log(index);
-}
-
-indexCheck(["program.js"])
--- whileLoop/new.js
+++
@@ -1,8 +0,0 @@
-function let (){
-
-    let a = 0
-    let b = 5
-
-    let result = a + b
-    console.log(result);
-}
