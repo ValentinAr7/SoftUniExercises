f7fdba97d239cf08e926607a685bccf37e76c99c
Valentin <valio990210@gmail.com>
2022-09-21 19:01:53 +0100

    output

--- Fundamentals/Data Types and Variables/amazingNumber.js
+++ Fundamentals/Data Types and Variables/amazingNumber.js
@@ -6,9 +6,13 @@ let sum = 0;
 for(let i = 0; i < num.length; i++){
 sum += Number(num[i])
 }
-console.log(sum);
 
-if(sum)
+
+if(sum.toString().includes("9")){
+    console.log(`${num} Amazing? True`);
+} else {
+    console.log(`${num} Amazing? False`);
+}
 }
 
-amazingNumbers(1233)
+amazingNumbers(122353)
