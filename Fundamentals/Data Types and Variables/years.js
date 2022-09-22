775b8e9d6765f1cea3372bdaee883b509e80fde6
Valentin <valio990210@gmail.com>
2022-09-22 18:23:05 +0100

    years log

--- Fundamentals/Data Types and Variables/years.js
+++ Fundamentals/Data Types and Variables/years.js
@@ -1,4 +1,12 @@
-function year(){
+function year(centuries){
 
-    
+    let years = centuries * 100
+    let days = Math.trunc(years * 365.2422)
+    let hours = days * 24
+    let minutes = hours * 60
+
+    console.log(`${centuries} centuries = $`);
+    1 centuries = 100 years = 36524 days = 876576 hours = 52594560 minutes
 }
+
+year(1)
