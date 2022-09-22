b62915d458db45b5692cfb1339358dca50eaedc4
Valentin <valio990210@gmail.com>
2022-09-22 18:24:40 +0100

    final years ex

--- Fundamentals/Data Types and Variables/years.js
+++ Fundamentals/Data Types and Variables/years.js
@@ -5,8 +5,7 @@ function year(centuries){
     let hours = days * 24
     let minutes = hours * 60
 
-    console.log(`${centuries} centuries = $`);
-    1 centuries = 100 years = 36524 days = 876576 hours = 52594560 minutes
+    console.log(`${centuries} centuries = ${years} years = ${days} days = ${hours} hours = ${minutes} minutes`);
 }
 
 year(1)
