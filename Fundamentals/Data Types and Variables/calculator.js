9d7ac28a71148c287272e71233d96ff45231df1b
Valentin <valio990210@gmail.com>
2022-09-25 09:51:35 +0100

    to fixed result

--- Fundamentals/Data Types and Variables/calculator.js
+++ Fundamentals/Data Types and Variables/calculator.js
@@ -4,9 +4,17 @@ let result = 0;
 
 if(operator === "+"){
     result = num1 + num2
-    console.log(result);
+    console.log((result).toFixed(2));
+} else if (operator === "-"){
+    result = num1 - num2
+    console.log((result).toFixed(2));
+} else if (operator === "/"){
+    result = num1 / num2
+    console.log((result).toFixed(2));
+} else if (operator === "*"){
+    result = num1 * num2
+    console.log((result).toFixed(2));
+}
 }
 
-}
-
-calculator(5, '+', 10)
+calculator(5, '*', 10)
