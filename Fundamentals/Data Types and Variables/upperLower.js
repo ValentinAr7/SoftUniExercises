6e9b3371887a3e5676402deb1fb3432193ac872f
Valentin <valio990210@gmail.com>
2022-09-25 09:44:39 +0100

    upper case check

---
+++ Fundamentals/Data Types and Variables/upperLower.js
@@ -0,0 +1,12 @@
+function upperLower (letter){
+
+let result = letter === letter.toUpperCase();
+
+    if(result){
+        console.log("upper-case");
+    } else{
+        console.log("lower-case");
+    }
+}
+
+upperLower("F")
