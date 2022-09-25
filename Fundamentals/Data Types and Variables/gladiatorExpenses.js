eac72ff23dbc26add0394f05ba1bd3792c65c68b
Valentin <valio990210@gmail.com>
2022-09-25 10:46:10 +0100

    gladiator expenses

--- Fundamentals/Data Types and Variables/gladiatorExpenses.js
+++ Fundamentals/Data Types and Variables/gladiatorExpenses.js
@@ -6,28 +6,27 @@ let swordRepair = 0;
 let shieldRepair = 0;
 let armorRepair = 0;
 
+let brokenShield = 0;
+
 let expenses = 0;
 
-for(let currentFight = 1; currentFight = lostFights; currentFight++){
-    if(currentFight % 2 == 0 && currentFight % 3 == 0){
-        expenses += shieldRepair
-    } else if(currentFight % 2 == 0){
-        expenses += helmetRepair
-    } else if(currentFight % 3 == 0){
-        expenses += swordRepair
-    }
+for(let currentFight = 1; currentFight <= lostFights; currentFight++){
     
-    if(shieldRepair == 2){
-        expenses += armorRepair
+    if(currentFight % 2 === 0){
+        expenses += helmetPrice
     }
-
-
+    if(currentFight % 3 == 0){
+        expenses += swordPrice
+    }
+    if(currentFight % 2 == 0 && currentFight % 3 == 0){
+        expenses += shieldPrice
+        brokenShield++
+        if(brokenShield % 2 == 0){
+            expenses += armorPrice
+        }
 }
-
-
-
-
-
+}
+console.log(expenses);
 }
 
 gladiatorExpenses(7, 2, 3, 4, 5)
