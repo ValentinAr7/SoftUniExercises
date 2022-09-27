f907db3e6b6bde81d41e1bb3477f082586f32a86
Valentin <valio990210@gmail.com>
2022-09-27 18:30:13 +0100

    for loop

---
+++ Fundamentals/Arrays/reverseElements.js
@@ -0,0 +1,14 @@
+function reverseElements(n, numbers){
+
+    for(let i = 0; i < numbers.length; i++){
+        console.log(numbers[i]);
+    }
+
+
+
+
+}
+
+reverseElements (3, [10, 20, 30, 40, 50])
+console.log("----------------------------");
+reverseElements (4, [-1, 20, 99, 5])
