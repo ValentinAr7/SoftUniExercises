eb9bda58c6dbccb92f685076a23da46c876c723c
Valentin <valio990210@gmail.com>
2022-09-30 21:16:25 +0100

    new array

--- Fundamentals/Arrays/reverseInPlace.js
+++ Fundamentals/Arrays/reverseInPlace.js
@@ -1,6 +1,14 @@
 function reversePlace (array){
 
-let array = []
+
+for(let i = 0; i < array.length / 2; i++){
+let tempElement = array[i];
+
+array[i] = array[array.length - 1 - i];
+input[array.length - 1 - i] = tempElement 
+}
+
+console.log(array.join(""));
 
 }
 reversePlace['a', 'b', 'c', 'd', 'e']
