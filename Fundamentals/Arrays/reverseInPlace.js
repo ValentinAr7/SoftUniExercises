8e4889214525d046e9ebb09e9f3bc96c8aa8361e
Valentin <valio990210@gmail.com>
2022-09-30 21:26:31 +0100

    reversing comment

--- Fundamentals/Arrays/reverseInPlace.js
+++ Fundamentals/Arrays/reverseInPlace.js
@@ -1,14 +1,19 @@
-function reversePlace (array){
+function reversePlace (input){
+for(let i = 0; i < input.length / 2; i++){ // increment and divide the array by 2
+let tempElement = input[i];                 
 
+input[i] = input[input.length - 1 - i]; 
+// last element - 1 - i (where "i" incremented every time)
+// 5 - 1 - 0
+// 5 - 1 - 1
+// 5 - 1 - 2 etc....
+//put last item on first items's place
+// 0 = 0(array length - 1 - i) (increment 0 and i)
 
-for(let i = 0; i < array.length / 2; i++){
-let tempElement = array[i];
+input[input.length - 1 - i] = tempElement 
+// put first item on last item's place
 
-array[i] = array[array.length - 1 - i];
-input[array.length - 1 - i] = tempElement 
 }
-
-console.log(array.join(""));
-
+console.log(input.join(''));
 }
 reversePlace['a', 'b', 'c', 'd', 'e']
