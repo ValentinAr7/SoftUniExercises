6acf9df2e8ff68cba64950fd21ae73dba2a7276f
Valentin <valio990210@gmail.com>
2022-10-01 15:25:16 +0100

    for of loop and var

---
+++ Fundamentals/Arrays/evenAndOddSubstraction.js
@@ -0,0 +1,18 @@
+function evenOddSubstraction (numbers){
+let sumEven = 0;
+let sumOdd = 0;
+
+
+for(let number of numbers){
+    let numberInt = Number(number)
+    if(numberInt % 2 ==0){
+        sumEven += numberInt
+    } else{
+        sumOdd += numberInt
+    }
+}
+
+
+}
+
+evenOddSubstraction[1,2,3,4,5,6]
