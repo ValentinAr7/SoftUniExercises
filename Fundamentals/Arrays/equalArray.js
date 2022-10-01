c10995d32b48f37b81be7a95245690a48d120cbf
Valentin <valio990210@gmail.com>
2022-10-01 15:54:34 +0100

    for loops

--- Fundamentals/Arrays/equalArray.js
+++ Fundamentals/Arrays/equalArray.js
@@ -1,6 +1,23 @@
-function equalArray (){
+function equalArray (arr1, arr2){
 
+for(let i = 0; i < arr1.length; i++){
+    arr1[i] = Number(arr1[i])    
+}
 
+for(let i = 0; i < arr2length; i++){
+    arr2[i] = Number(arr2[i])    
+}
+
+let areIdentical = true
+let arrSum = 0;
+
+if(arr1[i] !== arr2[i]){
+    console.log(`Are not identical. Found difference in ${i}`);
+    areIdentical = false;
+} else{
+    console.log("Are identical");
+
+}
 }
 
 equalArray 
