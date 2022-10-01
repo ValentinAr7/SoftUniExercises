e1b7e7199e4cb596b8130174b8ca4a7fdd72e8f9
Valentin <valio990210@gmail.com>
2022-10-01 15:36:47 +0100

    total sum

--- Fundamentals/Arrays/evenAndOddSubstraction.js
+++ Fundamentals/Arrays/evenAndOddSubstraction.js
@@ -1,36 +1,19 @@
-function evenOddSubstraction (numbers){
-let sumEven = 0;
-let sumOdd = 0;
-
-
-for(let number of numbers){
-    let numberInt = Number(number)
-    if(numberInt % 2 ==0){
-        sumEven += numberInt
-    } else{
-        sumOdd += numberInt
-    }
-}
-function evenOddSubstraction (numbers){
+function evenOddSubstraction(numbers) {
     let sumEven = 0;
     let sumOdd = 0;
-    
-    
-    for(let number of numbers){
+    let totalSum = 0
+
+    for (let number of numbers) {
         let numberInt = Number(number)
-        if(numberInt % 2 ==0){
+        if (numberInt % 2 == 0) {
             sumEven += numberInt
-        } else{
+        } else {
             sumOdd += numberInt
         }
     }
-    
-    let totalSum = sumEven - sumOdd
-    console.log(totalSum);
-    
-    }
-    
 
+    totalSum = sumEven - sumOdd
+    console.log(totalSum);
 }
 
-evenOddSubstraction[1,2,3,4,5,6]
+evenOddSubstraction[1, 2, 3, 4, 5, 6]
