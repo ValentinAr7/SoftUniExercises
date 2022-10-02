305bb0cea7e4ddc48ece97f998b6034fe6941244
Valentin <valio990210@gmail.com>
2022-10-02 16:28:26 +0100

    concatenation

--- Fundamentals/Arrays/mergeArrays.js
+++ Fundamentals/Arrays/mergeArrays.js
@@ -1,10 +1,22 @@
 function mergeArrays(arr1, arr2){
 
 let arr3 = [];
+let evenNumber = 0;
 
-for(let i = 0; i < arr1; i++){
-    console.log(arr1[i]);
+for(let i = 0; i < arr1.length; i++){
+
+    for(let y = 0; y < arr2.length; y++){
+
+        if(arr1[i].length % 2 == 0 && arr2[y].length % 2 == 0){
+            evenNumber = arr1[i] + arr2[y]
+            arr3.push(evenNumber)
+        }
+
+    }
+    
 }
+console.log(arr3);
+
 }
 mergeArrays
 (['5', '15', '23', '56', '35'],
