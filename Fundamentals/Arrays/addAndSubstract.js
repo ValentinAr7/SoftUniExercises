e473dd9a3c1b3d5dd2070a81a8007e45b8082754
Valentin <valio990210@gmail.com>
2022-10-02 14:11:16 +0100

    .new arry

---
+++ Fundamentals/Arrays/addAndSubstract.js
@@ -0,0 +1,9 @@
+function addAndSubstract(arr){
+
+for (let i = 0; i < arr.length; i++){
+    console.log();
+}
+
+}
+
+[5, 15, 23, 56, 35]
--- Fundamentals/Arrays/equalArray.js
+++ Fundamentals/Arrays/equalArray.js
@@ -4,22 +4,21 @@ for(let i = 0; i < arr1.length; i++){
     arr1[i] = Number(arr1[i])    
 }
 
-for(let i = 0; i < arr2length; i++){
+for(let i = 0; i < arr2.length; i++){
     arr2[i] = Number(arr2[i])    
 }
 
 let areIdentical = true
 let arrSum = 0;
-
+for(let i = 0; i < arr1.length; i++){
 if(arr1[i] !== arr2[i]){
     console.log(`Are not identical. Found difference in ${i}`);
     areIdentical = false;
 } else{
     console.log("Are identical");
-
 }
 }
-
+}
 equalArray 
 ['10','20','30'],
 ['10','20','30']
