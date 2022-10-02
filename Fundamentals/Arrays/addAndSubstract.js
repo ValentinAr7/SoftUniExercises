e33276401aeb553956b07df473ccaadc786f6bd2
Valentin <valio990210@gmail.com>
2022-10-02 14:36:09 +0100

    if else statement

--- Fundamentals/Arrays/addAndSubstract.js
+++ Fundamentals/Arrays/addAndSubstract.js
@@ -1,8 +1,18 @@
 function addAndSubstract(arr){
 
+    let newArr = [];
+
 for (let i = 0; i < arr.length; i++){
-    console.log(arr);
+    let num = arr[i];
+    
+    if(num % 2 == 0){
+        newArr.push(num + i);
+    } else {
+        newArr.push(num-i)
+    }
 }
+console.log(newArr);
+
 }
 
-addAndSubstract[5, 15, 23, 56, 35]
+addAndSubstract([5, 15, 23, 56, 35])
