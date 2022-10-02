5c3c38e6ccba254fbf5c9866640b58f42e1b530b
Valentin <valio990210@gmail.com>
2022-10-02 17:25:01 +0100

    ""

--- Fundamentals/Arrays/arrayRotation.js
+++ Fundamentals/Arrays/arrayRotation.js
@@ -1,6 +1,11 @@
 function arrayRotation (array, rotation){
 
+    for(let i = 0; i < rotation; i++){
 
+        let numberToMove = array.shift();
+        array.push(numberToMove);
+    }
+console.log(array.join(""));
 }
 
 arrayRotation([51, 47, 32, 61, 21], 2)
