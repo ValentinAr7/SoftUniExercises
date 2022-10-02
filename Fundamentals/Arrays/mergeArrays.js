87375ba43ab0cbb8214fade5353793ab939786c1
Valentin <valio990210@gmail.com>
2022-10-02 15:34:53 +0100

    merge arrays

---
+++ Fundamentals/Arrays/mergeArrays.js
@@ -0,0 +1,12 @@
+function mergeArrays(arr1, arr2){
+
+let arr3 = [];
+
+for(let indexArr1 = 0; indexArr1 < arr1; indexArr1++){
+    console.log(arr1[indexArr1]);
+}
+
+}
+mergeArrays
+(['5', '15', '23', '56', '35'],
+['17', '22', '87', '36', '11'])
