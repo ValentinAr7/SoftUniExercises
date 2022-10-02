601755475cdce31db01c8049bf550c625ad15e5a
Valentin <valio990210@gmail.com>
2022-10-02 15:03:30 +0100

    log

--- Fundamentals/Arrays/addAndSubstract.js
+++ Fundamentals/Arrays/addAndSubstract.js
@@ -2,7 +2,7 @@ function addAndSubstract(arr){
 
     let newArr = [];
     let inputArraySum = 0;
-    letresultArraySum = 0;
+    let resultArraySum = 0;
 
 for (let i = 0; i < arr.length; i++){
     let num = arr[i];
@@ -12,10 +12,13 @@ for (let i = 0; i < arr.length; i++){
     } else {
         newArr.push(num-i)
     }
+    inputArraySum+=num
+    resultArraySum+=newArr[i]
 }
 
 console.log(newArr);
-console.log(newArr++);
+console.log(inputArraySum);
+console.log(resultArraySum);
 
 
 
