34aa97ace3b30a4c59307a0d709cca091e44c732
Valentin <valio990210@gmail.com>
2022-10-02 15:31:33 +0100

    common eleents in new arr

--- Fundamentals/Arrays/commonElements.js
+++ Fundamentals/Arrays/commonElements.js
@@ -1,20 +1,14 @@
-function commonElements(arr1, arr2){
+function commonElements(arr1, arr2) {
 
-let matchingIndex = "";
-
-for(let i = 0; i < arr1.length; i++){
-
-
-for(let i = 0; i < arr2.length; i++){
-} 
-
-if(arr1[i] == arr2[i]){
-    matchingIndex = arr2[i]
-    console.log(matchingIndex);
+    for (let i = 0; i < arr1.length; i++) {
+        for (let r = 0; r < arr2.length; r++) {
+        
+            if (arr1[i] === arr2[r]) {
+            console.log(arr1[i]);
+            }
+    }
 }
-
-} 
 }
 commonElements
-(['Hey', 'hello', 2, 4, 'Peter', 'e'],
-['Petar', 10, 'hey', 4, 'hello', '2'])
+    (['Hey', 'hello', 2, 4, 'Peter', 'e'],
+    ['Petar', 10, 'hey', 4, 'hello', '2'])
