1c8be211af1f137308aeeef7beced1d02da9c7b7
Valentin <valio990210@gmail.com>
2022-10-02 15:20:42 +0100

    matching index

---
+++ Fundamentals/Arrays/commonElements.js
@@ -0,0 +1,20 @@
+function commonElements(arr1, arr2){
+
+let matchingIndex = "";
+
+for(let i = 0; i < arr1.length; i++){
+
+
+for(let i = 0; i < arr2.length; i++){
+} 
+
+if(arr1[i] == arr2[i]){
+    matchingIndex = arr2[i]
+    console.log(matchingIndex);
+}
+
+} 
+}
+commonElements
+(['Hey', 'hello', 2, 4, 'Peter', 'e'],
+['Petar', 10, 'hey', 4, 'hello', '2'])
