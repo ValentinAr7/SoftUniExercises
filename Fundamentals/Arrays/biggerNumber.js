aa083c98c43d0903d485309337c3a5ebd5471df8
Valentin <valio990210@gmail.com>
2022-10-03 19:45:33 +0100

    log

--- Fundamentals/Arrays/biggerNumber.js
+++ Fundamentals/Arrays/biggerNumber.js
@@ -1,16 +1,19 @@
 function bigNumber (num){
 
-    let numIsBigger = true;
+    
     let arr2 = [];
 
 for(let i = 0; i < num.length; i++){
     let currentNumber = num[i]
-    for(let y = currentNumber + 1; y < num.length; y++){
-    if(num[i] > num[y]){
+    let isBigger = true
+    for(let y = i + 1; y < num.length; y++){
+    if(currentNumber > num[y]){
+        isBigger = false;
+    }
+    }
+    if(isBigger){
         arr2.push(num[i])
     }
-    }
-
 }
 console.log(arr2);
 }
