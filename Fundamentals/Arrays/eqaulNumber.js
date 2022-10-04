962e4bea7ca168e2c1b2d99029d610e68fe4db5e
Valentin <valio990210@gmail.com>
2022-10-04 18:11:30 +0100

    push data into array

--- Fundamentals/Arrays/eqaulNumber.js
+++ Fundamentals/Arrays/eqaulNumber.js
@@ -3,9 +3,11 @@ function equalNumber (array){
     let numbersInRow = 0;
     for(let i = 0; i < array.length; i++){
         let currentNumber = array[i];
+        let nextNumber = array[i + 1] 
 
-        if(currentNumber === (array[i] + 1)){
-            numbersInRow++
+        if(currentNumber === nextNumber){
+            numbersInRow.push(currentNumber)
+            numbersInRow.push(nextNumber)
         }
     }
 console.log(numbersInRow);
