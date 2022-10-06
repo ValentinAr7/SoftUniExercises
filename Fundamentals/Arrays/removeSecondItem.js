e3abd7ed295085d6e93930b8d8674a81fa594f0b
Valentin <valio990210@gmail.com>
2022-10-06 18:19:56 +0100

    new array

--- Fundamentals/Arrays/removeSecondItem.js
+++ Fundamentals/Arrays/removeSecondItem.js
@@ -1,15 +1,15 @@
 function removeSecondItem (arr){
 
-
+    let newArr = [];
     
     for(let i = 0; i < arr.length; i++){
         let currentItem = arr[i];
 
         if(currentItem % 2 !== 0){
-            console.log(currentItem);
+            newArr.push(currentItem)
         }
     }
-    console.log(arr);
+    console.log(newArr);
 }
 
 removeSecondItem([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
