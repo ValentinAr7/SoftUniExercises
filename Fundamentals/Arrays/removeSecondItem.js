22a7a62e340eeb6dc409f01de83795c7069172f2
Valentin <valio990210@gmail.com>
2022-10-06 18:15:13 +0100

    if statement

--- Fundamentals/Arrays/removeSecondItem.js
+++ Fundamentals/Arrays/removeSecondItem.js
@@ -3,7 +3,10 @@ function removeSecondItem (arr){
     for(let i = 0; i < arr.length; i++){
         let currentItem = arr[i];
 
-
+        if(currentItem % 2 == 0){
+            arr.splice(currentItem, )
+        }
+    }
     
 }
 
