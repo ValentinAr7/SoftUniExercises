c1fb2f1eb3bbe88d12a669647d7dac7727df842b
Valentin <valio990210@gmail.com>
2022-10-06 19:09:57 +0100

    team score

--- Fundamentals/Arrays/competition.js
+++ Fundamentals/Arrays/competition.js
@@ -1,13 +1,20 @@
 function points(games) {
 
+    let totalPoint = 0;
+
     for(let i = 0; i < games.length; i++){
         let currentScore = games[i];
         let homeTeamScore = currentScore[0]
         let awayTeamScore = currentScore[2]
         
-        
+        if(homeTeamScore > awayTeamScore){
+            totalPoint = totalPoint + 3
+        } else if(homeTeamScore === awayTeamScore){
+            totalPoint = totalPoint + 1
+        } 
     }
+
+    console.log(totalPoint);
     
   }
-
-  points(['1:1','2:2','3:3','4:4','2:2','3:3','4:4','3:3','4:4','4:4'])
+  points(['1:0','2:0','3:0','4:4','2:2','3:3','1:4','2:3','2:4','3:4'])
