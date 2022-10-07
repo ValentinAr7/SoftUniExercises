45bd6b28b4f6a5051516235910f6b73c60c1d097
Valentin <valio990210@gmail.com>
2022-10-07 17:30:21 +0100

    log

--- Fundamentals/Arrays/sortingArrays.js
+++ Fundamentals/Arrays/sortingArrays.js
@@ -2,13 +2,16 @@ function mergeArrays(arr1, arr2) {
     let arr3 = [];
     for(let i = 0; i < arr1.length; i++){
         let currentNumber1 = arr1[i]
+
         for(let r=0; r < arr2.length; i++){
             let currentNumber = arr2[r]
         }
+        console.log(arr1);
     }
     
     console.log(currentNumber);
 
 }
 
-mergeArrays([1, 2, 3, 4, 5], [6, 7, 8, 9, 10] )
+mergeArrays([1, 2, 3, 4, 5],
+            [6, 7, 8, 9, 10] )
