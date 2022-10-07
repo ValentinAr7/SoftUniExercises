8bafa8ac726de9c99395fa4f05e329cbd7ea4082
Valentin <valio990210@gmail.com>
2022-10-07 17:50:02 +0100

    array push data

--- Fundamentals/Arrays/sortingArrays.js
+++ Fundamentals/Arrays/sortingArrays.js
@@ -1,16 +1,17 @@
 function mergeArrays(arr1, arr2) {
     let arr3 = [];
+
     for(let i = 0; i < arr1.length; i++){
         let currentNumber1 = arr1[i]
+        arr3.push(currentNumber1)
+        
+
 
         for(let r=0; r < arr2.length; i++){
-            let currentNumber = arr2[r]
         }
-        console.log(arr1);
     }
-    
-    console.log(currentNumber);
 
+    console.log(arr3);
 }
 
 mergeArrays([1, 2, 3, 4, 5],
