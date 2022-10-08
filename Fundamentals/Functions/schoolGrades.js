8c417c8bfeb0e61c7c9fcb9feb41e8504c61ee9d
Valentin <valio990210@gmail.com>
2022-10-08 18:00:05 +0100

    grade log

--- Fundamentals/Functions/schoolGrades.js
+++ Fundamentals/Functions/schoolGrades.js
@@ -2,6 +2,11 @@ function schoolGrade(){
 
 let grade = Number(input[0]);
 
+if(grade < 3.00){
+    console.log("Fail");
+} else if(grade >= 3.00 &&  grade < 3.50){
+    console.log("Poor");
+} else if(grade >= 3.50 &&  grade < 4.50)
 
 
 }
