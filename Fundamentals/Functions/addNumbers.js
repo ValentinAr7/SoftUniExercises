42f78b4351bcefca195bc2bf2623f9721e07472a
Valentin <valio990210@gmail.com>
2022-10-09 14:10:58 +0100

    comments

--- Fundamentals/Functions/addNumbers.js
+++ Fundamentals/Functions/addNumbers.js
@@ -1,10 +1,13 @@
 function addNumbers(num1, num2, num3) {
 
-    function add(num1, num2){
-        let sumOfNumbers = num1 + num2
-        return sumOfNumbers;
+    function add(num1, num2){                       //function takes the two numbers from the general function
+        let sumOfNumbers = num1 + num2              // variable to show the sign(+ - / *) and to
+        return sumOfNumbers;                        // return the value as a number
     }
-    let sum = add(num1, num2)
+    let sum = add(num1, num2)                       // store the value 
+
+
+
 
     function add2(sum, num3){
         let lastSum = sum - num3
