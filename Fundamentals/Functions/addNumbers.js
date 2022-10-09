5695c7efd028a0164391ca42f27a2076bad297fd
Valentin <valio990210@gmail.com>
2022-10-09 14:07:05 +0100

    first function adding

---
+++ Fundamentals/Functions/addNumbers.js
@@ -0,0 +1,15 @@
+function addNumbers(num1, num2, num3) {
+
+    function add(num1, num2){
+        let sumOfNumbers = num1 + num2
+        return sumOfNumbers;
+    }
+    let sum = add(num1, num2)
+
+
+
+
+
+}
+
+addNumbers(23, 6, 10)
