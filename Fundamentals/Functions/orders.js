f283b9481a130ea8bb876140758da4e1eb4ac668
Valentin <valio990210@gmail.com>
2022-10-09 09:58:44 +0100

    variables

---
+++ Fundamentals/Functions/orders.js
@@ -0,0 +1,21 @@
+function oreders(drink, quantity){
+
+    let coffee = 1.50
+    let water = 1.00
+    let coke = 1.40
+    let snacks = 2.00
+
+    let totalPrice = 0;
+
+    switch (totalPrice) {
+        case drink = coffee: totalPrice = drink * quantity 
+            break;
+    
+        default:
+            break;
+    }
+console.log(totalPrice);
+
+}
+
+oreders("coffee", 5)
