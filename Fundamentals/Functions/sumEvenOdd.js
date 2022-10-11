d067383c245b465ec12a5f6102f5c5306b0265ea
Valentin <valio990210@gmail.com>
2022-10-11 18:52:32 +0100

    return data

--- Fundamentals/Functions/sumEvenOdd.js
+++ Fundamentals/Functions/sumEvenOdd.js
@@ -1,16 +1,18 @@
 function sumEvenOdd(num){
 
     let evenSum = 0;
-    
+    let oddSum = 0;
+
 
     for(let i = 0; i < num.length; i++){
     let currentNumber = num[i]
     console.log(currentNumber);
 
     if(currentNumber % 2 === 0){
-
+        evenSum += currentNumber
     }
     }
+    console.log(evenSum);
 }
 
 sumEvenOdd("1000436")
