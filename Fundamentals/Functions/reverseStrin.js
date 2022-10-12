d92d3f4ea0671d8d29bf5a474b4660e676550ee3
Valentin <valio990210@gmail.com>
2022-10-12 18:14:42 +0100

    reverse function

--- Fundamentals/Functions/reverseStrin.js
+++ Fundamentals/Functions/reverseStrin.js
@@ -1,5 +1,10 @@
 function reverseString(array){
 
+    for(let i =0; i < array.length; i++){
+
+        let arrayAsString = array[i].asString();
+        let reversedString = arrayAsString.split().reverse().join("")
+    }
 
 }
-reverseString
+reverseString ([123,323,421,121])
