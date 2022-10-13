9bf9e607d92e17940b144c50f821a11f4c155ce2
Valentin <valio990210@gmail.com>
2022-10-13 17:47:06 +0100

    password length check

--- Fundamentals/Functions/loginCheck.js
+++ Fundamentals/Functions/loginCheck.js
@@ -1,14 +1,16 @@
 function loginCheck(password){
     let passwordLength = password.length
+   
     for(let i = 0; i < passwordLength; i++){
         
-        if(password.length < 6 ){
-            console.log("Password must be between 6 and 10 characters");
-        } else{
-            console.log("Succesfull login");
-        }
+    }
+
+    if(password.length < 6 ){
+        console.log("Password must be between 6 and 10 characters");
+    } else{
+        console.log("Succesfull login");
     }
 
 
 }
-loginCheck(['loiNt5g'])
+loginCheck('lo7777g')
