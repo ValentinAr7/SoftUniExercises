665aa6e3383213efb2fdd1acca65e44665acfa5b
Valentin <valio990210@gmail.com>
2022-10-15 16:05:19 +0100

    unshift and push

--- Fundamentals/Advanced Arrays/negativePositive.js
+++ Fundamentals/Advanced Arrays/negativePositive.js
@@ -3,9 +3,17 @@ function negativePositive (numbers){
     let orderedNumbers = [];
 
 for(let i = 0; i < numbers.length; i++){
-    let currentNumber = numbers[i]
-    
+    let currentNumber = Number(numbers[i])
+
+    if(currentNumber < 0){
+        orderedNumbers.unshift(currentNumber)
+    }    else {
+        orderedNumbers.push(currentNumber)
+    }
 }
 
+for (const num of orderedNumbers) {
+    console.log(num);
+}
 }
 negativePositive(['7', '-2', '8', '9'])
