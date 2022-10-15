6c4b51b878fd99a9fe636bb775e764f377f8ce55
Valentin <valio990210@gmail.com>
2022-10-15 18:13:39 +0100

    current Number

---
+++ Fundamentals/Advanced Arrays/processOddNumbers.js
@@ -0,0 +1,12 @@
+function processOdd (array1){
+
+    for(let i = 0; i < array1.length; i++){
+        let currentNumber = array1[i]
+        if(currentNumber % 2 !== 0){
+            
+        }
+    }
+
+}
+
+processOdd([10, 15, 20, 25])
