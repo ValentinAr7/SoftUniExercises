b0c15a77069eda1bc9efe56986280db7f4a634af
Valentin <valio990210@gmail.com>
2022-10-15 15:51:22 +0100

    for loop

---
+++ Fundamentals/Advanced Arrays/negativePositive.js
@@ -0,0 +1,11 @@
+function negativePositive (numbers){
+
+    let orderedNumbers = [];
+
+for(let i = 0; i < numbers.length; i++){
+    let currentNumber = numbers[i]
+    
+}
+
+}
+negativePositive(['7', '-2', '8', '9'])
