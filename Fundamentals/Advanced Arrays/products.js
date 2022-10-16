891b8aedc9475177ce2ae99605114fb82a841377
Valentin <valio990210@gmail.com>
2022-10-16 09:58:40 +0100

    sorted products + numbering

--- Fundamentals/Advanced Arrays/products.js
+++ Fundamentals/Advanced Arrays/products.js
@@ -1,10 +1,9 @@
 function products(prod){
 
     let sortedProducts = prod.sort()
-    console.log(sortedProducts);
 
     for(let i = 0; i < sortedProducts.length; i++){
-        
+        console.log(`${i + 1}. ${sortedProducts[i]}`);
     }
 
 }
