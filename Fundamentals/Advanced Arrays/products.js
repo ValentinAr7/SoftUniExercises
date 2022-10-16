9f2c684f9ae25660bdbf571b17bac269d3e71714
Valentin <valio990210@gmail.com>
2022-10-16 09:57:16 +0100

    sorted alphabetically

---
+++ Fundamentals/Advanced Arrays/products.js
@@ -0,0 +1,12 @@
+function products(prod){
+
+    let sortedProducts = prod.sort()
+    console.log(sortedProducts);
+
+    for(let i = 0; i < sortedProducts.length; i++){
+        
+    }
+
+}
+
+products(['Potatoes', 'Tomatoes', 'Onions', 'Apples'])
