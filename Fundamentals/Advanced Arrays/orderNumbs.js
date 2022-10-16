45b8a12dc6f62cd03994c4060da63e3eb190209a
Valentin <valio990210@gmail.com>
2022-10-16 09:43:56 +0100

    comment

--- Fundamentals/Advanced Arrays/orderNumbs.js
+++ Fundamentals/Advanced Arrays/orderNumbs.js
@@ -1,9 +1,10 @@
 function orderNumbers (numbers){
 
-    let orderInAscending = numbers.sort((a, b) =>{
+    let orderInAscending = numbers.sort((a, b) =>{      //return smalest numbers in array
         return a - b
     })
 
+    console.log(orderInAscending.slice(0,2).join(" "));     //slice the 2 smallest numbers
 }
 
 orderNumbers([30, 15, 50, 5])
