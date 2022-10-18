deb478dcb6040684fcf407eda2afdf023b54ba14
Valentin <valio990210@gmail.com>
2022-10-18 18:13:20 +0100

    new array and forloop

--- Fundamentals/Advanced Arrays/wagons.js
+++ Fundamentals/Advanced Arrays/wagons.js
@@ -1,6 +1,14 @@
-function vagonsSolve (){
+function vagonsSolve (vagons){
 
+    let vagonsNumber = Number(vagons[0])
+    let vagonsCapacity = Number(vagons[1]);
 
+    let vagonsArray = [] 
+    vagonsArray.push(vagons[0]) 
+
+    for(let i = 0; i < vagonsArray.length; i++){
+    console.log(vagonsArray[i]);
+    }
 
 }
 
