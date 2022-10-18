127dfb1f0b7091a1eda8af6644efd6c14c2eb531
Valentin <valio990210@gmail.com>
2022-10-18 18:21:08 +0100

    if

--- Fundamentals/Advanced Arrays/wagons.js
+++ Fundamentals/Advanced Arrays/wagons.js
@@ -6,22 +6,23 @@ function vagonsSolve (vagons){
     let vagonsArray = [] 
     vagonsArray.push(vagons[0]) 
 
+
     for(let i = 0; i < vagonsArray.length; i++){
     console.log(vagonsArray[i]);
 
-        
+        // for(let r = 2; i < )
 
-    if(vagonsArray[i] < vagonsCapacity){
-
-    }
+    // if(vagonsArray[i] < vagonsCapacity){
+    //     vagonsArray[i]+= 
+    // }
     }
 
 }
 
 vagonsSolve(['32 54 21 12 4 0 23',          // wagons       
                             '75',           // wagons capacity
-                            'Add 10',       // commands
-                            'Add 0',        
+                            '10',       // commands
+                            '0',        
                             '30',
                             '10',
                             '75'])
