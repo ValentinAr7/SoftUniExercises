ea398b2698b2ccdb69ccc60d8e892418fa35b6ed
Valentin <valio990210@gmail.com>
2022-10-18 18:02:47 +0100

    sorting

--- Fundamentals/Advanced Arrays/SortingAlphabetically.js
+++ Fundamentals/Advanced Arrays/SortingAlphabetically.js
@@ -3,7 +3,7 @@ function products(prod){
     let sortedProducts = prod.sort()
 
     for(let i = 0; i < sortedProducts.length; i++){
-        console.log(`${i + 1}. ${sortedProducts[i]}`);
+        console.log(`${i + 1}.${sortedProducts[i]}`);
     }
 
     // ${i + 1}  ------ Numbering, starting from i[0], and increase with 1 for ever other product
