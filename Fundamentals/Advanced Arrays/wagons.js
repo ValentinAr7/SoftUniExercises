422b71753b66dc55a9c3cda3dc809c30a66ce3d7
Valentin <valio990210@gmail.com>
2022-10-18 18:32:16 +0100

    shift

--- Fundamentals/Advanced Arrays/wagons.js
+++ Fundamentals/Advanced Arrays/wagons.js
@@ -1,28 +1,23 @@
-function vagonsSolve (vagons){
+function vagonsSolve(arr) {
 
-    let vagonsNumber = Number(vagons[0])
-    let vagonsCapacity = Number(vagons[1]);
+    let passangersInVagons = arr.shift().split("").map(Number)
+    let maxCapacity = Number(arr.shift());
 
-    let vagonsArray = [] 
-    vagonsArray.push(vagons[0]) 
-
-
-    for(let i = 0; i < vagonsArray.length; i++){
-    console.log(vagonsArray[i]);
-
+    for (let i = 0; i < arr.length; i++) {
+        let currentRow = arr[i]
         // for(let r = 2; i < )
 
-    // if(vagonsArray[i] < vagonsCapacity){
-    //     vagonsArray[i]+= 
-    // }
+        // if(vagonsArray[i] < vagonsCapacity){
+        //     vagonsArray[i]+= 
+        // }
     }
 
 }
 
 vagonsSolve(['32 54 21 12 4 0 23',          // wagons       
-                            '75',           // wagons capacity
-                            '10',       // commands
-                            '0',        
-                            '30',
-                            '10',
-                            '75'])
+    '75',           // wagons capacity
+    '10',       // commands
+    '0',
+    '30',
+    '10',
+    '75'])
