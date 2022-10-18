ff9f97f3845228050bcaafef3af2a988cc97738f
Valentin <valio990210@gmail.com>
2022-10-18 18:04:41 +0100

    comments

--- Fundamentals/Advanced Arrays/wagons.js
+++ Fundamentals/Advanced Arrays/wagons.js
@@ -4,10 +4,10 @@ function vagonsSolve (){
 
 }
 
-vagonsSolve(['32 54 21 12 4 0 23',
-                            '75',
-                            'Add 10',
-                            'Add 0',
+vagonsSolve(['32 54 21 12 4 0 23',          // wagons       
+                            '75',           // wagons capacity
+                            'Add 10',       // commands
+                            'Add 0',        
                             '30',
                             '10',
                             '75'])
