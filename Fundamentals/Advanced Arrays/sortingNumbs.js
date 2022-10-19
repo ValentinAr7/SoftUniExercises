1130be5e6811e8ad0fb92982bac47e0d64bdf4df
Valentin <valio990210@gmail.com>
2022-10-19 18:53:06 +0100

    numbers

---
+++ Fundamentals/Advanced Arrays/sortingNumbs.js
@@ -0,0 +1,10 @@
+function sortingNumbs (numbers){
+
+    let orderedNumbers  = [];
+
+    for(let i = 0; i < numbers.length; i++){
+        
+    }
+    
+}
+sortingNumbs ([1, 21, 3, 52, 69, 63, 31, 2, 18, 94])
