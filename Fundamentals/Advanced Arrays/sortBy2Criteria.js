6ed337223d6bc104696f3705dc02066d229ad830
Valentin <valio990210@gmail.com>
2022-10-20 20:56:27 +0100

    order by length

--- Fundamentals/Advanced Arrays/sortBy2Criteria.js
+++ Fundamentals/Advanced Arrays/sortBy2Criteria.js
@@ -1,13 +1,16 @@
 function sortBy2Criteria (array){
 
+    let currentName = array.sort((a,b)=> a.length - b.length)
+    console.log(currentName);
 
 
-    for (let i = 0; i < array.length; i++){
-        let currentName = array[i]
-        console.log(currentName);
-        // currentName= array.sort((a,b)=> a - b)
-        // console.log(currentName);
-    }
+    // for (let i = 0; i < array.length; i++){
+    //     let currentName = array[i]
+
+    //     currentName = array.sort((a,b)=> a.length - b.length)
+    //     console.log(currentName);
+
+    // }
 
 }
 
