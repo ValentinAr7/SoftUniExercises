5e99e8fe9f9c872991a0b4aebeaa0cd6008af3a9
Valentin <valio990210@gmail.com>
2022-10-20 21:00:09 +0100

    join

--- Fundamentals/Advanced Arrays/sortBy2Criteria.js
+++ Fundamentals/Advanced Arrays/sortBy2Criteria.js
@@ -1,16 +1,12 @@
 function sortBy2Criteria (array){
 
-    let currentName = array.sort((a,b)=> a.length - b.length)
-    console.log(currentName);
+    for (let i = 0; i < array.length; i++){
+        let currentName = array[i]
 
+        currentName = array.sort((a,b)=> a.length - b.length)
+        console.log(currentName.join(" "));
 
-    // for (let i = 0; i < array.length; i++){
-    //     let currentName = array[i]
-
-    //     currentName = array.sort((a,b)=> a.length - b.length)
-    //     console.log(currentName);
-
-    // }
+    }
 
 }
 
