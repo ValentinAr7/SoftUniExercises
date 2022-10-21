aff8912d5130057c4cf115007abd0a63e1e5aeaf
Valentin <valio990210@gmail.com>
2022-10-21 18:32:22 +0100

    includes name

--- Fundamentals/Advanced Arrays/houseParty.js
+++ Fundamentals/Advanced Arrays/houseParty.js
@@ -7,6 +7,11 @@ function houseParty(arr){
         let name = command[0]
 
         if(el.length === 3){
+            if(newGuestList.includes(name)){
+                console.log(`${name} is already in the list!`);
+            } else {
+                
+            }
 
 
         }
