94def73a310f4cd4fcf576020c3ce9e76dc28e55
Valentin <valio990210@gmail.com>
2022-10-22 16:12:17 +0100

    replace

--- Fundamentals/Advanced Arrays/Exam-shopping-cart.js
+++ Fundamentals/Advanced Arrays/Exam-shopping-cart.js
@@ -4,13 +4,18 @@ function shopping(input){
 
     let products = input.shift(input[0])
 
+   let products2 = products.replaceAll("!", " ")
+    shoppingList.push(products2)
+
+
     for(let i = 0; i < input.length; i++){
         
         let elements = input[i].split(" ")
 
-        
+        // if(elements[0] == )
     }
 }
+
 
 shopping(["Tomatoes!Potatoes!Bread",
 
