00ce50c6fe86a1b9c02977e5b5a56c787c5ffd7c
Valentin <valio990210@gmail.com>
2022-10-22 15:50:01 +0100

    exam prep

---
+++ Fundamentals/Advanced Arrays/Exam-shopping-cart.js
@@ -0,0 +1,16 @@
+function shopping(input){
+
+    let shoppingList = []
+
+    let products = input.shift(input[0])
+
+    console.log(products);
+}
+
+shopping(["Tomatoes!Potatoes!Bread",
+
+"Unnecessary Milk",
+
+"Urgent Tomatoes",
+
+"Go Shopping!"])
