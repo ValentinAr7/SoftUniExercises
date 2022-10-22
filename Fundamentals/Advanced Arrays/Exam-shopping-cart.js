c13e9cabec938c5fc58889d33cc6f1624215fd71
Valentin <valio990210@gmail.com>
2022-10-22 16:40:28 +0100

    second if

--- Fundamentals/Advanced Arrays/Exam-shopping-cart.js
+++ Fundamentals/Advanced Arrays/Exam-shopping-cart.js
@@ -2,24 +2,38 @@ function shopping(input){
 
     let shoppingList = []
 
-    let products = input.shift(input[0])
+    let products = input.shift(input[0]).replaceAll("!", " ")
 
-   let products2 = products.replaceAll("!", " ")
-    shoppingList.push(products2)
+    console.log(products);
 
 
     for(let i = 0; i < input.length; i++){
         
         let elements = input[i].split(" ")
 
-        // if(elements[0] == )
+        if(elements[0] == "Unnecessary"){
+            products.splice(elements[1], 1)
+            console.log(products);
+
+
+
+
+
+
+            // if(products.includes(elements[1])){
+            // let index = products.indexOf(elements[1])
+            // console.log(index);
+
+            
+            }
+        }
     }
-}
+
 
 
 shopping(["Tomatoes!Potatoes!Bread",
 
-"Unnecessary Milk",
+"Unnecessary Potatoes",
 
 "Urgent Tomatoes",
 
