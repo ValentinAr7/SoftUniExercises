9f3d38d364b09b54596e3ad75b7c20ce29323ff1
Valentin <valio990210@gmail.com>
2022-10-22 15:25:23 +0100

    total plunder

--- Fundamentals/Advanced Arrays/piratesPlunder.js
+++ Fundamentals/Advanced Arrays/piratesPlunder.js
@@ -12,9 +12,18 @@ function pirates (array){
             totalPlunder += dailyPlunder / 2
         ) 
 
-        
+        if(i % 5 == 0){
+           totalPlunder = (totalPlunder * 0.7) 
+        }
     }
+        
     
+        if(totalPlunder > expectedPlunder){
+            console.log(`Ahoy! ${(totalPlunder).toFixed(2)} plunder gained.`);
+        } else {
+            let percentage = (totalPlunder / expectedPlunder) * 100 
+            console.log(`Collected only ${(percentage).toFixed(2)}% of the plunder.`)
+        }
 }
 
 pirates([
