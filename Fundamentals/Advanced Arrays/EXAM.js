54ebac772a96ae4a1d778dd11834b01f9b6b033d
Valentin <valio990210@gmail.com>
2022-10-23 09:22:18 +0100

    new line

--- Fundamentals/Advanced Arrays/EXAM.js
+++ Fundamentals/Advanced Arrays/EXAM.js
@@ -3,20 +3,32 @@ function exam (array){
     let experianceNeeded = array.shift()
     let countBattles = array.shift()
 
+    array.unshift(0)
+
+
     let totalExperiance = 0
     
-    for(let i = 0; i <= array.length; i++){
-        let currentBattle = array[i]
+    for(let i = 0; i < array.length; i++){
+        let currentBattle = array[i] 
+
 
         totalExperiance += currentBattle
 
         if(i % 3 == 0)(
-            totalExperiance += (currentBattle * 15) / 100
+            totalExperiance += currentBattle * 0.15
         ) 
+
+        if(i % 5 == 0)(
+            totalExperiance -= (currentBattle * 10) / 100
+        )
+
     }
 
-    console.log(totalExperiance);
-
+    if(totalExperiance > experianceNeeded){
+     console.log(`Player successfully collected his needed experience for ${countBattles} battles.`);
+        } else {
+            console.log(`Player was not able to collect the needed experience, ${(experianceNeeded - totalExperiance).toFixed(2)} more needed.`);
+        }
 }
 
 exam ([500, 5, 
