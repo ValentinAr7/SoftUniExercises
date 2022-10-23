3e17e41b2e19e2ce53f95621c0cc1e221cb75bc6
Valentin <valio990210@gmail.com>
2022-10-23 08:58:41 +0100

    s

--- Fundamentals/Advanced Arrays/EXAM.js
+++ Fundamentals/Advanced Arrays/EXAM.js
@@ -1,15 +1,24 @@
 function exam (array){
 
-    let experianceNeeded = Number(array[0])
-    let countBattles = Number(array[1])
+    let experianceNeeded = array.shift()
+    let countBattles = array.shift()
 
-    console.log(countBattles);
+    let totalExperiance = 0
     
-    for(let i = 0; i < array.length; i++){
-        
+    for(let i = 0; i <= array.length; i++){
+        let currentBattle = array[i]
+
+        totalExperiance += currentBattle
+
+        if(i % 3 == 0)(
+            totalExperiance += (currentBattle * 15) / 100
+        ) 
     }
 
+    console.log(totalExperiance);
 
 }
 
-exam[(500, 5, 50, 100, 200, 100, 30)]
+exam ([500, 5, 
+    
+    50, 100, 200, 100, 30])
