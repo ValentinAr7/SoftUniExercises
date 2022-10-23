0b98cdaacc3acfae8ba2f8ab16701b015bf44285
Valentin <valio990210@gmail.com>
2022-10-23 08:29:11 +0100

    save

---
+++ Fundamentals/Advanced Arrays/EXAM.js
@@ -0,0 +1,16 @@
+function exam(input){
+
+    let experianceNeeded = Number(input[0])
+    console.log(experianceNeeded);
+
+
+}
+
+exam[(500, //amount of experiance needed to unlock new tank
+    5,      // count of battles
+
+    50,     // experiance gain for each battle
+    100, 
+    200, 
+    100, 
+    30)]
