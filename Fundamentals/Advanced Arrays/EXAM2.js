924b5ae68b09c0099310951ca1ad2ab5b4c2190a
Valentin <valio990210@gmail.com>
2022-10-23 09:28:04 +0100

    .

---
+++ Fundamentals/Advanced Arrays/EXAM2.js
@@ -0,0 +1,12 @@
+function exam2 (array){
+
+    let startingFuel = Number(array[1])
+    let ammunition = Number(array[2])
+
+    
+}
+
+exam2 (["Travel 20||Enemy 50||Enemy 50||Enemy 10||Repair 15||Enemy 50||Titan",
+    60,
+    100
+])
