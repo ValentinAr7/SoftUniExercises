830d1607e4f78d44d62a8b163eb173623f0b6da5
Valentin <valio990210@gmail.com>
2022-10-24 18:22:22 +0100

    take the values in variables

--- Fundamentals/Exam Preparation/softUniReception.js
+++ Fundamentals/Exam Preparation/softUniReception.js
@@ -1,7 +1,9 @@
-function softUniReception (){
+function softUniReception (array){
 
-
+    let teacher1 = array.shift()
+    let teacher2 = array.shift()
+    let teacher3 = array.shift()
 
 }
 
-softUniReception['5','6','4','20']
+softUniReception(['5','6','4','20'])
