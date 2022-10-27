873ddbd0a3b1ae8223006b09d2950eec4e19e8b3
Valentin <valio990210@gmail.com>
2022-10-27 18:48:39 +0100

    comments

--- Fundamentals/Exam Preparation/theHuntingGames.js
+++ Fundamentals/Exam Preparation/theHuntingGames.js
@@ -1,20 +1,29 @@
 function arrayModifier (array){
 
     let arrayNumbers = array.shift().split(" ").map(Number)
+    //take the first element of the array, split the numbers, and convert them to numbers
 
-    while (array != "end"){
+    while (array != "end"){ // while the element in different than "end" run the code below
 
-        let commandLine = array.shift().split(" ")
-        let command = commandLine[0]
-        let index1 = Number(commandLine[1])
-        let index2 = Number(commandLine[2])
+        let commandLine = array.shift().split(" ")      
+        //take the first line of the array ('swap 1 3',) and split it
 
-        switch (command) {
+
+        let command = commandLine[0]            //command = the first index of the command line e.g("swap")
+        let index1 = Number(commandLine[1])     // index1 = the second index of the command line
+        let index2 = Number(commandLine[2])     // index2 = the third index of the command line
+
+        switch (command) {          
+            
+            
             case "decrease":
                 for(let i = 0; i < arrayNumbers.length; i++){
                     arrayNumbers[i]--
                 }                
                 break;
+        // if the command = "decrease"
+
+
 
             case "swap":
 
