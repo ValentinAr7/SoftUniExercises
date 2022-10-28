f18404860c404e1114211d05a57481f31f78709a
Valentin <valio990210@gmail.com>
2022-10-28 18:30:43 +0100

    for loop

--- Fundamentals/Exam Preparation/hearts.js
+++ Fundamentals/Exam Preparation/hearts.js
@@ -7,10 +7,16 @@ function stValentines(input){
         let command = input[0]
         let jumps = input[1]
 
-        
+        for(let i = 0; i < neiberhood.length; i++){
+            let currentIndex = neiberhood[i]
+            let cupid = 0
+            if(command = "Jump"){
+                currentIndex -= 2
+            }
+        }
+        console.log(neiberhood);
 
     }
-
 }
 
 stValentines (["10@10@10@2",
