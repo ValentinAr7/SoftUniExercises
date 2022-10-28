20eb8cd62b82b48f4d1c9df2e76d65c61ab23557
Valentin <valio990210@gmail.com>
2022-10-28 19:24:23 +0100

    if statements

--- Fundamentals/Exam Preparation/hearts.js
+++ Fundamentals/Exam Preparation/hearts.js
@@ -16,12 +16,28 @@ function stValentines(input){
 
             if(neiberhood[cupid] == 0){
                 console.log(`Place ${neiberhood[i]} already had Valentine's day.`);
+            } else {
+                neiberhood[cupid] -= 2
+                if(neiberhood[cupid] == 0){
+                    console.log(`Place ${cupid} has Valentine's day.`);
+                }
             }
-        
+    }
+console.log(`Cupid's last position was ${cupid}`);
 
+    let missedHouses = 0
+    for (let house of neiberhood) {
+        if(house > 0){
+            missedHouses++
         }
     }
 
+
+    if(missedHouses > 0){
+        console.log(`Cupid has failed ${missedHouses} houses.`);
+    } else{
+        console.log("Mission complete");
+    }
 }
 
 stValentines (["10@10@10@2",
