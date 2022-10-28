77329d3cd6d34dc707e6981fac5f09385eed4e53
Valentin <valio990210@gmail.com>
2022-10-28 18:09:47 +0100

    separete input as string

---
+++ Fundamentals/Exam Preparation/hearts.js
@@ -0,0 +1,11 @@
+function stValentines(input){
+
+    let string = input.shift().split("@").map(Number)
+
+
+}
+
+stValentines (["10@10@10@2",
+                "Jump 1",
+                "Jump 2",
+                "Love!"])
