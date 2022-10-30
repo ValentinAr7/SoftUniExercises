a9d4795bee3eee44bd1fa6272eadd3aa3d36cdc2
Valentin <valio990210@gmail.com>
2022-10-30 15:39:10 +0000

    town info store

---
+++ Fundamentals/Objects and Classes/towns.js
@@ -0,0 +1,19 @@
+function towns (input){
+
+    let townInfo = {}
+
+    for (const line of input) {
+        
+        let [townName, latitude, longidtude] = line.split(" | ")
+
+        townInfo.town = townName
+        townInfo.latitude = Number(latitude).toFixed(2)
+        townInfo.longidtude = Number(longidtude).toFixed(2)
+
+console.log(townInfo);
+    }
+}
+
+towns (['Sofia | 42.696552 | 23.32601',
+
+'Beijing | 39.913818 | 116.363625'])
