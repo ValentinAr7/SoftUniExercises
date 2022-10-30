8484f5d6a6e6cbd301944b1959380fbc60aca3ee
Valentin <valio990210@gmail.com>
2022-10-30 10:38:56 +0000

    for in loop

--- Fundamentals/Objects and Classes/object.js
+++ Fundamentals/Objects and Classes/object.js
@@ -6,11 +6,15 @@ function object (list){
         employeeList[person] = person.length
     });
 
+    for (const key in employeeList) {
+        console.log(`Name: ${key} -- Personal Number: ${employeeList[key]}`);
+    }
+
 }
 
-object[
+object([
     'Silas Butler',
     'Adnaan Buckley',
     'Juan Peterson',
     'Brendan Villarreal'
-    ]
+    ])
