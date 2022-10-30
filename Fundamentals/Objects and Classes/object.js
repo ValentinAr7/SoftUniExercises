ae81f48146b99f83488ce166a5fdd636a1c4986a
Valentin <valio990210@gmail.com>
2022-10-30 10:37:22 +0000

    for each

---
+++ Fundamentals/Objects and Classes/object.js
@@ -0,0 +1,16 @@
+function object (list){
+
+    let employeeList = {}
+
+    array.forEach(person => {
+        employeeList[person] = person.length
+    });
+
+}
+
+object[
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+    ]
