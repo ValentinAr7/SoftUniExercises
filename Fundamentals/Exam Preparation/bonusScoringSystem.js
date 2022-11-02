2b83f9f93b9f650cf3da25e4b08b59b06f73e20f
Valentin <valio990210@gmail.com>
2022-11-02 17:46:13 +0000

    math ceil

--- Fundamentals/Exam Preparation/bonusScoringSystem.js
+++ Fundamentals/Exam Preparation/bonusScoringSystem.js
@@ -4,14 +4,19 @@ function bonusScoringSystem (array){
     let numberOfStudents = array.shift()
     let totalNumOfLectures = array.shift()
     let additionalBonus = array.shift()
+    let totalBonus = 0
 
     for(let i = 0; i < array.length; i++){
-        
+        let currentStudentAtendences = array[i]
+
+        totalBonus = currentStudentAtendences / totalNumOfLectures * (5 + additionalBonus)
+        console.log(Math.ceil(totalBonus));
+
     }
 
 }
 
-bonusScoringSystem (5,      // number of students
+bonusScoringSystem ([5,      // number of students
     25,                     // number of lectures
     30,                     //additional bonus
 
@@ -20,4 +25,4 @@ bonusScoringSystem (5,      // number of students
     24,
     16,
     20
-    )
+])
