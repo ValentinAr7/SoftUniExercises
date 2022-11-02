37df3023cd69e978ffd0e6b2f123841bd2f1c11f
Valentin <valio990210@gmail.com>
2022-11-02 17:37:17 +0000

    variables

--- Fundamentals/Exam Preparation/bonusScoringSystem.js
+++ Fundamentals/Exam Preparation/bonusScoringSystem.js
@@ -1,12 +1,16 @@
-function bonusScoringSystem (){
+function bonusScoringSystem (array){
 
 
+    let numberOfStudents = array.shift()
+    let totalNumOfLectures = array.shift()
+    let additionalBonus = array.shift()
 
 }
 
 bonusScoringSystem (5,
     25,
-    30,
+    30, 
+
     12,
     19,
     24,
