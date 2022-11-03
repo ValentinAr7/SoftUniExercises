3dd2f3b093c7b9da5d24d7aa17d8a137ae2f44ad
Valentin <valio990210@gmail.com>
2022-11-03 18:12:39 +0000

    case potion

--- Fundamentals/Exam Preparation/muOnline.js
+++ Fundamentals/Exam Preparation/muOnline.js
@@ -2,14 +2,29 @@ function mu (str){
 
     let array = str.shift().split("|")
 
+    let health = 0
+    let bitcoins = 0
+
     for (const element of array) {
         let line = element.split(" ")
         let command = line[0]
-        let num = line[1]
+        let num = Number(line[1])
         console.log(command);
+
+        switch (command) {
+            case "potion":
+                health += num
+                console.log(`You healed for ${num} hp`);
+                console.log(`Current health: ${health} hp`);
+               if(health = 100){
+                health === 100
+               }
+                break;
+        
+        }
     }
 
 
 }
-
-mu (["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
+mu (["potion 10"])
+// mu (["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
