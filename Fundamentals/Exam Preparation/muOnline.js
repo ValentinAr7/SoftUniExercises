a7c3817f2e71db57b5bcf6c75a1d6f7e33db92e8
Valentin <valio990210@gmail.com>
2022-11-03 19:54:14 +0000

    result

--- Fundamentals/Exam Preparation/muOnline.js
+++ Fundamentals/Exam Preparation/muOnline.js
@@ -5,21 +5,20 @@ function mu(str) {
     let health = 100
     let bitcoins = 0
 
+
     for (const element of array) {
 
         let line = element.split(" ")
         let command = line[0]
         let num = Number(line[1])
-
-
+  
 
         if (command == "potion") {
             health += num
+
+
             console.log(`You healed for ${num} hp`);
             console.log(`Current health: ${health} hp`);
-            if (health >= 100 ) {
-                health == 100
-            } 
 
 
         } else if (command == "chest") {
@@ -30,13 +29,26 @@ function mu(str) {
         } else {
             health -= num
 
-            if (health <= 0){
+                
+                if (health <= 0){
+
+                    
+            for(let i = 0; i < array.length; i++){
+
                 console.log(`You died! Killed by ${command}.`);
+                let lastIndex = array.pop()
+                console.log(array.indexOf(lastIndex));
+                // console.log(`for line ${forLine}`);
+                // let currentLine = array.pop()
+                // console.log(currentLine);
+                // console.log(array.indexOf(currentLine));
+            }
                 break;
+            
+        
+
             } else { 
                 console.log(`You slayed ${command}.`);
-
-
             }
         }
 
@@ -45,5 +57,5 @@ function mu(str) {
 
 }
 // mu(["potion 10|chest 100"])
-// mu (["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
-mu(["cat 10|potion 30|orc 10|chest 10|snake 25|chest 110"])
+mu (["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
+// mu(["cat 10|potion 30|orc 10|chest 10|snake 25|chest 110"])
