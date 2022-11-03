a8ce352e2392129b66bd3fd3c4c2b3385277de5e
Valentin <valio990210@gmail.com>
2022-11-03 18:30:02 +0000

    canges

--- Fundamentals/Exam Preparation/muOnline.js
+++ Fundamentals/Exam Preparation/muOnline.js
@@ -1,4 +1,4 @@
-function mu (str){
+function mu(str) {
 
     let array = str.shift().split("|")
 
@@ -9,22 +9,40 @@ function mu (str){
         let line = element.split(" ")
         let command = line[0]
         let num = Number(line[1])
-        console.log(command);
 
-        switch (command) {
-            case "potion":
-                health += num
-                console.log(`You healed for ${num} hp`);
-                console.log(`Current health: ${health} hp`);
-               if(health = 100){
-                health === 100
-               }
-                break;
-        
+        if (health >= 100 ) {
+            health === 100
+        } 
+
+        if (health <=0 ){
+            health === 0 
         }
+
+
+        if (command == "potion") {
+            health += num
+            console.log(`You healed for ${num} hp`);
+            console.log(`Current health: ${health} hp`);
+
+
+        } else if (command == "chest") {
+            bitcoins += num
+            console.log(`You found ${num} bitcoins.`);
+
+
+        } else {
+            health -= num
+
+            if (health <= 0){
+                console.log(`You slayed ${command}.`);
+            } else { 
+                console.log(`You died! Killed by ${command}.`);
+            }
+        }
+
     }
 
 
 }
-mu (["potion 10"])
-// mu (["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
+// mu(["potion 10|chest 100"])
+mu (["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
