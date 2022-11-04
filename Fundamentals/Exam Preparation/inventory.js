f9d87b1c909114c61dfc1399c7f1110609a95a29
Valentin <valio990210@gmail.com>
2022-11-04 18:27:57 +0000

    while loop

--- Fundamentals/Exam Preparation/inventory.js
+++ Fundamentals/Exam Preparation/inventory.js
@@ -1,8 +1,19 @@
 function inventory (array){
 
-    let
+    let journal = array.shift().split(",")
 
+    currentCommand = array.shift()
+    console.log(currentCommand);
 
+    while (currentCommand !== "Craft!") {
+
+        let lineItems = currentCommand.split(" ")
+        let command = lineItems[0]
+        let item = lineItems[2]
+        console.log(lineItems);
+            
+
+}
 }
 
 inventory(["Iron, Wood, Sword",
