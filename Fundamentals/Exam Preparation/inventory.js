ed193a0e31f02dd6090a45430e2311a364725ffe
Valentin <valio990210@gmail.com>
2022-11-04 18:46:15 +0000

    inventory

--- Fundamentals/Exam Preparation/inventory.js
+++ Fundamentals/Exam Preparation/inventory.js
@@ -1,21 +1,21 @@
 function inventory (array){
 
-    let journal = array.shift().split(" , ")
+    let journal = array.shift().split(" ")
     console.log(journal);
     currentCommand = array.shift()
     console.log(currentCommand);
 
-    while (currentCommand != "Craft!") {
+//     while (currentCommand != "Craft!") {
         
-        let lineItems = currentCommand.split("-")
-        let command = lineItems[0]
-        let item = lineItems[1]
+//         let lineItems = currentCommand.split("-")
+//         let command = lineItems[0]
+//         let item = lineItems[1]
             
 
-}
+// }
 }
 
-inventory(["Iron, Wood, Sword",
+inventory(["Iron Wood Sword",
     "Collect - Gold",
     "Drop - Wood",
     "Craft!"
