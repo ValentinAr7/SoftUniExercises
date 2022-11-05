2fac3a9172ca106910d66c9d33d1c4dbb8e71c39
Valentin <valio990210@gmail.com>
2022-11-05 19:11:08 +0000

    formating

--- Fundamentals/Exam Preparation/shootForTheWin.js
+++ Fundamentals/Exam Preparation/shootForTheWin.js
@@ -1,42 +1,40 @@
-function shootWin (array){
+function shootWin(array) {
 
     let shotTargets = array.shift().split(" ").map(Number)
-
     let targetIndex = array.pop()
-
     let shooting = array.map(Number)
 
     let numberOfShots = 0
 
-    for(let i = 0; i < shooting.length; i++){
+    for (let i = 0; i < shooting.length; i++) {
         let currentShot = shooting[i]
         let currentTarget = shotTargets[currentShot]
 
-        if(currentTarget > 0){
+        if (currentTarget > 0) {
             shotTargets[currentShot] = -1
             numberOfShots++
 
-
-        for(let j =0; j < shotTargets.length; j++){
-            if(shotTargets[j] > -1){
-                if(shotTargets[j] > currentTarget){
-                    shotTargets[j] -= currentTarget
-                } else {
-                    shotTargets[j] += currentTarget
+            for (let j = 0; j < shotTargets.length; j++) {
+                if (shotTargets[j] > -1) {
+                    if (shotTargets[j] > currentTarget) {
+                        shotTargets[j] -= currentTarget
+                    } else {
+                        shotTargets[j] += currentTarget
+                    }
                 }
             }
         }
     }
 
-            }
-            console.log(`Shot targets ${numberOfShots} -> ${shotTargets.join(" ")}`);
-            console.log(shooting);
+    console.log(`Shot targets ${numberOfShots} -> ${shotTargets.join(" ")}`);
+    console.log(shooting);
+    
 }
 
-shootWin (["24 50 36 70",
-        "0",
-        "4",
-        "3",
-        "1",
-        "End"])
+shootWin(["24 50 36 70",
+    "0",
+    "4",
+    "3",
+    "1",
+    "End"])
 
