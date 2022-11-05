0db5b7ed1287b889575715d35da9a1f6e36eb511
Valentin <valio990210@gmail.com>
2022-11-05 18:08:55 +0000

    first elements

---
+++ Fundamentals/Exam Preparation/shootForTheWin.js
@@ -0,0 +1,14 @@
+function shootWin (array){
+
+    let shotTargets = array.shift().split(" ").map(Number)
+    console.log(shotTargets);
+
+}
+
+array (["24, 50, 36, 70",
+        "0",
+        "4",
+        "3",
+        "1",
+        "End"])
+
