a4c3140e36be7af7eb6c8229600677f36c379cc4
Valentin <valio990210@gmail.com>
2022-11-05 19:08:04 +0000

    shooting targets

--- Fundamentals/Exam Preparation/shootForTheWin.js
+++ Fundamentals/Exam Preparation/shootForTheWin.js
@@ -1,14 +1,32 @@
 function shootWin (array){
 
     let shotTargets = array.shift().split(" ").map(Number)
-    console.log(shotTargets);
 
     let targetIndex = array.pop()
 
-    array = array.map(Number)
-    console.log(array);
+    let shooting = array.map(Number)
 
-    
+    for(let i = 0; i < shooting.length; i++){
+        let currentShot = shooting[i]
+        let currentTarget = shotTargets[currentShot]
+
+        if(currentTarget > 0){
+            shotTargets[currentShot] = -1
+
+        for(let j =0; j < shotTargets.length; j++){
+            if(shotTargets[j] > -1){
+                if(shotTargets[j] > currentTarget){
+                    shotTargets[j] -= currentTarget
+                } else {
+                    shotTargets[j] += currentTarget
+                }
+            }
+        }
+    }
+
+            }
+            console.log(shotTargets);
+            console.log(shooting);
 }
 
 shootWin (["24 50 36 70",
