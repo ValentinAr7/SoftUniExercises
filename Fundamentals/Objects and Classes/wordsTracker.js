5a0e4c731ae3864815cca835b52bb5acd0f0b4b7
Valentin <valio990210@gmail.com>
2022-11-06 16:28:34 +0000

    count the words in array

--- Fundamentals/Objects and Classes/wordsTracker.js
+++ Fundamentals/Objects and Classes/wordsTracker.js
@@ -9,8 +9,16 @@ function wordsTracker (input){
     }
     
     for (const word of input) {
-        
+        if(result.hasOwnProperty(word)){
+            result[word]++
+        }
     }
+
+     const sorted = Object.entries(result).sort((a,b)=> a[1] - b[1])
+
+     for (const kvp of sorted) {
+        console.log(`${kvp[0]} - ${kvp[1]}`);
+     }
 }
 
 wordsTracker([
