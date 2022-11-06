5c26282527488d17444ba9d5db5a70ecb2d71b56
Valentin <valio990210@gmail.com>
2022-11-06 16:04:52 +0000

    save

--- Fundamentals/Exam Preparation/computerStore.js
+++ Fundamentals/Exam Preparation/computerStore.js
@@ -1,3 +1,6 @@
+//PROGRAMMING FUNDAMENTALS WITH JAVASCRIPT - МАЙ 2022
+//Exam Preparation
+
 function computerStore(data) {
 
     let totalPrice = 0
@@ -21,7 +24,9 @@ function computerStore(data) {
         let currentPart = partPrices[i]
         if(currentPart < 0){
             console.log("Invalid price!");
-        }
+            continue
+        } 
+
         totalPrice += partPrices[i]
 
     }
@@ -40,18 +45,22 @@ function computerStore(data) {
         console.log("-----------");
         console.log(`Total price: ${(receiptPriceSpecial).toFixed(2)}$`)
 
+        if(receiptPriceSpecial === 0){
+            console.log("Invalid order");
+        }
 
         //Regular clients discounts
     } else if (command === "regular") {
         regularTaxes = totalPrice * 0.2
         finalPriceNoDiscount = totalPrice + regularTaxes
         receiptPriceRegular = totalPrice + regularTaxes
-
+        
         console.log(`Congratulations you've just bought a new computer!`);
         console.log(`Price without taxes: ${(totalPrice).toFixed(2)}$`);
         console.log(`Taxes: ${(regularTaxes).toFixed(2)}$`);
         console.log("-----------");
         console.log(`Total price: ${(receiptPriceRegular).toFixed(2)}$`)
+
     }
 
    
@@ -59,11 +68,14 @@ function computerStore(data) {
 
 }
 
-computerStore(["1050",
-    "200",
+computerStore([
+    "1023",
+    "15",
+    "-20",
+    "-5.50",
     "450",
-    "2",
-    "18.50",
-    "16.86",
-    "regular"
+    "20",
+    "17.66",
+    "19.30",
+    "regular",
 ])
