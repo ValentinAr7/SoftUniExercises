d5e678b2fe46ff85fc1d7473994544ece3a2fef6
Valentin <valio990210@gmail.com>
2022-11-07 18:51:05 +0000

    check the commands

--- Fundamentals/Exam Preparation/treasureHunt.js
+++ Fundamentals/Exam Preparation/treasureHunt.js
@@ -1,23 +1,42 @@
 function treasurHunt(input){
 
 let initialLoot = input.shift().split("|")
-let end = input.pop()
 
+let index = 0
+let command = input[index]
+index++
+
+while(command !== "Yohoho"){
+    let tokens = command.split(" ") // ["wood", "gold", "coins"]
+    let operation = tokens.shift()        //commands only
+
+    switch (command) {
+        case "Loot":
+                treasureChest.push(currentLine)
+            break;
+    
+        default:
+            break;
+    }
+}
+
+
+
+}
+
+
+
+
+let treasureChest = []
+treasureChest.push(initialLoot)
 
 for(let i = 0; i < input.length; i++){
     let currentLine = input[i]  //all
 
 
-    let lineArguments = currentLine.split(" ") // ["wood", "gold", "coins"]
-    let command = lineArguments.shift()        //commands only
-    let item1 = lineArguments[1]
-    let item2 = lineArguments[2]
-    let item3 = lineArguments[3]
 
     console.log(currentLine);
 
-
-}
 
 }
 
