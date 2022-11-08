e85116f81528066ff910f90d841f3297581aa4f3
Valentin <valio990210@gmail.com>
2022-11-08 21:55:37 +0000

    damage repair

--- Fundamentals/Exam Preparation/man-o-war.js
+++ Fundamentals/Exam Preparation/man-o-war.js
@@ -10,38 +10,55 @@ function manOwar (data){
     while(command != "Retire"){
         let tokens = command.split(" ")
         let operation = tokens[0]
-        let atackIndex = tokens[1]
-        let damage = tokens[2]
+        let index1 = Number(tokens[1])
+        let index2 = Number(tokens[2])
+        let index3 = Number(tokens[3])
         
         switch (operation) {
             case "Fire":
-                if(atackIndex < statusWarShip.length){
-            statusWarShip[atackIndex] = statusWarShip[atackIndex] - damage
-                console.log(statusWarShip);
+                if(index1 < statusWarShip.length){
+
+            statusWarShip[index1] = statusWarShip[index1] - index2
             }
 
-            if(statusWarShip[atackIndex] <= 0){
+            if(statusWarShip[index1] <= 0){
                 console.log("You won! The enemy ship has sunken.");
             }
+                break;
 
-            
-                break;
-        
-            default:
-                break;
+
+
+                case "Defend":
+                i = Number(tokens[1])
+                let end = Number(tokens[2])
+                let dmg = Number(tokens[3])
+
+                if(i < 0 || i > statusOfShip.length || end < 0 || end > statusOfShip.length){
+                    command = data[index]
+                    index++
+                    continue
+                }
+
+                for(let x = i; x <= end; x++){
+                    statusOfShip[x] = statusOfShip[x] - dmg
+                }
+
+                case "Repair":
+                    
+                    break
         }
 
         command = data[index]
         index++
     }
-
+console.log(statusOfShip);
 
 }
 
 manOwar(["12>13>11>20>66",
     "12>22>33>44>55>32>18",
     "70",
-    "Fire 2 88",
+    "Fire 2 11",
     "Fire 8 100",
     "Defend 3 6 11",
     "Defend 0 3 5",
