c15293d7e538926dce2b09eb2c39a7255d164de7
Valentin <valio990210@gmail.com>
2022-11-08 18:43:58 +0000

    variables

---
+++ Fundamentals/Exam Preparation/man-o-war.js
@@ -0,0 +1,23 @@
+function manOwar (data){
+
+    let statusOfShip = data.shift().split(">")
+    let statusWarShip = data.shift.split(">")
+    let maximumHealth = data.shift()
+
+    let index = 0
+    let 
+
+
+}
+
+manOwar(["12>13>11>20>66",
+    "12>22>33>44>55>32>18",
+    "70",
+    "Fire 2 11",
+    "Fire 8 100",
+    "Defend 3 6 11",
+    "Defend 0 3 5",
+    "Repair 1 33",
+    "Status",
+    "Retire"
+    ])
