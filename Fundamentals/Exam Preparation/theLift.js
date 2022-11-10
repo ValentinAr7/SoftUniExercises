675f745ae159956a5d63858a9657f1fa6332c565
Valentin <valio990210@gmail.com>
2022-11-10 20:36:48 +0000

    wagons task

--- Fundamentals/Exam Preparation/theLift.js
+++ Fundamentals/Exam Preparation/theLift.js
@@ -1,22 +1,57 @@
 function theLift(input){
 
     let peopleWaiting = input.shift()
-    let queue = input.shift().split(" ")
-    let liftSpace = 4
+    let wagons = input.shift().split(" ")
 
-    for(let i = 0; i < queue.length; i++){
-        let currenWagon = queue[i]
+    let liftFreeCapacity = wagons.length * 4 
+    console.log(wagons);
+    let sum = 0
 
-        if(currenWagon < liftSpace){
-            peopleWaiting -= currenWagon        
-            console.log(peopleWaiting);
-    }
-}
+    
 
-}
+    for(i = 0; i < wagons.length; i++){
+        sum += parseInt(wagons[i]);
+        }
+
+        let freeCapacity = liftFreeCapacity - sum
+        
+        for(let i = 0; i < wagons.length; i++){
+            let currentWagon = wagons[i]
+            let freeWagonSpace = 4 - currentWagon
+            console.log(freeWagonSpace);
+
+            if(peopleWaiting <= 0){
+                break
+            }
+
+            if(freeWagonSpace <= 0){
+                continue
+            }
+
+            if(peopleWaiting < freeWagonSpace){
+                    wagons[i] += peopleWaiting
+                    freeWagonSpace -= peopleWaiting
+                    freeCapacity -= peopleWaiting
+                    peopleWaiting -= peopleWaiting
+                    break
+            }
+            else if(peopleWaiting > freeWagonSpace){
+                peopleWaiting -= peopleWaiting
+                wagons[i] += freeWagonSpace
+                liftFreeCapacity -= freeWagonSpace
+            } else{
+                peopleWaiting -=peopleWaiting
+                wagons[i]
+            }
+        }
+
+     }
+    
+
+
 
 theLift([
     "15",
-"0 1 2 0"
+"5 1 2 0"
 ]
 )
