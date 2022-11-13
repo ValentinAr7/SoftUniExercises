6f3ea6858243a65255d19b1e334130c1cfe220e8
Valentin <valio990210@gmail.com>
2022-11-13 09:31:59 +0000

    current text manipulation

--- Fundamentals/Text Processing/textProcessing.js
+++ Fundamentals/Text Processing/textProcessing.js
@@ -1,5 +1,11 @@
-function textProcessing (){
+function textProcessing (words, text){
 
+    let wordsArray = words.shift().split(", ")
+
+    for(let i = 0; i < wordsArray.length; i++){
+        let currentWord = wordsArray[i]
+        console.log(currentWord);
+    }
 
 }
 textProcessing(['great, learning',
