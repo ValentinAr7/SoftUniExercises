c58bb2211e33f4f66471674d0497db1058afdd53
Valentin <valio990210@gmail.com>
2022-11-13 12:00:42 +0000

    concatentaion

---
+++ Fundamentals/Text Processing/repeatingLetters.js
@@ -0,0 +1,19 @@
+function repeatingLetters (string){
+
+    let letters = string.split("")
+    console.log(letters);
+
+    let result = []
+
+    for(let i = 0; i< letters.length; i++){
+        let currentLetter = letters[i]
+        let prev = letters[i - 1]
+    
+        if(currentLetter !== prev){
+result += currentLetter
+        }
+    }
+console.log(result);
+}
+
+repeatingLetters('aaaaabbbbbcdddeeeedssaa')
