11bb5a8caf919b0725c57b9b0fefb5ffd9cb4bda
Valentin <valio990210@gmail.com>
2022-11-13 12:22:01 +0000

    case splitter

---
+++ Fundamentals/Text Processing/Pascal-CaseSplitter.js
@@ -0,0 +1,18 @@
+function pascalCaseSplitter(text){
+
+
+    let result = text[0]
+    let lower = text.toLowerCase()
+
+    for (let i = 1; i < text.length; i++){
+    let currentLetter =  text[i]
+    
+    if(text[i] !== lower[i]){
+        result += ", "
+    }
+        result += text[i]
+    }
+    console.log(result);
+}
+
+pascalCaseSplitter('SplitMeIfYouCanHaHaYouCantOrYouCan')
