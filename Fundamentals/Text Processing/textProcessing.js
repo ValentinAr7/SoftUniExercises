c790ff653ce416677ba52c9f3bdb5e102ebb2f5c
Valentin <valio990210@gmail.com>
2022-11-13 09:40:00 +0000

    replace words and console

--- Fundamentals/Text Processing/textProcessing.js
+++ Fundamentals/Text Processing/textProcessing.js
@@ -1,13 +1,13 @@
 function textProcessing (words, text){
 
-    let wordsArray = words.shift().split(", ")
+    let wordsArray = words.split(", ")
 
     wordsArray.forEach(word => {
         const match = "*".repeat(word.length)
-        text = text.replace(match, word)
+        text = text.replace(match, `${word}`)
     });
-
+    console.log(text);
 }
-textProcessing(['great, learning',
+textProcessing('great, learning',
 
-'softuni is ***** place for ******** new programming languages'])
+'softuni is ***** place for ******** new programming languages')
