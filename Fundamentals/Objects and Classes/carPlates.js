8c6ae90d42d5738c0ac2c73c36eb5cb0ff0f2aea
Valentin <valio990210@gmail.com>
2022-11-14 18:16:16 +0000

    car plates check

---
+++ Fundamentals/Objects and Classes/carPlates.js
@@ -0,0 +1,23 @@
+function carPlates (input){
+
+    let parkingList = new Map
+
+    let fullList = input.split(" ")
+    console.log(fullList);
+
+}
+
+carPlates([
+
+'IN, CA2844AA',
+'IN, CA1234TA',
+'OUT, CA2844AA',
+'IN, CA9999TT',
+'IN, CA2866HI',
+'OUT, CA1234TA',
+'IN, CA2844AA',
+'OUT, CA2866HI',
+'IN, CA9876HH',
+'IN, CA2822UU'
+
+])
