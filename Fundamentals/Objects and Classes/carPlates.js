5d4fccc92ce46a8c7ee41f4dee7f3d931e6e4dc1
Valentin <valio990210@gmail.com>
2022-11-15 18:15:41 +0000

    formatting

--- Fundamentals/Objects and Classes/carPlates.js
+++ Fundamentals/Objects and Classes/carPlates.js
@@ -1,4 +1,4 @@
-function carPlates (input){
+function carPlates(input) {
 
     let parkingList = new Map
 
@@ -9,33 +9,32 @@ function carPlates (input){
         let command = element[0]
         let numberPlate = element[1]
 
-        if(command === "IN"){
+        if (command === "IN") {
             parkingList.set(numberPlate, command)
-        } else if( command === "OUT"){
+        } else if (command === "OUT") {
             parkingList.delete(numberPlate)
         }
     });
 
-    let sortedCarNumbers = Array.from(parkingList).sort((a,b)=> a[0].localeCompare(b[0]))
-    //return an array from arrays (matrix)
-    // sort by letters
+    let sortedCarNumbers = Array.from(parkingList).sort((a, b) => a[0].localeCompare(b[0]))
 
-    if(sortedCarNumbers.length === 0){
+
+    if (sortedCarNumbers.length === 0) {
         console.log("Parking is empty");
-    } 
+    }
     sortedCarNumbers.forEach((numberPlate) => console.log(numberPlate[0]))
 }
 carPlates([
 
-'IN, CA2844AA',
-'IN, CA1234TA',
-'OUT, CA2844AA',
-'IN, CA9999TT',
-'IN, CA2866HI',
-'OUT, CA1234TA',
-'IN, CA2844AA',
-'OUT, CA2866HI',
-'IN, CA9876HH',
-'IN, CA2822UU'
+    'IN, CA2844AA',
+    'IN, CA1234TA',
+    'OUT, CA2844AA',
+    'IN, CA9999TT',
+    'IN, CA2866HI',
+    'OUT, CA1234TA',
+    'IN, CA2844AA',
+    'OUT, CA2866HI',
+    'IN, CA9876HH',
+    'IN, CA2822UU'
 
 ])
