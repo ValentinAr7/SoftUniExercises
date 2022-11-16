148b057adb586c3098b9c1e748d8d380010361d1
Valentin <valio990210@gmail.com>
2022-11-16 18:32:27 +0000

    while

--- Fundamentals/Exam Preparation/imitationGame.js
+++ Fundamentals/Exam Preparation/imitationGame.js
@@ -1,5 +1,19 @@
 function imitationGame (input){
 
+    let encryptedMessage = input.shift()
+
+    let index = 0
+    let line = input[index]
+    index++
+
+    while(line !== "Decode"){
+
+        let line = input[index]
+        index++
+
+    }
+
+    while
 
 }
 
