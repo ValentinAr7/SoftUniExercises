b9a326f660e2dcd7d793aafacc6ad9652e912c52
Valentin <valio990210@gmail.com>
2022-11-16 18:51:41 +0000

    commands

--- Fundamentals/Exam Preparation/imitationGame.js
+++ Fundamentals/Exam Preparation/imitationGame.js
@@ -6,14 +6,19 @@ function imitationGame (input){
     let line = input[index]
     index++
 
+    let lineArguments = line.split("|")
+    let command = lineArguments[0]
+    console.log(lineArguments);
+
     while(line !== "Decode"){
+
 
         let line = input[index]
         index++
 
     }
 
-    while
+
 
 }
 
