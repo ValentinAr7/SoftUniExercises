df24c23a765f166e2789e2c1965f0a6eb62620d1
Valentin <valio990210@gmail.com>
2022-11-19 17:50:02 +0000

    second group in regexp

--- Fundamentals/Regular Expressions/matchDates.js
+++ Fundamentals/Regular Expressions/matchDates.js
@@ -2,8 +2,8 @@ function matchDates (input){
 
     let text = input.shift()
 
-    let pattern = /\b\d{2}([\/.-])[A-Z][a-z]+\1\d+/
+    let pattern = /\b(\d{2})([\/.-])[A-Z][a-z]+\2\d+/
 
-        
+
 }
 matchDates("01/Jan-1951, 23/sept/1973, 1/Feb/2016")
