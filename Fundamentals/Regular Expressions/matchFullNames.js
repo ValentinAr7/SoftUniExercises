f2a9272aa9a2f0f91aebf858ff6bccf9d67bebf7
Valentin <valio990210@gmail.com>
2022-11-19 16:40:02 +0000

    match names

--- Fundamentals/Regular Expressions/matchFullNames.js
+++ Fundamentals/Regular Expressions/matchFullNames.js
@@ -1,7 +1,9 @@
-function fullnames (){
+function fullnames (text){
 
+let pattern = /\b[A-Z][a-z]+ [A-Z][a-z]+\b/g;
 
-
+let names = text.match(pattern)
+console.log(names.join(" "));
 }
 
 fullnames(["ivan ivanov, Ivan ivanov, ivan Ivanov, IVan Ivanov, Ivan IvAnov, Ivan Ivanov"])
