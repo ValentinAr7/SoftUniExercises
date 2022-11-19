9f096a485f9a686f4ba77b8382f172d67e773b87
Valentin <valio990210@gmail.com>
2022-11-19 18:01:47 +0000

    print

--- Fundamentals/Regular Expressions/matchDates.js
+++ Fundamentals/Regular Expressions/matchDates.js
@@ -2,8 +2,13 @@ function matchDates (input){
 
     let text = input.shift()
 
-    let pattern = /\b(\d{2})([\/.-])([A-Z][a-z]{2})\2(\d{4})/
+    let pattern = /\b(?<day>\d{2})([/.-])(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})/g
 
+    let matches = text.matchAll(pattern)
+
+    for(const match of matches){
+        console.log(`Day: ${match.groups.day}, Month: ${match.groups.month}, Year: ${match.groupd.year}`);
+    }
 
 }
-matchDates("01/Jan-1951, 23/sept/1973, 1/Feb/2016")
+matchDates(["01/Jan-1951, 23/sept/1973, 1/Feb/2016"])
