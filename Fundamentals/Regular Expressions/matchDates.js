3ee9e707701e8e8b700c937126531e4cbc69401c
Valentin <valio990210@gmail.com>
2022-11-19 17:48:36 +0000

    dates

--- Fundamentals/Regular Expressions/matchDates.js
+++ Fundamentals/Regular Expressions/matchDates.js
@@ -1,6 +1,9 @@
 function matchDates (input){
 
+    let text = input.shift()
 
+    let pattern = /\b\d{2}([\/.-])[A-Z][a-z]+\1\d+/
 
+        
 }
 matchDates("01/Jan-1951, 23/sept/1973, 1/Feb/2016")
