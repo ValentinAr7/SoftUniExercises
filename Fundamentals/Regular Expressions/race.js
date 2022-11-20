28c1e9a721c6054372bce3c78ef1ffbd0d3c59dd
Valentin <valio990210@gmail.com>
2022-11-20 11:48:52 +0000

    race data input

---
+++ Fundamentals/Regular Expressions/race.js
@@ -0,0 +1,13 @@
+function race(radeDataArray){
+
+
+
+}
+
+race (['G4e@55or%6g6!68e!!@ ',
+        'R1@!3a$y4456@',
+        'B5@i@#123ll',
+        'G@e54o$r6ge#',
+        '7P%et^#e5346r',
+        'T$o553m&6'
+])
