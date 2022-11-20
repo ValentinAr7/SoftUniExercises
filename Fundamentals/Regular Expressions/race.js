5492da72e30902cfe923b643fb4e8a85b729ac3a
Valentin <valio990210@gmail.com>
2022-11-20 13:47:52 +0000

    formatting

--- Fundamentals/Regular Expressions/race.js
+++ Fundamentals/Regular Expressions/race.js
@@ -1,6 +1,6 @@
-function race(raceDataArray){
+function race(raceDataArray) {
 
-    let racersArray = raceDataArray.shift().split(", ") 
+    let racersArray = raceDataArray.shift().split(", ")
     let raceObject = {}
 
     const patternName = /[A-Za-z]+/g
@@ -8,21 +8,21 @@ function race(raceDataArray){
 
     let command = raceDataArray.shift()
 
-    while(command !== 'end of race' ){
+    while (command !== 'end of race') {
         const currentName = command.match(patternName).join("")
 
         let currentDistance = command.match(patterDistance).join("")
 
-        if (racersArray.includes(currentName)){
+        if (racersArray.includes(currentName)) {
             let distance = 0
 
             currentDistance.forEach(digit => {
                 distance += Number(digit)
             });
 
-            if(!raceObject.hasOwnProperty(currentName)){
+            if (!raceObject.hasOwnProperty(currentName)) {
                 raceObject[currentName] = distance;
-            } else{
+            } else {
                 raceObject[currentName] += distance
             }
         }
@@ -32,11 +32,11 @@ function race(raceDataArray){
     }
 }
 
-race (['G4e@55or%6g6!68e!!@ ',
-        'R1@!3a$y4456@',
-        'B5@i@#123ll',
-        'G@e54o$r6ge#',
-        '7P%et^#e5346r',
-        'T$o553m&6',
-        'end of race'
+race(['G4e@55or%6g6!68e!!@ ',
+    'R1@!3a$y4456@',
+    'B5@i@#123ll',
+    'G@e54o$r6ge#',
+    '7P%et^#e5346r',
+    'T$o553m&6',
+    'end of race'
 ])
