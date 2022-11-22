b9b0b5753b6b8a38d7f6916009cbb2b0f455e0e7
Valentin <valio990210@gmail.com>
2022-11-22 18:17:48 +0000

    repeat until command = travel

--- Fundamentals/Final Exam/worldTour.js
+++ Fundamentals/Final Exam/worldTour.js
@@ -1,7 +1,19 @@
-function worldTour (){
+function worldTour (input){
 
 
+    let wrldTour = input.shift()
+    let index = 0
+    let command = input[index]
+    
+    while (command !== "Travel"){
+
+        index++
+    }
 
 }
 
-worldTour([])
+worldTour(["Hawai::Cyprys-Greece",
+    "Add Stop:7:Rome",
+    "Remove Stop:11:16",
+    "Switch:Hawai:Bulgaria",
+    "Travel"])
