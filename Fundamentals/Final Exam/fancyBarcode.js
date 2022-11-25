f80a8e2139c56ed2a07634ac371e791a961b157a
Valentin <valio990210@gmail.com>
2022-11-25 18:46:38 +0000

    for and while loop

--- Fundamentals/Final Exam/fancyBarcode.js
+++ Fundamentals/Final Exam/fancyBarcode.js
@@ -1,9 +1,17 @@
 function fancyBarcode (input){
 
-    let numberOfBarcodes = input.shift()
+    let n = input.shift()
 
     let pattern = /(@#{1,})([A-Z][A-Za-z0-9]{4,}[A-Z])(@#{1, })/gm
 
+    for(let i = 0; i <n; i ++){
+        let barcode = input[i]
+
+        let match = pattern.exec(barcode)
+        while(match !== null){
+            match = pattern.exec(barcode)
+    }
+    }
     let barcodes = input.match(pattern)
     console.log(barcodes.join(" "));
 }
