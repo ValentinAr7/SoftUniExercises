acaba3696698d46f8dbf01be520581ed885a8eda
Valentin <valio990210@gmail.com>
2022-11-25 18:01:18 +0000

    regex

--- Fundamentals/Final Exam/fancyBarcode.js
+++ Fundamentals/Final Exam/fancyBarcode.js
@@ -1,7 +1,11 @@
 function fancyBarcode (input){
 
+    let numberOfBarcodes = input.shift()
 
+    let pattern = /@#+[A-Z][A-Za-z0-9]+@#+/gm
 
+    let barcodes = input.match(pattern)
+    console.log(barcodes.join(" "));
 }
 
 fancyBarcode(["3",
