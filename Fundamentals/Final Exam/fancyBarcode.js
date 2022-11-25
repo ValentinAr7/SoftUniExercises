8985ae287148f9004fb91e22fb6b4187b4014939
Valentin <valio990210@gmail.com>
2022-11-25 19:13:19 +0000

    loop through the barcode characters

--- Fundamentals/Final Exam/fancyBarcode.js
+++ Fundamentals/Final Exam/fancyBarcode.js
@@ -2,15 +2,21 @@ function fancyBarcode (input){
 
     let n = Number(input.shift())
 
-    let pattern = /(@#{1,})([A-Z][A-Za-z0-9]{4,}[A-Z])(@#{1, })/g
+    let pattern = /(@#{1,})([A-Z][A-Za-z0-9]{4,}[A-Z])(@#{1,})/g
 
     for(let i = 0; i <n; i ++){
         let barcode = input[i]
 
         let match = pattern.exec(barcode)
+        let concatenatedDigit = "";
+        let isValid = false
         while(match !== null){
             console.log(match[2]);
-            match = pattern.exec(barcode)
+            let barcodeText = match[2]
+
+            for(let ch of barcodeText){
+                
+            }
     }
     }
 
