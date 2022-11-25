902d2248bf4b922ec689cb52ca36e84bfbf2b0fd
Valentin <valio990210@gmail.com>
2022-11-25 18:48:39 +0000

    pattern

--- Fundamentals/Final Exam/fancyBarcode.js
+++ Fundamentals/Final Exam/fancyBarcode.js
@@ -1,19 +1,19 @@
 function fancyBarcode (input){
 
-    let n = input.shift()
+    let n = Number(input.shift())
 
-    let pattern = /(@#{1,})([A-Z][A-Za-z0-9]{4,}[A-Z])(@#{1, })/gm
+    let pattern = /(@#{1,})([A-Z][A-Za-z0-9]{4,}[A-Z])(@#{1, })/g
 
     for(let i = 0; i <n; i ++){
         let barcode = input[i]
 
         let match = pattern.exec(barcode)
         while(match !== null){
+            console.log(match[2]);
             match = pattern.exec(barcode)
     }
     }
-    let barcodes = input.match(pattern)
-    console.log(barcodes.join(" "));
+
 }
 
 fancyBarcode(["3",
