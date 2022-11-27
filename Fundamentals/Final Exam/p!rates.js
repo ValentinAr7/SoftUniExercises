1f3c469d2a5142867bc1e72b158176738073e208
Valentin <valio990210@gmail.com>
2022-11-27 16:54:13 +0000

    comments

--- Fundamentals/Final Exam/p!rates.js
+++ Fundamentals/Final Exam/p!rates.js
@@ -11,20 +11,25 @@ function pirates (input){
         let population = Number(splitCommand[1])
         let gold = Number(splitCommand[2])
 
-        let result = array.find((x)=>x.city === city)
-        let index = result.indexOf(result)
+        let result = array.find((x) => x.city === city)
+        // check if array contains repeating cities
 
-        if(result == true){
+        let index = array.indexOf(result)
+        // find the index of the repeating city
+
+        if(result){
             array[index].population += population
             array[index].gold += gold
+        //if the city repeat add up the gold and population
+        } else {
+            cities = {city, population, gold}
+            array.push(cities)
+        //else - make an object with the 3 elements
         }
-
-    
-        });
 
         command = input.shift()
     }
-
+    array.forEach(e=>console.log(e))
 }
 
 pirates (["Tortuga||345000||1250",
