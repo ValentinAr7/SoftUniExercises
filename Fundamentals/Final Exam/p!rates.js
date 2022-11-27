59c45b935e03bb3905242ceaab624b8bb668f1d2
Valentin <valio990210@gmail.com>
2022-11-27 16:39:05 +0000

    additional arrays

--- Fundamentals/Final Exam/p!rates.js
+++ Fundamentals/Final Exam/p!rates.js
@@ -1,19 +1,20 @@
 function pirates (input){
 
     let command = input.shift()
-    
+    let cities = {}
+    let array = []
 
     while(command !== "Sail"){
         let splitCommand = command.split("||")
-        
-        let cities = splitCommand[0]
+
+        let city = splitCommand[0]
         let population = splitCommand[1]
         let gold = splitCommand[2]
-        console.log(cities);
 
-        for(let i = 0; i < cities; i++){
-            console.log(cities);
-        }
+        let repeeatingCity = 0
+
+    
+        });
 
         command = input.shift()
     }
@@ -23,7 +24,8 @@ function pirates (input){
 pirates (["Tortuga||345000||1250",
 
 "Santo Domingo||240000||630",
-"Havana||410000||1100",
+"Tortuga||410000||1100", 
+"Tortuga||410000||1100",         //Havana
 "Sail",
 "Plunder=>Tortuga=>75000=>380",
 "Prosper=>Santo Domingo=>180",
