0fc27d16c1d6cf7e15e1a293d2a23675d28f4ff2
Valentin <valio990210@gmail.com>
2022-11-27 11:55:17 +0000

    concatenation

--- Fundamentals/Final Exam/theImitationGame.js
+++ Fundamentals/Final Exam/theImitationGame.js
@@ -5,7 +5,37 @@ function imitationGame (input){
         let command = input.shift()
 
         while(command !== "Decode"){
-            console.log(command);
+            let line = command.split("|")
+            let operation = line[0]
+            let index1;
+            let index2;
+
+            switch (operation) {
+                case "ChangeAll":
+                    index1 = line[1]
+                    index2 = line[2]
+
+                    for(let i = 0; i < encryptedMessage.length; i++){
+                        if(encryptedMessage.includes(index1)){
+                            encryptedMessage = encryptedMessage.replace(index1, index2)
+                        }
+                    }
+                    console.log(encryptedMessage);
+                    break;
+
+                case "Insert":
+                    index1 = line[1]
+                    index2 = line[2]
+
+                    let firstPart = encryptedMessage.slice(0, index1)
+                    let secondPart = encryptedMessage.slice(index1)
+                    let message = firstPart + index2 + secondPart
+                    console.log(message);
+                    break;
+            
+                default:
+                    break;
+            }
 
             command = input.shift()
         }
