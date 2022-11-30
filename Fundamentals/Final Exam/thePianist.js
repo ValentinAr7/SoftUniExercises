01f6600a73e048dd906abf4a4080481f90ab8cc9
Valentin <valio990210@gmail.com>
2022-11-30 18:22:37 +0000

    change keys

--- Fundamentals/Final Exam/thePianist.js
+++ Fundamentals/Final Exam/thePianist.js
@@ -1,22 +1,22 @@
-function thePianist (input){
+function thePianist(input) {
 
     let numberOfPieces = Number(input.shift())
     let store = {}
 
-    for(let i = 0; i < numberOfPieces; i++){
-        
+    for (let i = 0; i < numberOfPieces; i++) {
+
         let [piece, composer, key] = input.shift().split("|")
 
-        if(!store.hasOwnProperty(piece)){
+        if (!store.hasOwnProperty(piece)) {
             store[piece] = {}
         }
-        store[piece] [composer]= key 
+        store[piece][composer] = key
     }
     console.table(store);
 
     let line = input.shift().split("|")
 
-    while (line !== "Stop"){
+    while (line !== "Stop") {
         let command = line[0]
         let piece = line[1]
         let composer = line[2]
@@ -24,53 +24,57 @@ function thePianist (input){
 
         switch (command) {
             case "Add":
-            if(store.hasOwnProperty(piece)){
-                console.log(`${piece} is already in the collection!`);
+                if (store.hasOwnProperty(piece)) {
+                    console.log(`${piece} is already in the collection!`);
+                    break;
+                }
+                store[piece] = {}
+                store[piece][composer] = key
+                console.log(`${piece} by ${composer} in ${key} addede to the collection`);
                 break;
-            }
-
-            store[piece] = {}
-            store[piece][composer] = key
-
-            console.log(`${piece} by ${composer} in ${key} addede to the collection`);
-            break;
-        
 
             case "Remove":
-                if(!store.hasOwnProperty(piece)){
+                if (!store.hasOwnProperty(piece)) {
                     console.log(`Invalid operation! ${piece} does not exist in the collection`);
+                    break
                 }
+                delete store[piece]
+                console.log(`Successfuly removed ${piece}`);
+                break;
+
+
+            case "ChangeKey":
+                if(!store.hasOwnProperty(piece)){
+                    console.log(`Invalid operation! ${piece} does not exist in the collection.`);
+                }
+
+            let newKey = composer;
+
+            for(let [keyComposer, valueKey] of Object.entries(store[piece])){
+                store[piece][keyComposer] = newKey
+            }
             break;
         }
-            delete store[piece]
-            console.log(`Successfuly removed ${piece}`);
-    
+        line = input.shift()
     }
 }
 
-thePianist (
+thePianist(
     [
         '3',
-        
+
         'Fur Elise|Beethoven|A Minor',
-        
         'Moonlight Sonata|Beethoven|C# Minor',
-        
         'Clair de Lune|Debussy|C# Minor',
-        
         'Add|Sonata No.2|Chopin|B Minor',
-        
         'Add|Hungarian Rhapsody No.2|Liszt|C# Minor',
-        
         'Add|Fur Elise|Beethoven|C# Minor',
-        
         'Remove|Clair de Lune',
-        
         'ChangeKey|Moonlight Sonata|C# Major',
-        
+
         'Stop'
-        
-        ]
+
+    ]
 )
 
 
