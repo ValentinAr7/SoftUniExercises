d0eb9a0f48530c466c2534997e4ffa1cc4a98d08
Valentin <valio990210@gmail.com>
2022-12-03 11:15:02 +0000

    switch case

--- Fundamentals/Final Exam/secreetChat.js
+++ Fundamentals/Final Exam/secreetChat.js
@@ -5,6 +5,23 @@ function secretChat (input){
     let line = input.shift()
     while(line !== 'Reveal'){
         let splitLine = line.split(':|:')
+        let command = splitLine[0]
+        let index1 = splitLine [1]
+        let index2 = splitLine [2]
+
+        switch (command) {
+            case 'ChangeAll':
+                
+                break;
+        
+            case 'Reverse':
+            
+                break;
+
+            case 'InsertSpace':
+
+            break;
+        }
         line = input.shift()
     }
 
