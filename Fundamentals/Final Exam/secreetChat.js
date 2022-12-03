23c005c07422c6ce2cd2499fd08031ebc3dfdef0
Valentin <valio990210@gmail.com>
2022-12-03 11:10:45 +0000

    secret chat

---
+++ Fundamentals/Final Exam/secreetChat.js
@@ -0,0 +1,13 @@
+function secretChat (input){
+
+    let message = input.shift()
+
+    
+
+}
+secretChat (["heVVodar!gniV",
+        'ChangeAll:|:V:|:l',
+        'Reverse:|:!gnil',
+        'InsertSpace:|:5',
+        ' Reveal'
+    ])
