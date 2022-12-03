1708204f1e306572e6c44577114c1d5b34a1853f
Valentin <valio990210@gmail.com>
2022-12-03 11:13:25 +0000

    split commands

--- Fundamentals/Final Exam/secreetChat.js
+++ Fundamentals/Final Exam/secreetChat.js
@@ -2,12 +2,17 @@ function secretChat (input){
 
     let message = input.shift()
 
-    
+    let line = input.shift()
+    while(line !== 'Reveal'){
+        let splitLine = line.split(':|:')
+        line = input.shift()
+    }
+
 
 }
 secretChat (["heVVodar!gniV",
         'ChangeAll:|:V:|:l',
         'Reverse:|:!gnil',
         'InsertSpace:|:5',
-        ' Reveal'
+        'Reveal'
     ])
