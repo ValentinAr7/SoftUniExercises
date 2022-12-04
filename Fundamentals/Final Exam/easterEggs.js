2b84f3eccb0dc74e3e55e5661e8aa432d01be32d
Valentin <valio990210@gmail.com>
2022-12-04 11:17:06 +0000

    easter eggs

---
+++ Fundamentals/Final Exam/easterEggs.js
@@ -0,0 +1,15 @@
+function easterEggs (input){
+
+
+    // [@|#]+(?<color>[a-z]{3,}+)[@|#]+[\W\D]+[\/]+(?<number>[0-9]+)[\/]+
+}
+
+easterEggs(
+'@@@@green@*/10/',
+'@yel0w@*26*',
+'#red#####//8//',
+'@limon*@*23*',
+'@@@red#*/%^&/6/',
+'@gree_een@/notnumber/',
+'###purple@@@@@*$%^&*/5/'
+)
