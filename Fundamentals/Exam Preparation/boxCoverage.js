55a60d117aee12d53b43254a932e3dc44647e222
Valentin <valio990210@gmail.com>
2022-12-08 08:37:30 +0000

    box cov

---
+++ Fundamentals/Exam Preparation/boxCoverage.js
@@ -0,0 +1,22 @@
+function boxGoc (input){
+
+    let sizeOfSide = input.shift()
+    let numberOfSheets = input.shift()
+
+    let squareArea = sizeOfSide * sizeOfSide* 6
+    
+
+    for(let i = 0; i < input.length; i++){
+
+        
+    }
+}
+
+
+boxGoc ([2,
+    2,
+    0.2,
+    7,
+    6,
+    8.5
+    ])
