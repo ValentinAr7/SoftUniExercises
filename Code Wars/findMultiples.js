40c8d0f449c7b1f1f23f4d5662cf7e988a8e1470
Valentin <valio990210@gmail.com>
2022-12-10 11:37:08 +0000

    find multiples of a number

---
+++ Code Wars/findMultiples.js
@@ -0,0 +1,13 @@
+function findMultiples(integer, limit) {
+    
+    let result = []
+    for(let i = integer; i <= limit; i += integer){
+    
+    result.push(i)
+    
+    }
+    return result
+
+  }
+
+  findMultiples(5, 25)
