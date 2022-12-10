d9f2d032aa1cbfc0938cb93c326880287554b8a8
Valentin <valio990210@gmail.com>
2022-12-10 11:04:25 +0000

    find the ball in the cup with for loop

--- Fundamentals/Text Processing/Code Wars/ballAndCups.js
+++ Fundamentals/Text Processing/Code Wars/ballAndCups.js
@@ -1,7 +1,11 @@
 function cupAndBalls(b, arr){
-
-
-
-};
+    
+        for (var i=0; i<arr.length; ++i)
+          if (arr[i][0]==b)
+            b=arr[i][1];
+          else if (arr[i][1]==b)
+            b=arr[i][0];
+        console.log(b);
+      };
 
 cupAndBalls (1, [[2,3],[1,2],[1,2]])
