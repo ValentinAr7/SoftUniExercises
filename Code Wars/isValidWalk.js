0f2da5c043718dd74cff68c7c8fbe255dace4080
Valentin <valio990210@gmail.com>
2022-12-11 11:33:36 +0000

    return to begining

--- Code Wars/isValidWalk.js
+++ Code Wars/isValidWalk.js
@@ -1,7 +1,22 @@
 function isValidWalk(walk) {
 
+let isValid = false
+let meterBlock = 0
+let startingPoint = walk[0]
 
+for(let i = 0; i < walk.length; i++){
+    let currentStep = walk[i]
+    
+    if(i == 9){
+        
+        
 
+        isValid = true
+    } else {
+        isValid = false
+    }
+}
+console.log(isValid);
 }
 
 isValidWalk(['n','s','n','s','n','s','n','s','n','s'])
