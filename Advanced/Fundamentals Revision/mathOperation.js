633077ec34e8040dd3355ce6865e24f659076666
ValentinAr7 <valio990210@gmail.com>
2022-12-18 13:46:21 +0000

    input

--- Advanced/Fundamentals Revision/mathOperation.js
+++ Advanced/Fundamentals Revision/mathOperation.js
@@ -3,12 +3,33 @@ function mathOperation (input) {
     let operation = input.pop()
     let num1 = input[0]
     let num2 = input[1]
+    let result = 0
 
     switch (operation){
         case '+':
+            result = num1 + num2
+            console.log(result);
+        break;
+
+        case '*':
+            result = num1 * num2
+            console.log(result);
+
+        break;
+
+        case '-':
+        result = num1 - num2
+        console.log(result);
+
+        break;
+
+        case '/':
+            result = num1 / num2
+            console.log(result);
+            break;
 
     }
 
 }
 
-mathOperation ([5, 6, '+'])
+mathOperation ([3, 5.5, '*'])
