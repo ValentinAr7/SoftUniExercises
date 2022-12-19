eda9d5313db247b8b3ebeb0e74a3422a57dc440b
ValentinAr7 <valio990210@gmail.com>
2022-12-19 19:46:15 +0000

    city speed limit

--- Advanced/Fundamentals Revision/roadRadar.js
+++ Advanced/Fundamentals Revision/roadRadar.js
@@ -2,17 +2,39 @@ function roadRadar (speed, area){
 
     switch (area) {
         case 'motorway':
-            let speedLimit = 130
+            let motorwaySpeedLimit = 130
 
-
-            if(speed > speedLimit){
-                console.log(`The speed is ${speed - speedLimit} km/h faster than the allowed speed of ${speedLimit} - {status}`);
+            if(speed > motorwaySpeedLimit){
+                console.log(`The speed is ${speed - motorwaySpeedLimit} km/h faster than the allowed speed of ${motorwaySpeedLimit} - {status}`);
             }
             console.log(`Driving ${speed} km/h in a {speed limit} zone`);
             break;
+
+
+            case 'interstate':
+                let InterstatespeedLimit = 90
     
+    
+                if(speed > InterstatespeedLimit){
+                    console.log(`The speed is ${speed - InterstatespeedLimit} km/h faster than the allowed speed of ${InterstatespeedLimit} - {status}`);
+                }
+                console.log(`Driving ${speed} km/h in a {speed limit} zone`);
+                break;
 
             
+                case 'city':
+                    let citySpeedLimit = 50
+        
+        
+                    if(speed > InterstatespeedLimit){
+                        console.log(`The speed is ${speed - InterstatespeedLimit} km/h faster than the allowed speed of ${InterstatespeedLimit} - {status}`);
+                    }
+                    console.log(`Driving ${speed} km/h in a {speed limit} zone`);
+                    break;
+    
+    
+
+
     }
 
 }
