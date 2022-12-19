2e2b10c2a32604b95ef5e156ca0b6002ba8a83a2
ValentinAr7 <valio990210@gmail.com>
2022-12-19 19:53:05 +0000

    speeding limits

--- Advanced/Fundamentals Revision/roadRadar.js
+++ Advanced/Fundamentals Revision/roadRadar.js
@@ -22,15 +22,32 @@ function roadRadar (speed, area){
                 break;
 
             
+
+
                 case 'city':
                     let citySpeedLimit = 50
         
-        
                     if(speed > citySpeedLimit){
+                        let status = ""
+
+                        if ((speed - citySpeedLimit > 20)){
+                            status = "speeding"
+                        } else if ((speed - citySpeedLimit > 40)){
+                            status = "excessive speeding"
+                        } else {
+                            status = 'reckless speeding'
+                        }
                         console.log(`The speed is ${speed - citySpeedLimit} km/h faster than the allowed speed of ${citySpeedLimit} - {status}`);
                     }
                     console.log(`Driving ${speed} km/h in a {speed limit} zone`);
                     break;
+
+
+
+
+
+
+                    
 
                     case 'residential':
                         let residentialSpeedLimit = 50
