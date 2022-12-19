b8fae8f2512ff6662dfc352dddd0f66b54a818ad
ValentinAr7 <valio990210@gmail.com>
2022-12-19 19:44:04 +0000

    motorway case

--- Advanced/Fundamentals Revision/roadRadar.js
+++ Advanced/Fundamentals Revision/roadRadar.js
@@ -1,5 +1,19 @@
 function roadRadar (speed, area){
 
+    switch (area) {
+        case 'motorway':
+            let speedLimit = 130
+
+
+            if(speed > speedLimit){
+                console.log(`The speed is ${speed - speedLimit} km/h faster than the allowed speed of ${speedLimit} - {status}`);
+            }
+            console.log(`Driving ${speed} km/h in a {speed limit} zone`);
+            break;
+    
+
+            
+    }
 
 }
 
