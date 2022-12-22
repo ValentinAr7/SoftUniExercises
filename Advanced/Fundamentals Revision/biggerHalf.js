ce48d3afc76c3ab7bccc980208065937e0d03eef
ValentinAr7 <valio990210@gmail.com>
2022-12-22 19:35:17 +0000

    splice the array

---
+++ Advanced/Fundamentals Revision/biggerHalf.js
@@ -0,0 +1,12 @@
+function biggerHalf (input){
+
+let newArray = input.sort((a,b) => a - b)
+
+let halfArray = newArray.splice(newArray.length / 2)
+
+
+
+
+}
+
+biggerHalf ([3, 19, 14, 7, 2, 19, 6])
