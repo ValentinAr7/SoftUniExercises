638a758696aad5e069d6229c3fa7c66fc1b5bd71
ValentinAr7 <valio990210@gmail.com>
2022-12-23 18:34:51 +0000

    input

--- Advanced/Fundamentals Revision/biggerHalf.js
+++ Advanced/Fundamentals Revision/biggerHalf.js
@@ -5,8 +5,13 @@ let newArray = input.sort((a,b) => a - b)
 let halfArray = newArray.splice(newArray.length / 2)
 
 
+if (halfArray.length > newArray.length){
 
+    console.log(halfArray);
+} else {
+    console.log(halfArray);
+}
 
 }
 
-biggerHalf ([3, 19, 14, 7, 2, 19, 6])
+biggerHalf ([3, 19, 14, 7, 2, 19, 6, 19])
