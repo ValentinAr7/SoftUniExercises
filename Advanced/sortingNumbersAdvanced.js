34f37c4d8c884a5ec99a3dd5c120cf98a4ba7709
ValentinAr7 <valio990210@gmail.com>
2022-12-24 15:48:09 +0000

    cut array in two

--- Advanced/sortingNumbersAdvanced.js
+++ Advanced/sortingNumbersAdvanced.js
@@ -2,6 +2,12 @@ function sortingNums(input) {
 
 let result = []
 
+let orderNumbers = input.sort((a,b) => a- b )
+let halfArray = input.splice(0, input.length / 2)
+
+console.log(halfArray);
+console.log(input);
+
 }
 
 sortingNums ([1, 65, 3, 52, 48, 63, 31, -3, 18, 56])
