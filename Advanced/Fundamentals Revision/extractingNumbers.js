517695ad9968d97d3777d5fc81f40eaa61cecc05
ValentinAr7 <valio990210@gmail.com>
2022-12-24 10:24:00 +0000

    connecting the numbers

--- Advanced/Fundamentals Revision/extractingNumbers.js
+++ Advanced/Fundamentals Revision/extractingNumbers.js
@@ -1,11 +1,23 @@
 function extractingNumber (array){
 
     let res = []
-    
+    firstNum = array.shift()
+    let currentBiggestNum = 0
+
+    for (let i = 0; i < array.length; i++){
+        let currentNumber = array[i]
+        if(currentNumber > currentBiggestNum){
+            res.push(currentNumber)
+            currentBiggestNum = currentNumber
+        }
+    }
+console.log(firstNum  + ' ' + res);
 
 }
 
 extractingNumber ([1,
+
+
     3,
     8,
     4,
