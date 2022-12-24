d51d84c0da6ad2021409a2aa019dd4a1a1ab424c
ValentinAr7 <valio990210@gmail.com>
2022-12-24 16:13:21 +0000

    order 2 arrays in 1

--- Advanced/sortingNumbersAdvanced.js
+++ Advanced/sortingNumbersAdvanced.js
@@ -5,6 +5,18 @@ let result = []
 let orderNumbers = input.sort((a,b) => a- b )
 let halfArray = input.splice(0, input.length / 2)
 
+for(let i = 0; i <= halfArray.length; i++){
+    result.push(halfArray[i])
+
+    for(let i =0; i < input.length; i++){
+        result.push(input[i])
+    }
+}
+
+console.log(result);
+
+
+
 console.log(halfArray);
 console.log(input);
 
