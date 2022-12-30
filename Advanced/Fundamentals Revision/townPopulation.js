bbce0334a61d880daadc5abb4645f8220dbba1bb
ValentinAr7 <valio990210@gmail.com>
2022-12-30 21:09:59 +0200

    town population

--- Advanced/Fundamentals Revision/townPopulation.js
+++ Advanced/Fundamentals Revision/townPopulation.js
@@ -10,15 +10,28 @@ let townData = array
         }
     })
 
-    console.log(townData[0]);
+    let registry = {}
 
+    for(let town of townData){
+        if(registry[town.name] === undefined){
+            registry[town.name] === town.population
+        } else {
+            registry[town.name] += town.population
+        }
+    }
+
+    return registry
 
 }
 
-townPopulation (
+let result = townPopulation (
 
 ['Sofia <-> 1200000',
 'Montana <-> 20000',
 'New York <-> 10000000',
 'Washington <-> 2345000',
 'Las Vegas <-> 1000000'])
+
+
+
+console.log(result);
