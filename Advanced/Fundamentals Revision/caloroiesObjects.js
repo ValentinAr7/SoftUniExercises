b68306ccef45ca4d335a20d9c20c61c54820f75a
Valentin <valio990210@gmail.com>
2023-01-06 18:22:07 +0000

    colories objects

---
+++ Advanced/Fundamentals Revision/caloroiesObjects.js
@@ -0,0 +1,15 @@
+function caloriesObects (data){
+
+    let result = {}
+
+    for(let i = 0; i < data.length; i +=2){
+        result[data[i]] = Number(data[i + 1])
+    }
+
+    console.log(result);
+
+}
+
+caloriesObects(['Yoghurt', '48', 'Rise', '138',
+
+'Apple', '52'])
