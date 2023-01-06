07b075af994421f531149ec7d00097093fb369d5
Valentin <valio990210@gmail.com>
2023-01-06 21:39:40 +0200

    construction crew task

--- Advanced/Fundamentals Revision/constructionCrew.js
+++ Advanced/Fundamentals Revision/constructionCrew.js
@@ -2,8 +2,21 @@ function constructionCrew (data){
 
 let worker = {}
 
+    worker.weight = {
+        weight: data.weight}
 
+    worker.experience = {Experiance: data.experience}
 
+    if(data.dizziness == true){
+        worker.water = {
+            levelOfHydrated: (0.1 * data.weight * data.experience) + data.levelOfHydrated
+        }
+        worker.dizziness = {
+            dizziness: false
+        }
+    } 
+
+console.table(worker);
 }
 
 constructionCrew ({ 
@@ -13,4 +26,13 @@ constructionCrew ({
     levelOfHydrated: 0,
     dizziness: true 
 
+})
+
+console.log("----------------");
+
+constructionCrew({ 
+    weight: 120,
+    experience: 20,
+    levelOfHydrated: 200,
+    dizziness: true 
 })
