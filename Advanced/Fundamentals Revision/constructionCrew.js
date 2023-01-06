49fc06a9d8f818378554d9d1b1b4b760eee8bd57
Valentin <valio990210@gmail.com>
2023-01-06 21:25:44 +0200

    worker properties

---
+++ Advanced/Fundamentals Revision/constructionCrew.js
@@ -0,0 +1,16 @@
+function constructionCrew (data){
+
+let worker = {}
+
+
+
+}
+
+constructionCrew ({ 
+    
+    weight: 80,
+    experience: 1,
+    levelOfHydrated: 0,
+    dizziness: true 
+
+})
