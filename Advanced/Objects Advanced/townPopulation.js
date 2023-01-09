acb4a43278c07ed7c5395c51bb74fb72157bba57
Valentin <valio990210@gmail.com>
2023-01-09 12:15:36 +0200

    population calc

--- Advanced/Objects Advanced/townPopulation.js
+++ Advanced/Objects Advanced/townPopulation.js
@@ -1,11 +1,29 @@
 function townPopulation (data){
 
+for(let i =0; i < data.length; i++){
+    let currentTown = data[i].split(" <-> ")
+    
+    let obj = {
+        town: currentTown[0],
+        population: currentTown[1]
+    }
+
+    if(obj.hasOwnProperty(town)){
+        obj.population += obj.population
+    }
+
+    console.log(obj);
+}
 
 }
 
-townPopulation ([
-'Sofia <-> 1200000',
-'Montana <-> 20000',
-'New York <-> 10000000',
-'Washington <-> 2345000',
-'Las Vegas <-> 1000000'])
+townPopulation (
+    ['Istanbul <-> 100000',
+
+    'Honk Kong <-> 2100004',
+    
+    'Jerusalem <-> 2352344',
+    
+    'Mexico City <-> 23401925',
+    
+    'Istanbul <-> 1000'])
