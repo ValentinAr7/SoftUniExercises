208963924f88ba3b3de4468276deedf03c2a8693
Valentin <valio990210@gmail.com>
2023-01-09 19:14:19 +0200

    formatting

--- Advanced/Objects Advanced/6-lowestPriceInCities.js
+++ Advanced/Objects Advanced/6-lowestPriceInCities.js
@@ -1,36 +1,36 @@
-function lowestPrice (input){
+function lowestPrice(input) {
 
     let products = []
 
-while(input.length > 0){
-    let [town, product, price] = input.shift().split(" | ")
+    while (input.length > 0) {
+        let [town, product, price] = input.shift().split(" | ")
 
-        if(products.filter(x => x.product === product).length > 0){
+        if (products.filter(x => x.product === product).length > 0) {
 
             let obj = products.find(x => x.product === product);
 
-        if(obj.price > Number(price)){
-            obj.price = Number(price);
-            obj.town = town
+            if (obj.price > Number(price)) {
+                obj.price = Number(price);
+                obj.town = town
+            }
+        } else {
+            let obj = { product, town, price: Number(price) };
+            products.push(obj);
         }
-    } else {
-        let obj = {product, town, price: Number(price)};
-        products.push(obj);
-    }
     }
 
-for(let product of products){
-    console.log(`${product.product} -> ${product.price} -> ${product.town}`);
-}
+    for (let product of products) {
+        console.log(`${product.product} -> ${product.price} -> ${product.town}`);
+    }
 
 }
 
-lowestPrice ([
-'Sample Town | Sample Product | 1000',
-'Sample Town | Orange | 2',
-'Sample Town | Peach | 1',
-'Sofia | Orange | 3',
-'Sofia | Peach | 2',
-'New York | Sample Product | 1000.1',
-'New York | Burger | 10'
+lowestPrice([
+    'Sample Town | Sample Product | 1000',
+    'Sample Town | Orange | 2',
+    'Sample Town | Peach | 1',
+    'Sofia | Orange | 3',
+    'Sofia | Peach | 2',
+    'New York | Sample Product | 1000.1',
+    'New York | Burger | 10'
 ])
