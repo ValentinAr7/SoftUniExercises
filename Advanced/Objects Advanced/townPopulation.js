2eb3364f4ec331eb11869bff10b0b82f69481b74
Valentin <valio990210@gmail.com>
2023-01-09 12:05:41 +0200

    town population

---
+++ Advanced/Objects Advanced/townPopulation.js
@@ -0,0 +1,11 @@
+function townPopulation (data){
+
+
+}
+
+townPopulation ([
+'Sofia <-> 1200000',
+'Montana <-> 20000',
+'New York <-> 10000000',
+'Washington <-> 2345000',
+'Las Vegas <-> 1000000'])
