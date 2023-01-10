8c72f624f2be37d41a876cb82385c1073e36b537
Valentin <valio990210@gmail.com>
2023-01-10 08:00:19 +0200

    get the initial first letter of product names

--- Advanced/Objects Advanced/7-storeCatalogue.js
+++ Advanced/Objects Advanced/7-storeCatalogue.js
@@ -3,6 +3,8 @@ function storeCatalogue(input) {
       // Iterate over the products and add them to the groupedProducts object
     for (const product of input) {
         let [productName, price] = product.split(" : ")
+
+      // Get the initial first letter of the product name  
         const initial = productName[0].toUpperCase()
 
         
