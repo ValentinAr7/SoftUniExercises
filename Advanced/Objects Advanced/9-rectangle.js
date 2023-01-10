36267dee7e9a1fbba3c02885bab538319105053c
Valentin <valio990210@gmail.com>
2023-01-10 15:17:22 +0200

    show rectangle dimensions

--- Advanced/Objects Advanced/9-rectangle.js
+++ Advanced/Objects Advanced/9-rectangle.js
@@ -9,3 +9,10 @@ function rectangle(width, height, color) {
     }
     return rect;
 }
+
+let rect = rectangle(4, 5, 'red');
+
+console.log(rect.width); // 4
+console.log(rect.height); // 5
+console.log(rect.color); // 'Red'
+console.log(rect.calcArea()); // 20
