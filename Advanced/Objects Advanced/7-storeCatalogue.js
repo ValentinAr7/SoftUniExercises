30bb0206fb98624781aad423607e30b611879b5a
Valentin <valio990210@gmail.com>
2023-01-10 08:24:32 +0200

    final task

--- Advanced/Objects Advanced/7-storeCatalogue.js
+++ Advanced/Objects Advanced/7-storeCatalogue.js
@@ -1,16 +1,40 @@
-function storeCatalogue(input) {
+function groupProducts(products) {
+    // Create an object to store the products grouped by their initial
+    const groupedProducts = {};
   
-      // Iterate over the products and add them to the groupedProducts object
-    for (const product of input) {
-        let [productName, price] = product.split(" : ")
-
-      // Get the initial first letter of the product name  
-        const initial = productName[0].toUpperCase()
-
-        
+    // Iterate over the products and add them to the groupedProducts object
+    products.forEach(product => {
+      // Extract the product name and price
+      const [name, price] = product.split(': ');
+      // Get the initial of the product name
+      const initial = name[0].toUpperCase();
+  
+      // If the initial is not a property of the groupedProducts object, create an empty array for it
+      if (!groupedProducts[initial]) {
+        groupedProducts[initial] = [];
+      }
+  
+      // Add the product to the array for its initial
+      groupedProducts[initial].push({ name, price });
+    });
+  
+    // Sort the products within each group by name
+    for (let initial in groupedProducts) {
+      groupedProducts[initial] = groupedProducts[initial].sort((a, b) => a.name.localeCompare(b.name));
     }
-}
-storeCatalogue(
+  
+    // Sort the initials in ascending order
+    const sortedInitials = Object.keys(groupedProducts).sort();
+  
+    // Print the grouped and sorted products
+    sortedInitials.forEach(initial => {
+      console.log(initial);
+      groupedProducts[initial].forEach(product => {
+        console.log(`  ${product.name}: ${product.price}`);
+      });
+    });
+  }
+groupProducts(
 
     ['Appricot : 20.4',
      'Fridge : 1500', 
