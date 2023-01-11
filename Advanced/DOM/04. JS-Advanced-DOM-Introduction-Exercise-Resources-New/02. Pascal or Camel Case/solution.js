97eea6609ea7e7aa5239949d16c1fd8c5360b608
Valentin <valio990210@gmail.com>
2023-01-11 09:38:31 +0200

    comments

--- Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -3,17 +3,23 @@ function solve() {
   let convention = document.getElementById("naming-convention").value
   let result = ""
 
-  // if(convention = "Camel Case"){
+  // splitting the input text by spaces
   let words = inputText.split(" ")
 
   if (convention == "Camel Case") {
+    // looping through the words
     for (let i = 0; i < words.length; i++) {
+      // converting each word to lowercase
       words[i] = words[i].toLowerCase()
 
+      // if the current word is not the first word in the input text,
+      // then capitalizing the first letter of the word
       if (i != 0) {
         words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1)
       }
     }
+
+    //same code as above but first word is in Capital Letter
   } else if (convention == "Pascal Case") {
     for (let i = 0; i < words.length; i++) {
       words[i] = words[i].toLowerCase()
@@ -21,8 +27,12 @@ function solve() {
       words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1)
     }
   } else {
+    // if the naming convention is neither Camel Case nor Pascal Case, 
+    // then the function returns "Error!"
     return document.getElementById("result") = "Error!"
   }
+
+  // joining the words back together
   result = words.join("")
   document.getElementById("result").textContent = result
   console.log(words);
