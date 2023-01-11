53facba51e78e2fad10ec07061322839a13afdb2
Valentin <valio990210@gmail.com>
2023-01-11 08:32:56 +0200

    substract elements

--- Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
+++ Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -1,3 +1,7 @@
 function subtract() {
-    console.log('TODO:...');
+    let num1 = getElementById("firstNumber").value;
+    let num2 = getElementById("secondNumber").value;
+    let result = getElementById("result")
+
+    result.textContent =Number(num1) - Number(num2)
 }
