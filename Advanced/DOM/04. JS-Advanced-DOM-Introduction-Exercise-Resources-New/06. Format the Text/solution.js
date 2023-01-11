5d4169840d5fb392776975e707193205714e83cc
Valentin <valio990210@gmail.com>
2023-01-11 12:23:19 +0200

    separete paragraphs into 3 esntances

--- Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,13 @@
 function solve() {
-  //TODO
+  let input = document.getElementById("input").value
+  let output = document.getElementById("output")
+
+  let sentance = input.split(".").filter(s => s.length !=0)
+
+  while(sentance.length >0){
+    let textForParagraph = sentance.splice(0,3).join(",") + "."
+    let p = document.createElement("p");
+    p.textContent = textForParagraph
+    output.appendChild(p)
+  }
 }
