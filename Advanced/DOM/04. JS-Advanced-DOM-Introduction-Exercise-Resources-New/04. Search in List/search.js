4105df5a9c42be1495655b5ec84a90cfe1dc72ff
Valentin <valio990210@gmail.com>
2023-01-11 10:30:35 +0200

    search elements

--- Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,12 +1,12 @@
 function search() {
    let searchText = document.getElementById("searchText").value
    let result = document.getElementById("result")
-   let listElement = document.querySelector("ul#towns li")
+   let listElement = document.querySelectorAll("ul#towns li")
    let matches =0
 
    for (const element of listElement) {
-      element.style.fontWeight = "normal"
-      element.style.textDecoration = ""
+      element.style.fontWeight = "normal";
+      element.style.textDecoration = "";
    }
 
    for(let element of listElement){
