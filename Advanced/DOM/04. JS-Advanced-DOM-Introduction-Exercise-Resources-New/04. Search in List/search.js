d4dabb69750403aabc5a24523dcdf69ce8822a53
Valentin <valio990210@gmail.com>
2023-01-11 10:25:29 +0200

    find matching towns

--- Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,3 +1,23 @@
 function search() {
-   // TODO
+   let searchText = document.getElementById("searchText").value
+   let result = document.getElementById("result")
+   let listElement = document.querySelector("ul#towns li")
+   let matches =0
+
+   for (const element of listElement) {
+      element.style.fontWeight = "normal"
+      element.style.textDecoration = ""
+   }
+
+   for(let element of listElement){
+      let text = element.textContent
+
+      if(text.match(searchText)){
+         matches++
+         element.style.fontWeight = "bold"
+         element.style.textDecoration = "underline"
+      }
+
+   }
+result.textContent = `${matches} matches found`
 }
