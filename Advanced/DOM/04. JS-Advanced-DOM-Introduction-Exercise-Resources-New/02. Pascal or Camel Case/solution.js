af6ff28019d3efbdec8ba0e308f7df7f8333fc8e
Valentin <valio990210@gmail.com>
2023-01-11 09:35:22 +0200

    print result

--- Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -4,36 +4,30 @@ function solve() {
   let result = ""
 
   // if(convention = "Camel Case"){
-      let words = inputText.split(" ")
-      
-      if(convention == "Camel Case"){
-        for(let i = 0; i < words.length; i++){
-          words[i] = words[i].toLowerCase()
-          
-          if(i != 0){
-            words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1)
-          }
-        }
-      } else if (convention == "Pascal Case"){
-        for(let i = 0; i < words.length; i++){
-          words[i] = words[i].toLowerCase()
-          
-          words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1)
-        }
-      } else{
-        return document.getElementById("result") = "Error!"
+  let words = inputText.split(" ")
+
+  if (convention == "Camel Case") {
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].toLowerCase()
+
+      if (i != 0) {
+        words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1)
       }
-      console.log(words);
     }
+  } else if (convention == "Pascal Case") {
+    for (let i = 0; i < words.length; i++) {
+      words[i] = words[i].toLowerCase()
 
-    //   for(let word of words){
-    //     word[0].toUpperCase()
-    //   let res = word
-    //   result.textContent = word
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].substring(1)
+    }
+  } else {
+    return document.getElementById("result") = "Error!"
+  }
+  result = words.join("")
+  document.getElementById("result").textContent = result
+  console.log(words);
+}
 
-      
-    // }
-  
-  
+
 
 
