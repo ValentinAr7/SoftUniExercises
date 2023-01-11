c73ccc4985977cf0f98e08e855c0cf8f53fb1b14
Valentin <valio990210@gmail.com>
2023-01-11 08:52:38 +0200

    to camel case

--- Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,18 @@
 function solve() {
-  //TODO...
+  let inputText = document.getElementById("text").value
+  let convention = document.getElementById("naming-convention").value
+  let butt = document.getElementById("result")
+
+  if(convention = "Camel Case"){
+    let wordsSeparetor = convention.map(element => {
+      let words = element.split(" ").toLowerCase()
+
+      for(let word of words){
+        word[0].toUpperCase()
+      return words
+      }
+      
+    })
+  }
 }
+
