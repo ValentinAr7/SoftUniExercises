96c1ea00000312d31b76fba0beac1f5df384f22f
Valentin <valio990210@gmail.com>
2023-01-11 10:33:20 +0200

    comments and formating

--- Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ Advanced/DOM/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -2,22 +2,26 @@ function search() {
    let searchText = document.getElementById("searchText").value
    let result = document.getElementById("result")
    let listElement = document.querySelectorAll("ul#towns li")
-   let matches =0
+   let matches = 0
 
+   // Loop through all the list items and remove any styling
    for (const element of listElement) {
       element.style.fontWeight = "normal";
       element.style.textDecoration = "";
    }
 
-   for(let element of listElement){
+   for (let element of listElement) {
+      //Get the text content of the current list item
       let text = element.textContent
 
-      if(text.match(searchText)){
+      // Check if the text content matches the search query
+      if (text.match(searchText)) {
          matches++
          element.style.fontWeight = "bold"
          element.style.textDecoration = "underline"
       }
 
    }
-result.textContent = `${matches} matches found`
+   // Update the result element with the number of matches found
+   result.textContent = `${matches} matches found`
 }
