23dcea00089692c2d3e77474421d66c23363eefe
Valentin <valio990210@gmail.com>
2023-01-12 09:58:54 +0200

    look up for matching emails

--- Advanced/DOM/2.DOM-Manipulations-and-Events/02. Delete-From-Table/app.js
+++ Advanced/DOM/2.DOM-Manipulations-and-Events/02. Delete-From-Table/app.js
@@ -1,3 +1,17 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+
+let value = document.querySelector('input[name="email"]').value
+
+let rows = Array.from(document.querySelectorAll("tbody tr"))
+
+
+let found = false
+for(let row of rows){
+    if(row.children[1].textContent == value){
+        const parent = row.parentElement
+        parent.removeChild(row);
+        found = true
+    }
+
+}
 }
