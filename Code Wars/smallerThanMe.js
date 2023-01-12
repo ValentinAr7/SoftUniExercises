edca4cafc036b76f83d1e3eb887f6f093809abe4
Valentin <valio990210@gmail.com>
2023-01-12 13:26:22 +0200

    smaller than me epty array

---
+++ Code Wars/smallerThanMe.js
@@ -0,0 +1,11 @@
+function smaller(nums) {
+
+    let result = []
+
+
+}
+    
+
+    smaller([5, 4, 7, 9, 2, 4, 4, 5, 6])   //([5, 4, 7, 9, 2, 4, 4, 5, 6])
+    console.log("-------------");
+    smaller([1, 1, -1, 0, 0])
