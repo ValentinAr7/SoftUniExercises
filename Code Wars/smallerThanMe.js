e3259da7fd49e30e6f215c65041c322369589355
Valentin <valio990210@gmail.com>
2023-01-12 14:57:07 +0200

    smaller numbers t the right

--- Code Wars/smallerThanMe.js
+++ Code Wars/smallerThanMe.js
@@ -1,11 +1,20 @@
-function smaller(nums) {
+function smaller(arr) {
 
-    let result = []
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      let count = 0;
+
+      for (let j = i + 1; j < arr.length; j++) {
+        if (arr[j] < arr[i]) {
+          count++;
+        }
+      }
+      result.push(count);
+    }
+    console.log(result); 
+    }
 
 
-}
-    
-
-    smaller([5, 4, 7, 9, 2, 4, 4, 5, 6])   //([5, 4, 7, 9, 2, 4, 4, 5, 6])
+    smaller([5, 4, 7, 9, 2, 4, 4, 5, 6])   //[4, 1, 5, 5, 0, 0, 0, 0, 0]);
     console.log("-------------");
-    smaller([1, 1, -1, 0, 0])
+    // smaller([1, 1, -1, 0, 0])
