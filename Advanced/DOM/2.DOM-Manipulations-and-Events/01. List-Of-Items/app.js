f86618af5bbb54e1362b896a14703f897ec5858f
Valentin <valio990210@gmail.com>
2023-01-12 09:25:27 +0200

    comments

--- Advanced/DOM/2.DOM-Manipulations-and-Events/01. List-Of-Items/app.js
+++ Advanced/DOM/2.DOM-Manipulations-and-Events/01. List-Of-Items/app.js
@@ -1,18 +1,22 @@
 function addItem() {
     
+    //select input field and read its value
     let text = document.getElementById("newItemText").value;
 
-    let liElement = document.getElementById("li")
+    //create new empty li element
+    //this li element is stored in the RAM but ot displayed
+    //on the screen
+    let liElement = document.createElement("li")
 
-    liElement.textContent("text")
+    //assign input value to element text content
+    liElement.textContent = text
 
+    //appent the child to the parent ul items
+    //not the li is visible on the screen
     let ulElement = document.getElementById("items")
-    ulElement.appendChild("liElement")
+    ulElement.appendChild(liElement)
 
-    document.getElementById("newTextItem").value = ""
-
-
-        
-
+    //select input field and clear content value
+    document.getElementById("newItemText").value = ""
 
 }
