3fb47d49773b62f193c3626bc9d977e3400926b0
Valentin <valio990210@gmail.com>
2023-01-13 10:58:44 +0200

    empty fields

--- Advanced/DOM/3.DOM-Exercises/04. Fill-Dropdown/dropdown.js
+++ Advanced/DOM/3.DOM-Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,16 +1,16 @@
 function addItem() {
 
     let text = document.getElementById("newItemText").value
-    let value = document.getElementById("newItemValue").value
+    let NewValue = document.getElementById("newItemValue").value
     let select = document.getElementById('menu');
 
     let optionElement = document.createElement("option")
-    optionElement.textContent = text + " " + value
+    optionElement.textContent = text + " " + NewValue
     console.log(optionElement);
 
     select.appendChild(optionElement)
 
-    text.textContent = ""
-    value.textContent = ""
+    text.value = ""
+    NewValue.value = ""
 
 }
