7a26ee8a427fbc0cac91f477c59b4857513667d8
Valentin <valio990210@gmail.com>
2023-01-13 11:49:40 +0200

    convert the text to ASCI + 1

--- Advanced/DOM/3.DOM-Exercises/05. Encode-and-Decode-Messages/app.js
+++ Advanced/DOM/3.DOM-Exercises/05. Encode-and-Decode-Messages/app.js
@@ -6,7 +6,15 @@ function encodeAndDecodeMessages() {
     buttons[0].addEventListener("click", encode)
     buttons[1].addEventListener("clicl", decode)
 
-    function encode(){}
+    function encode(e){
+        let encodedMessage = ''
+        let input = textAreas[0].value
+        for(let i = 0; i < input.length; i++){
+            encodedMessage += String.fromCharCode(input[index].charCodeAt(0) + 1)
+        }
+        textAreas[1].value = encodedMessage;
+        textAreas[0].value = ''
+    }
 
     function decode(){}
 }
