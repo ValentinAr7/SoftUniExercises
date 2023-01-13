963fe70e3be861d22baa817181cbcb9e6e28c0a2
Valentin <valio990210@gmail.com>
2023-01-13 11:46:34 +0200

    buttons and text areas

--- Advanced/DOM/3.DOM-Exercises/05. Encode-and-Decode-Messages/app.js
+++ Advanced/DOM/3.DOM-Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,12 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+
+    let textAreas = document.getElementById("textArea")
+    let buttons = document.querySelectorAll("button")
+
+    buttons[0].addEventListener("click", encode)
+    buttons[1].addEventListener("clicl", decode)
+
+    function encode(){}
+
+    function decode(){}
 }
