880bba848bca15625676a1f4b77c3b9697f5667a
Valentin <valio990210@gmail.com>
2023-01-13 10:53:33 +0200

    append child and displayvalues indrop down

--- Advanced/DOM/3.DOM-Exercises/04. Fill-Dropdown/dropdown.js
+++ Advanced/DOM/3.DOM-Exercises/04. Fill-Dropdown/dropdown.js
@@ -2,9 +2,15 @@ function addItem() {
 
     let text = document.getElementById("newItemText").value
     let value = document.getElementById("newItemValue").value
+    let select = document.getElementById('menu');
 
     let optionElement = document.createElement("option")
-    optionElement.textContent = text + value
+    optionElement.textContent = text + " " + value
     console.log(optionElement);
+
+    select.appendChild(optionElement)
+
+    text.textContent = ""
+    value.textContent = ""
 
 }
