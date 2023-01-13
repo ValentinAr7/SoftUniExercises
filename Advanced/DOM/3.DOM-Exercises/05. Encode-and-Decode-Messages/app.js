a04847053378ac751870255b1702c8387caf6e7c
Valentin <valio990210@gmail.com>
2023-01-13 12:56:30 +0200

    empty the boxes

--- Advanced/DOM/3.DOM-Exercises/05. Encode-and-Decode-Messages/app.js
+++ Advanced/DOM/3.DOM-Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,16 +1,16 @@
 function encodeAndDecodeMessages() {
 
-    let textAreas = document.getElementById("textArea")
+    let textAreas = document.querySelectorAll("textArea")
     let buttons = document.querySelectorAll("button")
 
     buttons[0].addEventListener("click", encode)
-    buttons[1].addEventListener("clicl", decode)
+    buttons[1].addEventListener("click", decode)
 
     function encode(e){
         let encodedMessage = ''
         let input = textAreas[0].value
         for(let i = 0; i < input.length; i++){
-            encodedMessage += String.fromCharCode(input[index].charCodeAt(0) + 1)
+            encodedMessage += String.fromCharCode(input[i].charCodeAt(0) + 1)
         }
         textAreas[1].value = encodedMessage;
         textAreas[0].value = ''
@@ -20,10 +20,9 @@ function encodeAndDecodeMessages() {
         let decodedMessage = ''
         let input = textAreas[1].value
         for(let i = 0; i < input.length; i++){
-            decodedMessage += String.fromCharCode(input[index].charCodeAt(0) - 1)
+            decodedMessage += String.fromCharCode(input[i].charCodeAt(0) - 1)
         }
-        textAreas[0].value = encodedMessage;
-        textAreas[1].value = ''
+        textAreas[1].value = decodedMessage
 
     }
 }
