e8646c3de8d610670522a0612c27c628df1a2610
Valentin <valio990210@gmail.com>
2023-01-14 12:32:16 +0200

    select buttons and fields and add event listener

--- Advanced/DOM/3.DOM-Exercises/06. Furniture/solution.js
+++ Advanced/DOM/3.DOM-Exercises/06. Furniture/solution.js
@@ -1,4 +1,12 @@
 function solve() {
 
-  //TODO...
+  const [generateBtn, buyBtn] = Array.from(document.querySelectorAll("button"));
+  const [input, output] = Array.from(document.querySelectorAll("textArea"));
+  const tbody = document.querySelector("tbody");
+
+  generateBtn.addEventListener('click', generate);
+  buyBtn.addEventListener('click', buy)
+
+
+
 }
