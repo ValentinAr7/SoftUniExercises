6024b0254a3bc67c03d31712f1e214621f6b56fc
Valentin <valio990210@gmail.com>
2023-01-14 12:35:22 +0200

    create elements and append them to the row

--- Advanced/DOM/3.DOM-Exercises/06. Furniture/solution.js
+++ Advanced/DOM/3.DOM-Exercises/06. Furniture/solution.js
@@ -7,6 +7,23 @@ function solve() {
   generateBtn.addEventListener('click', generate);
   buyBtn.addEventListener('click', buy)
 
+  const items = [];
+
+  function generate(){
+    const data = JSON.parse(input.value);
+
+    for(let item of data){
+      const row = document.createElement('tr')
+
+      row.appendChild(createColumn('img', item.img));
+      row.appendChild(createColumn('p', item.name));
+      row.appendChild(createColumn('p', item.price));
+      row.appendChild(createColumn('p', item.decFactor));
+    }
+  }
+
+  
+
 
 
 }
