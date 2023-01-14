5da66368539a7b0241d3c74f1fd2e82adf76fa9e
Valentin <valio990210@gmail.com>
2023-01-14 11:29:49 +0200

    command processor

---
+++ Advanced/Functions Advanced/commandProcessor.js
@@ -0,0 +1,29 @@
+function createProcessor(){
+    let test = ''
+
+
+function append(str){
+    state+= str
+}
+
+function removeStart(n){
+    state = state.slice(n)
+}
+
+function removeEnd(n){
+    state =state.slice(0,-n);
+}
+
+function print(){
+    console.log(state);
+}
+
+
+
+return {
+    append,
+    removeStart,
+    removeEnd,
+    print
+}
+}
