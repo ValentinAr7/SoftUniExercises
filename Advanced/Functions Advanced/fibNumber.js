ddf3e0a232d350998116d730817abc770eebc5af
Valentin <valio990210@gmail.com>
2023-01-15 10:23:28 +0200

    fibonator

--- Advanced/Functions Advanced/fibNumber.js
+++ Advanced/Functions Advanced/fibNumber.js
@@ -1,10 +1,23 @@
-function fibonaci (){
+function getFibonator() {
 
-    let num1 = 1;
-    let num2 = 1;
+    let prev = 0;
+    let curr = 1;
 
-    let fub = num1 + num2
-    console.log(fub);
+    return function () {
+        const next = prev + curr
+        prev = curr;
+        curr = next
 
+        return prev
+    }
 }
+
+let fib = getFibonator();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
 
