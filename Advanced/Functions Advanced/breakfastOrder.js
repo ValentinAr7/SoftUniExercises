ff665dc24aeb8a3a810791c304ff007dedd57eae
Valentin <valio990210@gmail.com>
2023-01-15 16:14:55 +0200

    #

--- Advanced/Functions Advanced/breakfastOrder.js
+++ Advanced/Functions Advanced/breakfastOrder.js
@@ -1,4 +1,4 @@
-function breakfastOrder(){
+function breakfastOrder() {
 
     let stock = {
         protein: 0,
@@ -14,40 +14,36 @@ function breakfastOrder(){
         report
     }
 
-    function manager(line){
+    function manager(line) {
         let [command, param, qty] = line.split(" ")
         const fn = commands[command];
         return fn(param, qty)
     }
 
-    function restock(type, qty){
+    function restock(type, qty) {
         stock[type] += Number(qty)
         return 'Success';
     }
 
-    function report (){
+    function report() {
         return `protein = ${stock.protein}, carbohydrate = ${stock.carbohydrate}, fat = ${stock.fat}, flovour = ${stock.flavour}`
     }
-    
+
     function prepare(recipeAsString, qty) {
         qty = Number(qty);
         const recipe = Object.entries(recipe[recipeAsString]);
 
         recipe.forEach(ingridient => ingridient[1] *= qty);
 
-        for(let [ingridient, requiered] of recipe){
-            if(stock[ingridient] < requiered){
+        for (let [ingridient, requiered] of recipe) {
+            if (stock[ingridient] < requiered) {
                 return `Error: not enough: ${ingridient} in stock`
             }
         }
-    recipe.forEach((ingridient =>ingridient[1] *= qty))
+        recipe.forEach(([ingridient, requiered]) => stock[ingridient] -= requiered)
+        return 'Success'
     }
-
-
-    return function (command){
-
-
+    return function (command) {
     }
-
 
 } breakfastOrder()
