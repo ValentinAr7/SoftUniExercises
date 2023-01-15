81e33d4f7ffc0e854a30d7a5e3ca8134f771a86a
Valentin <valio990210@gmail.com>
2023-01-15 19:17:29 +0200

    result

--- Advanced/Objects Advanced/Small Projects/07. Simple Calculator/app.js
+++ Advanced/Objects Advanced/Small Projects/07. Simple Calculator/app.js
@@ -1,30 +1,36 @@
 function calculator() {
-    let num1 = document.getElementById('num1').value;
-    let num2 = document.getElementById('num2').value;
-    let res = document.getElementById('result')
+    // let num1 = document.getElementById('num1').value;
+    // let num2 = document.getElementById('num2').value;
+    // let res = document.getElementById('result')
 
-    let btnAdd = document.getElementById('sumButton')
-    btnAdd.addEventListener('click', add)
+    // let btnAdd = document.getElementById('sumButton')
+    // btnAdd.addEventListener('click', add)
 
-    function add(){
-        res = num1 + num2
-        init.value = res
-    }
-
-    function init(){
- 
-    }
-
-    // function substract (num1, num2){
+    let field1 = null;
+    let field2 = null;
+    let result = null;
 
 
-    // }
-
-   
     return{
         add,
+        substract,
         init
     }
+
+    function init(f1, f2, fr){
+        field1 = document.querySelector(f1)
+        field2 = document.querySelector(f2)
+        result = document.querySelector(fr)
+    }
+
+    function add(){
+        result.value = Number(field1.value) + Number(field2.value)
+    }
+
+    function substract (){
+        result.value = Number(field2.value) - Number(field1.value)
+    }
+
 
 
 }
