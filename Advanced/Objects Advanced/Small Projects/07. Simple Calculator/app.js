29affdf951c6214a0e30fbadfd44bab6738bbb3d
Valentin <valio990210@gmail.com>
2023-01-15 19:01:28 +0200

    elements and functions

--- Advanced/Objects Advanced/Small Projects/07. Simple Calculator/app.js
+++ Advanced/Objects Advanced/Small Projects/07. Simple Calculator/app.js
@@ -1,6 +1,36 @@
 function calculator() {
-    // TODO:
+    let num1 = document.getElementById('num1').value;
+    let num2 = document.getElementById('num2').value;
+    let res = document.getElementById('result')
+
+    let btnAdd = document.getElementById('sumButton')
+    btnAdd.addEventListener('click', add)
+
+    function add(){
+        res = num1 + num2
+        init.value = res
+    }
+
+    function init(){
+ 
+    }
+
+    // function substract (num1, num2){
+
+
+    // }
+
+   
+    return{
+        add,
+        init
+    }
+
+
 }
+
+const calculate = calculator ();
+calculate.init ('#num1', '#num2', '#result');
 
 
 
