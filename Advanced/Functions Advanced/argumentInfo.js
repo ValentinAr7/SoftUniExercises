c7331db2cb401002139bb5190c2dc9e628a27fc1
Valentin <valio990210@gmail.com>
2023-01-15 09:56:25 +0200

    order result

--- Advanced/Functions Advanced/argumentInfo.js
+++ Advanced/Functions Advanced/argumentInfo.js
@@ -2,10 +2,18 @@ function argumentInfo(){
 
 const params = Array.from(arguments)
 
-const type = {}
+const types = {}
 
+for(let arg of params){
+    const type = typeof arg;
+    console.log(`${type}: ${arg}`);
 
-
+    if(types[type] == undefined){
+        types[type] = 0;
+    }
+    types[type]++
+}
+console.log(types);
 }
 
 argumentInfo('cat', 42, function () { 
