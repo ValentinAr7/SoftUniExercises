81ca8b72e840ec1a6e9c22a879b58f61412e94c9
Valentin <valio990210@gmail.com>
2023-01-16 13:19:16 +0200

    formatting

--- Advanced/Unit Testing and Error Handling/05/symmetry.test.js
+++ Advanced/Unit Testing and Error Handling/05/symmetry.test.js
@@ -1,18 +1,16 @@
-const {expect} = require('chai');
-const {isSymmetric} = require ('/symmetry');
+const { expect } = require('chai');
+const { isSymmetric } = require('/symmetry');
 
 describe('Symmetry Checker', () => {
-    it ('works with symetric numeric arrays', () => {
+    it('works with symetric numeric arrays', () => {
         expect(isSymmetric([1, 2, 2, 1]).to.be.true)
     })
 
-
-    it ('returns fals for non-numeric arrays', () => {
+    it('returns fals for non-numeric arrays', () => {
         expect(isSymmetric([1, 2, 3]).to.be.false)
     })
 
-    
-    it ('returns false for non-array', () => {
+    it('returns false for non-array', () => {
         expect(isSymmetric(5).to.be.false)
     })
 })
