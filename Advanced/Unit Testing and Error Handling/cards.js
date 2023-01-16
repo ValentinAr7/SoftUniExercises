db2b88771c81b517f9eb5246f41a3601a4eae004
Valentin <valio990210@gmail.com>
2023-01-16 11:12:27 +0200

    cards

--- Advanced/Unit Testing and Error Handling/cards.js
+++ Advanced/Unit Testing and Error Handling/cards.js
@@ -1,6 +1,9 @@
-function cards (face, suit){
+function cards(face, suit) {
 
     const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    if (faces.indexOf(face) == -1) {
+        throw new Error('Invalid face: ' + face)
+    }
 
     const suits = {
         'S': '\u2660',
@@ -8,18 +11,18 @@ function cards (face, suit){
         'D': '\u2666',
         'C': '\u2663',
     }
-    
+
     const result = {
         face,
         suit: suits[suit],
-        toString(){
+        toString() {
             return this.face + this.suit
         }
     }
 
-return result
-} 
+    return result
+}
 
 console.log(cards('A', 'S').toString())
-console.log(cards ('10', 'H').toString())
-console.log(cards ('1', 'C').toString())
+console.log(cards('10', 'H').toString())
+console.log(cards('1', 'C').toString())
