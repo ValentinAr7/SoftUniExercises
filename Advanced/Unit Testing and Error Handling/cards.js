c58811e167b44a0dc900b26a34cc60edeb7b3845
Valentin <valio990210@gmail.com>
2023-01-16 11:09:06 +0200

    faces

--- Advanced/Unit Testing and Error Handling/cards.js
+++ Advanced/Unit Testing and Error Handling/cards.js
@@ -1,5 +1,7 @@
 function cards (face, suit){
 
+    const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+
     const suits = {
         'S': '\u2660',
         'H': '\u2665',
