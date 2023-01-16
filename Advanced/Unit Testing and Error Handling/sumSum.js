2514a99b5e965d9619857c76df88863504da6a0a
Valentin <valio990210@gmail.com>
2023-01-16 10:54:12 +0200

    check if first element is array

--- Advanced/Unit Testing and Error Handling/sumSum.js
+++ Advanced/Unit Testing and Error Handling/sumSum.js
@@ -1,12 +1,22 @@
-function subSum (array, start, finish){
+function subSum (arr, start, finish){
+    let sum = 0
+
+    if(Array.isArray(arr) == false){
+        return NaN
+    }
+
 
 for(let i = start; i<=finish; i++){
-    console.log(i);
+    sum = arr[i]
+    
 }
+console.log(sum);
 }
 
 
 subSum ([10, 20, 30, 40, 50, 60], 3, 300)
 subSum ([1.1, 2.2, 3.3, 4.4, 5.5], -3, 1)
 subSum ([10, 'twenty', 30, 40], 0, 2)
+subSum ([], 1, 2)
+subSum ('text', 0, 2)
 
