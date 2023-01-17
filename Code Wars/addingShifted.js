b7d17ca4bb2aae4c33ea9ea14856689c64683d59
Valentin <valio990210@gmail.com>
2023-01-17 20:22:33 +0200

    adding the numbers

--- Code Wars/addingShifted.js
+++ Code Wars/addingShifted.js
@@ -1,8 +1,24 @@
 function addingShifted (arrayOfArrays, shift) {
-  
+
+    let array1 = arrayOfArrays[0]
+    let array2 = arrayOfArrays[1]
+
     let result = []
 
+    for(let i = 0; i < array1.length; i++){
+        let num1 = array1[i]
+
+        for(let i = 0; i < array2.length; i++){
+        let num2 = array2[i]
+        
+        result.push(num1 + num2)
+        }
+        
+    }
+
+
+    console.log(result);
 }
 
 
-addingShifted [[1, 2, 3, 4, 5, 6], [7, 7, 7, 7, 7, -7]];
+addingShifted ([[1, 2, 3, 4, 5, 6], [7, 7, 7, 7, 7, -7]]);
