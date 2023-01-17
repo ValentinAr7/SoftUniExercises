56c3fecf56865b27a1d3ab7ea94f2038dd6a3625
Valentin <valio990210@gmail.com>
2023-01-17 20:09:07 +0200

    n squared row

---
+++ Code Wars/sumOfOddNumbers.js
@@ -0,0 +1,10 @@
+function rowSumOddNumbers(n) {
+        return Math.pow(n, 3);
+      }
+      
+      function rowSumOddNumbers(n) {
+        return n*n*n
+      }
+
+
+(rowSumOddNumbers(42)) //74088;
