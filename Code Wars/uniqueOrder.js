d08f87a8794b0fb8e32415776544da3fa1a19917
Valentin <valio990210@gmail.com>
2023-01-17 19:46:42 +0200

    check for matching eltters

--- Code Wars/uniqueOrder.js
+++ Code Wars/uniqueOrder.js
@@ -1,12 +1,25 @@
-var uniqueInOrder=function(iterable){
+var uniqueInOrder=function(string){
 
+    let result = []
+
+    for(let i = 0; i < string.length; i++){
+        let currentLetter = string[i]
+        let nextChar = string[i + 1]
+
+        if(!result.includes(currentLetter) && currentLetter !== nextChar){
+            result.push(currentLetter)
+        }
+    }
+    console.log(result);
 
   }
 
+//   uniqueInOrder('AAAABBBCCDAABBB')
 
-  describe("Tests", () => {
-    it("test", () => {
-      assert.deepEqual(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
-    });
-  });
+
+//   describe("Tests", () => {
+//     it("test", () => {
+//       assert.deepEqual(uniqueInOrder('AAAABBBCCDAABBB'), ['A','B','C','D','A','B'])
+//     });
+//   });
   
