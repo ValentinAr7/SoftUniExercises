b83ad3c95192fae69fe008e9508a3d53b72da78b
Valentin <valio990210@gmail.com>
2023-01-18 16:26:45 +0200

    class compare to method

--- Advanced/Classes/05-tickets.js
+++ Advanced/Classes/05-tickets.js
@@ -1,5 +1,21 @@
 function ticketsData(arrayOfStrings, sortingCriteria){
 
+    class Ticket {
+        constructor(destination, price, status){
+            this.destination = destination;
+            this.price = price;
+            this.status = status
+        }
+
+        compareTo(other, criteria){
+            if(typeof this[criteria] === 'string'){
+                return this[criteria].localeCompare(other[criteria]);
+            } else {
+                return this[criteria] - other[criteria]
+            }
+        }
+    }
+
     let tickets = []
 
     for(let i = 0; i < arrayOfStrings.length; i++){
@@ -7,14 +23,13 @@ function ticketsData(arrayOfStrings, sortingCriteria){
         price = Number(price)
         let ticket = new Ticket(destination, price, status);
         tickets.push(ticket)
-
     }
-
+    
 
 }
 
-ticketsData(['Philadelphia|94.20|available',
+console.log(ticketsData(['Philadelphia|94.20|available',
 'New York City|95.99|available',
 'New York City|95.99|sold',
 'Boston|126.20|departed'],
-'destination')
+'destination'))
