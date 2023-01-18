b0c779b890a9404d92596464a5b5b65bc998a30c
Valentin <valio990210@gmail.com>
2023-01-18 12:36:58 +0200

    constructor

---
+++ Advanced/Classes/05-tickets.js
@@ -0,0 +1,15 @@
+class Tickets {
+
+    constructor(destinationName, price, status){
+        this.destinationName = destinationName
+        this.price = price
+        this.status = status
+    }
+
+}
+
+['Philadelphia|94.20|available',
+'New York City|95.99|available',
+'New York City|95.99|sold',
+'Boston|126.20|departed'],
+'destination'
