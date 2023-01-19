ec3b777336be68bca769e02008d2f0ea84450ba5
Valentin <valio990210@gmail.com>
2023-01-19 19:39:13 +0200

    string cutter

--- Code Wars/trimmingString.js
+++ Code Wars/trimmingString.js
@@ -3,19 +3,34 @@ function trim(str, size) {
     let trimmedStr = []
     let cutter = size - 3
 
+    if(str.includes('...')){
+    let newStr = str.substring(-3)
+    console.log(newStr);   
+     }
+
     if(str.length <= 3){
     trimmedStr = str.slice(0, size)
-    } else {
+    console.log(trimmedStr + '...');
+    return trimmedStr + '...'
+   } 
+    
+    else if( str.length <= size){
+        console.log(str);
+    return str        
+        } 
+    
+    else {
         trimmedStr = str.slice(0, cutter )
+        console.log(trimmedStr + '...');
+        return trimmedStr + '...'
     }
 
 
+    
 
-console.log(trimmedStr + '...'); 
-
-
-    return str;
-  
 }
 
-trim("Hi", 1) //"Creating ka..."
+trim("Creating kata is fun", 14)
+trim("He", 1)
+trim("Code Wars is pretty rad", 50)
+trim('Hey...', 3)
