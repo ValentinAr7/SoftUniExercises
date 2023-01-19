10bce2d2b76115d59df0cb63312d1ae39c533bbf
Valentin <valio990210@gmail.com>
2023-01-19 18:28:05 +0200

    engines

--- Advanced/Objects Advanced/carFactory.js
+++ Advanced/Objects Advanced/carFactory.js
@@ -1,26 +1,37 @@
-function carFactory (data){
+function carFactory(data) {
 
     let res = {}
     res.model = data.model
 
-    if(data.power <= 90){
-       res.engine {
-        power: 90,
-        volume: 1800
-       }
-    }
+    if (data.power <= 90) {
+        res.engine {
+            power: 90,
+            volume: 1800
+        }
+    } else if (data.power <= 90) {
+        res.engine {
+            power: 120,
+            volume: 2400
+        }
+    } else (data.power <= 90){
+        res.engine {
+         power: 200,
+         volume: 3500
+        }
+     }
 
 }
 
-carFactory({ 
-model: 'VW Golf II',
-power: 90,
-color: 'blue',
-carriage: 'hatchback',
-wheelsize: 14 })
+carFactory({
+    model: 'VW Golf II',
+    power: 90,
+    color: 'blue',
+    carriage: 'hatchback',
+    wheelsize: 14
+})
 
 
-// { model: 'VW Golf II', 
-// engine: { power: 90, volume: 1800 }, 
-// carriage: { type: 'hatchback', color: 'blue' }, 
+// { model: 'VW Golf II',
+// engine: { power: 90, volume: 1800 },
+// carriage: { type: 'hatchback', color: 'blue' },
 // wheels: [13, 13, 13, 13] }
