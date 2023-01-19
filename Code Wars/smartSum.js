a1b698de0fb9268f3137fee13c50e9596bf0a4cf
Valentin <valio990210@gmail.com>
2023-01-19 18:45:02 +0200

    comments

--- Code Wars/smartSum.js
+++ Code Wars/smartSum.js
@@ -1,5 +1,9 @@
 const smartSum = (...args) => args.reduce
 ((s, a) => s + (Number.isInteger(a) ? a : smartSum(...a)), 0);
 
+//This function uses (...) to gather all the arguments into an array. It uses Array.reduce() method to iterate through the array of arguments (arga) and add the sum of the elements in the array.
+
+// The function checks if the current element a is an integer using Number.isInteger(a) method. If it is an integer, it adds the element to the accumulated sum, s.
+
 
 smartSum(1,2,3,[4,5],6)
