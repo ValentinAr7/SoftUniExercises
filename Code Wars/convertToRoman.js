6b0f65e2811c02693e311f5c571b61e4ba4d33f9
Valentin <valio990210@gmail.com>
2023-01-22 13:26:41 +0200

    convert numbers to roman numbers

--- Code Wars/convertToRoman.js
+++ Code Wars/convertToRoman.js
@@ -1,65 +1,14 @@
-function convert(number) {
-
-    let result = []
-
-    const romanNumerals = {
-            1000: "M",
-            900: "CM",
-            500: "D",
-            400: "CD",
-            100: "C",
-            90: "XC",
-            50: "L",
-            40: "XL",
-            10: "X",
-            9: "IX",
-            5: "V",
-            4: "IV",
-            1: "I"
-    }
-
-
-    for (let key in romanNumerals) {
-
-        if(number >= key){
-            result.push(romanNumerals[key])
-            number -= key
+function intToRoman(num) {
+    let values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
+    let symbols = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ];
+    let roman = "";
+    for (let i = 0; i < values.length; i++) {
+        while (num >= values[i]) {
+            roman += symbols[i];
+            num -= values[i];
         }
-
-      }
-
-
-    console.log(result);
-    console.log(number);
+    }
+    console.log(roman);
 }
 
-
-convert(1000)
-
-
-
-
-
-// if(num > 1000){
-//     result.push('M')
-//     num -= 1000
-// } else if (num > 500){
-//     result.push('D')
-//     num-=500
-// } else if (num > 100){
-//     result.push('C')
-//     num-=100
-// } else if (num > 50){
-//     result.push('L')
-//     num-=50
-// }
-// else if (num > 10){
-//     result.push('X')
-//     num -= 10
-// } else if (num > 5){
-//     result.push('V')
-//     num-=5
-// } else {
-//     result.push('I')
-//     num-=1
-// }
+intToRoman(1990)
