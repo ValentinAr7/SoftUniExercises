473bad6a452a1fd674a8afbcebf775fc9a59b48b
Valentin <valio990210@gmail.com>
2023-01-22 13:31:51 +0200

    comments

--- Code Wars/convertToRoman.js
+++ Code Wars/convertToRoman.js
@@ -1,10 +1,15 @@
 function intToRoman(num) {
-    
+
+    //Check if number is in the range between 1 and 3999
+    //check the number is integer
+    //check the number is whole number
     if (num < 1 || num > 3999 || typeof num !== 'number' || num % 1 !== 0) {
         return "Invalid input. Please enter a positive integer between 1 and 3999.";
     }
-    let values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
-    let symbols = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ];
+
+
+    let values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    let symbols = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
     let roman = "";
     for (let i = 0; i < values.length; i++) {
         while (num >= values[i]) {
