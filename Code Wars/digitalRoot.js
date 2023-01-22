df27a391ae5de992ceb2ba007ed34f014cdf14f2
Valentin <valio990210@gmail.com>
2023-01-22 14:26:13 +0200

    for loop

--- Code Wars/digitalRoot.js
+++ Code Wars/digitalRoot.js
@@ -1,36 +1,33 @@
 function digitalRoot(n) {
-   
+
 
     let sum = 0 //29
 
     let num = n.toString()
 
-
-
-
-    for(let i = 0; i < num.length; i++){
-
-        let currentNum = Number(num[i])
-        sum+=currentNum
-        
-
-
-
+    while (num.length != 1) {
+        for (let i = 0; i < num.length; i++) {
+            let currentNum = Number(num[i])
+            sum += currentNum
+        }
     }
 
-    let sumStr = sum.toString()
 
-    for(let j=0; j<sumStr.length; j++){
-        let curretnNum= Number(sumStr[j])
-        sum  += curretnNum
-    }
 
 
     console.log(sum);
 
-    
-    
 
-  }
 
-  digitalRoot(493193)  //-->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2)
+
+}
+
+digitalRoot(493193)  //-->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2)#
+
+
+      // let sumStr = sum.toString()
+
+    // for(let j=0; j<sumStr.length; j++){
+    //     let curretnNum= Number(sumStr[j])
+    //     sum  += curretnNum
+    // }
