89b23078b056b0a6657ea409e3be1def2c621660
Valentin <valio990210@gmail.com>
2023-01-22 14:32:38 +0200

    convert to string

--- Code Wars/digitalRoot.js
+++ Code Wars/digitalRoot.js
@@ -3,12 +3,7 @@ function digitalRoot(n) {
   return digitalRoot(n.toString().split('').reduce((a, b) => parseInt(a) + parseInt(b), 0));
 }
 
+//convert to string
+
+
 digitalRoot(493193)  //-->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2)#
-
-
-      // let sumStr = sum.toString()
-
-    // for(let j=0; j<sumStr.length; j++){
-    //     let curretnNum= Number(sumStr[j])
-    //     sum  += curretnNum
-    // }
