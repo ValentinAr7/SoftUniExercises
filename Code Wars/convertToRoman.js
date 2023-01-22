31c75adbe7c7a5330ec24f90679136e3f04a2840
Valentin <valio990210@gmail.com>
2023-01-22 13:29:17 +0200

    invalid number

--- Code Wars/convertToRoman.js
+++ Code Wars/convertToRoman.js
@@ -1,4 +1,8 @@
 function intToRoman(num) {
+    
+    if (num < 1 || num > 3999 || typeof num !== 'number' || num % 1 !== 0) {
+        return "Invalid input. Please enter a positive integer between 1 and 3999.";
+    }
     let values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ];
     let symbols = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ];
     let roman = "";
@@ -8,7 +12,7 @@ function intToRoman(num) {
             num -= values[i];
         }
     }
-    console.log(roman);
+    return roman
 }
 
 intToRoman(1990)
