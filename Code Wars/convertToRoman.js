1cefc4a531cd25aa066582c5412027b71f5f30e1
Valentin <valio990210@gmail.com>
2023-01-22 12:44:34 +0200

    MDCL CONVERTER

--- Code Wars/convertToRoman.js
+++ Code Wars/convertToRoman.js
@@ -16,10 +16,25 @@ function convert(num) {
     for(key in romanNumerals){
         let values = romanNumerals[key]
         
-        if(num < )
-
-        
+        if(num > 1000){
+            result.push('M')
+            num -= 1000
+        } else if (num > 500){
+            result.push('D')
+            num-=500
+        } else if (num > 100){
+            result.push('C')
+            num-=100
+        } else if (num > 50){
+            result.push('L')
+            num-=50
+        } else if (num > 10){
+            result.push('D')
+            num-=500
+        } 
     }
+    console.log(result);
+    console.log(num);
 
 }
 
