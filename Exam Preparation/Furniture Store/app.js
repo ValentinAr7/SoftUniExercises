bcc46e4d15cf7b00ee5d39d4a0917204817e13d2
Valentin <valio990210@gmail.com>
2023-01-23 18:20:39 +0200

    get elements by id and add event listener

--- Exam Preparation/Furniture Store/app.js
+++ Exam Preparation/Furniture Store/app.js
@@ -1,5 +1,38 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TO DO
+    
+    let model = document.getElementById('model');
+    let year = document.getElementById('year');
+    let description = document.getElementById('description');
+    let price = document.getElementById('price');
+
+    let btnAdd = document.getElementById('add')
+    btnAdd.addEventListener('click', add)
+
+    function add(){
+
+        if(model == '' || year == '' || description == '' || price == ''){
+            return;
+        }
+        
+    }
+
+
+
+
+
+
+
+    function moreInfo(){
+
+    }
+
+    function buy(){
+
+
+    }
+
+
+
 }
