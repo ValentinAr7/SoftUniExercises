1ac2d957b74b39d86f6fbcabf2d99b93a38bd529
Valentin <valio990210@gmail.com>
2023-01-23 18:10:45 +0200

    files

--- Advanced/DOM/2.DOM-Manipulations-and-Events/02. Delete-From-Table/app.js
+++ Advanced/DOM/2.DOM-Manipulations-and-Events/02. Delete-From-Table/app.js
@@ -4,7 +4,6 @@ let value = document.querySelector('input[name="email"]').value
 
 let rows = Array.from(document.querySelectorAll("tbody tr"))
 
-
 let found = false
 for(let row of rows){
     if(row.children[1].textContent == value){
