7fb9fed95de568139e1217f4d0316223547d383f
Valentin <valio990210@gmail.com>
2023-01-23 19:00:29 +0200

    input values

--- Exam Preparation/Furniture Store/app.js
+++ Exam Preparation/Furniture Store/app.js
@@ -2,15 +2,25 @@ window.addEventListener('load', solve);
 
 function solve() {
     
-    let model = document.getElementById('model');
-    let year = document.getElementById('year');
-    let description = document.getElementById('description');
-    let price = document.getElementById('price');
+    const input ={
+        model: document.getElementById('model'),
+        year: document.getElementById('year'),
+        description: document.getElementById('description'),
+        price: document.getElementById('price'),
+    }
+
+
+    let table = document.querySelectorAll('#information tr:nth-child(1) th')
 
     let btnAdd = document.getElementById('add')
     btnAdd.addEventListener('click', add)
 
     function add(){
+
+        const model = input.model.value
+        const year = input.year.value
+        const description = input.description.value
+        const price = input.price.value
 
         if(model == '' || year == '' || description == '' || price == ''){
             return;
