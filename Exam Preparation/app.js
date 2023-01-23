95d2c3efc95ee6ec9e1cfd0f8f2a162fce52ecd6
Valentin <valio990210@gmail.com>
2023-01-23 10:40:34 +0200

    get elements in objects

--- Exam Preparation/app.js
+++ Exam Preparation/app.js
@@ -1,5 +1,15 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    
+
+    const input = {
+        title: document.getElementById('post-title'),
+        category: document.getElementById('post-category'),
+        content: document.getElementById('post-content')
+    };
+
+    const lists = {
+        review: document.getElementById('review-list'),
+        published: document.getElementById('published-list')
+    }
 }
