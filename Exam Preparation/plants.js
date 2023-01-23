b8050d80b116f032f97a538613cde35feef97f92
Valentin <valio990210@gmail.com>
2023-01-23 11:59:53 +0200

    error and success message

--- Exam Preparation/plants.js
+++ Exam Preparation/plants.js
@@ -4,4 +4,19 @@ class Garden {
         this.plants = [];
         this.storage = storage;
     }
+
+    addPlant(plantName, spaceRequiered){
+        if(this.spaceAvailable < spaceRequiered){
+            throw new Error('Not enough space in the garden.')
+        }
+        this.spaceAvailable -= spaceRequiered;
+        this.plants.push({
+            plantName,
+            spaceRequiered,
+            ripe: false,
+            quantity: 0
+        })
+
+        return `The ${plantName} has been successfully planted in the garden.`
+    }
 }
