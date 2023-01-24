843d97a8c3b168f12ccb437b2d2810c7852b658c
Valentin <valio990210@gmail.com>
2023-01-24 12:57:58 +0200

    read values frrom fields

--- Exam Preparation/Scary Solution/01.Scary-Story_Resources/app.js
+++ Exam Preparation/Scary Solution/01.Scary-Story_Resources/app.js
@@ -1,14 +1,30 @@
 window.addEventListener("load", solve);
 
 function solve() {
+
+  //Input fields
   const firstNameElement = document.getElementById('first-name');
   const lastNameElement = document.getElementById('last-name');
   const ageElement = document.getElementById('age');
   const storyTitleElement = document.getElementById('story-title')
   const genreElement = document.getElementById('genre')
   const storyElemenet = document.getElementById('story')
+  //buttons
+  let btnPublishElement = document.getElementById('form-btn')
 
-  
+  let firstName = firstNameElement.value;
+  let lastName = lastNameElement.value;
+  let age = ageElement.value;
+  let storyTitle = storyTitleElement.value;
+  let genre = genreElement.value;
+  let story = storyElemenet.value;
+
+  btnPublishElement.addEventListener('click', (e)=> {
+    e.preventDefault()
+
+    
+
+  })
 
 
 }
