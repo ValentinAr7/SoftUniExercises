d216cce2d7890ec20224e2a3609cbbd87a88cd37
Valentin <valio990210@gmail.com>
2023-01-24 13:21:35 +0200

    text content in preview

--- Exam Preparation/Scary Solution/01.Scary-Story_Resources/app.js
+++ Exam Preparation/Scary Solution/01.Scary-Story_Resources/app.js
@@ -9,20 +9,43 @@ function solve() {
   const storyTitleElement = document.getElementById('story-title')
   const genreElement = document.getElementById('genre')
   const storyElemenet = document.getElementById('story')
+  const previewListElement = document.getElementById('preview-list')
   //buttons
   let btnPublishElement = document.getElementById('form-btn')
-
-  let firstName = firstNameElement.value;
-  let lastName = lastNameElement.value;
-  let age = ageElement.value;
-  let storyTitle = storyTitleElement.value;
-  let genre = genreElement.value;
-  let story = storyElemenet.value;
 
   btnPublishElement.addEventListener('click', (e)=> {
     e.preventDefault()
 
+    let firstName = firstNameElement.value;
+    let lastName = lastNameElement.value;
+    let age = ageElement.value;
+    let storyTitle = storyTitleElement.value;
+    let genre = genreElement.value;
+    let story = storyElemenet.value;
+
+    if(!firstName || !lastName || !age || !storyTitle || !genre || !story){
+      return;
+    }
+
+    let fullNameCellElement = document.createElement('h4');
+    let ageCellElement = document.createElement('p');
+    let storyTitleCellElement = document.createElement('p');
+    let genreCellElement = document.createElement('p');
+    let fullStoryCellElement = document.createElement('p')
+
+    fullNameCellElement.textContent = `Name: ${firstName} ${lastName}`;
+    ageCellElement.textContent = `Age: ${age}`;
+    storyTitleCellElement.textContent = `Title: ${title}`;
+    genreCellElement.textContent = `Genre: ${genre}`;
+    fullStoryCellElement.textContent = `${story}`
+
+
+
+
     
+
+
+
 
   })
 
