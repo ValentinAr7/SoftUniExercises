4417750a78ef580244af3999f1a1808bed78d3e6
Valentin <valio990210@gmail.com>
2023-01-24 12:54:17 +0200

    get elements by id

--- Exam Preparation/Scary Solution/01.Scary-Story_Resources/app.js
+++ Exam Preparation/Scary Solution/01.Scary-Story_Resources/app.js
@@ -1,5 +1,14 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO ....
+  const firstNameElement = document.getElementById('first-name');
+  const lastNameElement = document.getElementById('last-name');
+  const ageElement = document.getElementById('age');
+  const storyTitleElement = document.getElementById('story-title')
+  const genreElement = document.getElementById('genre')
+  const storyElemenet = document.getElementById('story')
+
+  
+
+
 }
