41873886d8719d8912525f33f17fd8d61f7ce021
Valentin <valio990210@gmail.com>
2023-01-25 11:07:38 +0200

    create elements and assign values

--- Exam Preparation/Mail Delivery/app.js
+++ Exam Preparation/Mail Delivery/app.js
@@ -1,4 +1,53 @@
 function solve() {
 
+    let recepientElement = document.getElementById('recipientName');
+    let titleElement = document.getElementById('title')
+    let messageElement = document.getElementById('message')
+
+    let addToListButton = document.getElementById('add')
+    let resetButton = document.getElementById('reset')
+
+
+    addToListButton.addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        let name = recepientElement.value;
+        let title = titleElement.value;
+        let message = messageElement.value;
+
+        if(!name || !title || !message ){
+            return
+        }
+
+        let liElement = document.createElement('li');
+        let titleH4 = document.createElement('h4');
+        let nameH4 = document.createElement('h4');
+        let spanText = document.createElement('span');
+
+        let buttonList = document.createElement('div');
+        buttonList.id = 'list-action'
+        
+        let btnSend = document.createElement('button')
+        btnSend.id = 'send'
+        btnSend.textContent = 'Send'
+        buttonList.appendChild(btnSend)
+
+
+        let btnDelete = document.createElement('button')
+        btnDelete.id = 'delete'
+        btnDelete.textContent = 'Delete'
+        buttonList.appendChild(btnDelete)
+
+        titleH4.value = title
+        nameH4.value = name
+        spanText.value = message
+
+
+
+
+    })
+
+
+
 }
 solve()
