7f93f9470c5d106dfc4911df11ca77de811813bc
Valentin <valio990210@gmail.com>
2023-01-25 19:00:55 +0200

    get and create elements

--- Exam Preparation/Music Site/Resources/app.js
+++ Exam Preparation/Music Site/Resources/app.js
@@ -1,5 +1,34 @@
 window.addEventListener('load', solve);
 
 function solve() {
-    //TODO
+    
+    let genreElement = document.getElementById('genre');
+    let songNameElement = document.getElementById('name');
+    let authorElement = document.getElementById('author');
+    let dateElement = document.getElementById('date');
+
+    let allHitContainer = document.querySelector('.all-hits-container')
+
+    let addBtnElement = document.getElementById('add-btn');
+
+    addBtnElement.addEventListener('click', (e)=>{
+        e.preventDefault()
+
+    if(!genreElement.value || !songNameElement.value || !authorElement.value || !dateElement.value){
+        return;
+    }
+
+    let genreCellElement = document.createElement('h2');
+    let nameCellElement = document.createElement('h2');
+    let authorCellElement = document.createElement('h2');
+    let dateCellElement = document.createElement('h3');
+
+
+
+
+    })
+
+
+
+
 }
