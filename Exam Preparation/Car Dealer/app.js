b7ec57ea663d1d55308318ca0db816c7bbf02332
Valentin <valio990210@gmail.com>
2023-01-25 09:34:20 +0200

    values and ifs

--- Exam Preparation/Car Dealer/app.js
+++ Exam Preparation/Car Dealer/app.js
@@ -1,5 +1,35 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  //TODO ....
+
+  let carMakeElement = document.getElementById('make');
+  let carModelElement = document.getElementById('model');
+  let productionYearElement = document.getElementById('year');
+  let fuelTypeElement = document.getElementById('fuel');
+  let originalPriceElement = document.getElementById('original-price');
+  let sellingPriceElement = document.getElementById('selling-price');
+
+  let publishBtnElement = document.getElementById('publish');
+
+  publishBtnElement.addEventListener('click', (e)=>{
+    e.preventDefault();
+
+    let make = carMakeElement.value;
+    let model = carModelElement.value;
+    let year = productionYearElement.value;
+    let fuel = fuelTypeElement.value;
+    let originalPrice = originalPriceElement.value;
+    let sellingPrice = sellingPriceElement.value;
+
+    if(!make  ||  !model  ||  !year  ||  !fuel   ||  !originalPrice  || !sellingPrice) {
+        return}
+
+    if(originalPrice > sellingPrice){
+      return
+    }
+      
+
+  })
+
+
 }
