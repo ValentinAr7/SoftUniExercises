7c34f99371be62f06bbf2c8677d8cf63970b0976
Valentin <valio990210@gmail.com>
2023-01-26 10:23:47 +0200

    check if plants are ripen

--- Advanced/Classes/gardenExam.js
+++ Advanced/Classes/gardenExam.js
@@ -21,6 +21,30 @@ class Garden {
     }
 
     ripenPlant(plantName, quantity){
+        if(quantity <= 0){
+            throw new Error (`The quantity cannot be zero or negative.`)
+        }
+
+        const plants = this.plants.find(p => p.plantName == plantName)
+        if(plants == undefined){
+            throw new Error (`There is no ${plantName} in the garden.`)
+        }
+
+        if(plants.ripe == true){
+            throw new Error (`The ${plantName} is already ripe.`)
+        }
+        
+        plants.ripe = true;
+        plants.quantity += quantity
+
+        if(quantity == 1){
+            return `${quantity} ${plantName} has successfully ripened.`
+        } else {
+            return `${quantity} ${plantName}s have successfully ripened`
+        }
+
+
+
 
     }
 
@@ -34,7 +58,12 @@ class Garden {
 
 }
 
+
+
 const myGarden = new Garden(250)
 console.log(myGarden.addPlant('apple', 20));
-console.log(myGarden.addPlant('orange', 200));
-console.log(myGarden.addPlant('olive', 50));
+console.log(myGarden.addPlant('orange', 100));
+console.log(myGarden.addPlant('cucumber', 30));
+console.log(myGarden.ripenPlant('apple', 10));
+console.log(myGarden.ripenPlant('orange', 1));
+console.log(myGarden.ripenPlant('orange', 4));
