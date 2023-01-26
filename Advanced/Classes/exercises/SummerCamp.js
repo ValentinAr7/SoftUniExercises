4f82aa8f7a49c09e41e9eeb901c89f6662dc46d2
Valentin <valio990210@gmail.com>
2023-01-26 11:39:40 +0200

    data

---
+++ Advanced/Classes/exercises/SummerCamp.js
@@ -0,0 +1,10 @@
+class SummerCamp {
+    constructor
+}
+
+
+const summerCamp = new SummerCamp("Jane Austen", "Pancharevo Sofia 1137, Bulgaria");
+console.log(summerCamp.registerParticipant("Petar Petarson", "student", 200));
+console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+console.log(summerCamp.registerParticipant("Petar Petarson", "student", 300));
+console.log(summerCamp.registerParticipant("Leila Wolfe", "childd", 200))
