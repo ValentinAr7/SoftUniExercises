ce9d537ad0bc56ce8017ba6c1735faaf4d2e7337
Valentin <valio990210@gmail.com>
2023-01-26 18:11:31 +0200

    return message

--- Advanced/Classes/artGalery.js
+++ Advanced/Classes/artGalery.js
@@ -11,27 +11,67 @@ class ArtGallery {
     }
 
     addArticle(articleModel, articleName, quantity) {
-        let articleModelLower = articleModel.toLowerCase()
-        if (!this.possibleArticles[articleModelLower]) {
+        let articleModel = articleModel.toLowerCase()
+        if (!this.possibleArticles[articleModel]) {
             throw new Error(`This article model is not included in this gallery!`)
         }
 
         if (this.listOfArticles.some(x => x.articleName == articleName && x.articleModel == articleModel)) {
             quantity += quantity
         } else {
-            let addObjectToArtciles = {articleModel, articleName, quantity}
-            this.listOfArticles.push(addObjectToArtciles)
+            this.listOfArticles.push({articleModel, articleName, quantity})
         }
 
         return `Successfully added article ${articleName} with a new quantity- ${quantity}.`
+    }
+
+    inviteGuest ( guestName, personality){
+        let guestFormat = {
+            guestName,
+            points: 0,
+            purchaseArticle: 0
+        }
+
+        this.guests.push(guestFormat);
+        
+        if(this.guests.some(x=> x.guestName == guestName)){
+            throw new Error (`${guestName} has already been invited.`)
+        }
+
+        if(personality == 'Vip'){
+            this.guests.points = 500
+        } else if(personality == 'Middle'){
+            this.guests.points == 250
+        } else {
+            this.guests.points == 50
+
+        }
+        return `You have successfully invited ${guestName}!`
+        
 
     }
+
+    // buyArticle ( articleModel, articleName, guestName){
+    //     if(!this.listOfArticles.some(x => x.articleName == articleName && x.articleModel == articleModel)){
+    //         throw new Error (`This article is not found.`)
+    //     }
+
+    //     if(quantity == 0){
+    //         return `This guest is not invited.`
+    //     }
+
+
+    // }
 
 
 }
 
 
 const artGallery = new ArtGallery('Curtis Mayfield');
-console.log(artGallery.addArticle('picture', 'Mona Liza', 3));
-console.log(artGallery.addArticle('Item', 'Ancient vase', 2));
-console.log(artGallery.addArticle('PICTURE', 'Mona Liza', 1));
+artGallery.addArticle('picture', 'Mona Liza', 3);
+artGallery.addArticle('Item', 'Ancient vase', 2);
+artGallery.addArticle('picture', 'Mona Liza', 1);
+artGallery.inviteGuest(' ', 'Vip');
+artGallery.inviteGuest('Peter', 'Middle');
+console.log(artGallery.buyArticle('picture', 'Mona Liza', 'John'));
+console.log(artGallery.buyArticle('item', 'Ancient vase', 'Peter'));
