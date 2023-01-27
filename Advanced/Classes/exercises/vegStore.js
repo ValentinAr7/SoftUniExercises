c2456f0992074bb4ecdf91979d12c854d5997249
Valentin <valio990210@gmail.com>
2023-01-27 10:36:23 +0200

    succesfuly added products

--- Advanced/Classes/exercises/vegStore.js
+++ Advanced/Classes/exercises/vegStore.js
@@ -9,32 +9,30 @@ class VegetableStore{
 
         vegetables.forEach(element => {
             let [type, quantity, price] = element.split(' ')
-            let currentProduct = this.availableProducts.some(x => x.type === type)
+            quantity = Number(quantity)
+            price = Number(price)
+
+            let currentProduct = this.availableProducts.find(x => x.type === type)
+
+            
             if(!currentProduct){
                 this.availableProducts.push({
                     type,
-                    quantity: Number(quantity),
-                    price: Number(price)
+                    quantity,
+                    price
                 })
             } else {
-                currentProduct.quantity += Number(quantity)
-                if(currentProduct.price > Number(price)){
+                currentProduct.quantity += quantity
+                if(currentProduct.price < price){
                     currentProduct.price == price
                 }
             }
+        });
             let buff = [];
             this.availableProducts.forEach(product => buff.push(product.type))
-            return `Successfully added ${this.availableProducts.type}`
-
-        });
-
+            return `Successfully added ${buff.join(', ')}`
     }
     
-
-
 }
-
-let vegStore = new VegetableStore("Jerrie Munro", "1463 Pette Kyosheta,Sofia");
-
-console.log(vegStore.loadingVegetables(["Okra 2.5 3.5", "Beans 10 2.8",
-"Celery 5.5 2.2", "Celery 0.5 2.5"]));
+let vegStore = new VegetableStore("Jerrie Munro", "1463 Pette Kyosheta, Sofia");
+console.log(vegStore.loadingVegetables(["Okra 2.5 3.5", "Beans 10 2.8","Celery 5.5 2.2", "Celery 0.5 2.5"]));
