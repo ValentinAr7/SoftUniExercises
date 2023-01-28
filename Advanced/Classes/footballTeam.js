3e553ef7a74ace24256ce36b15c0a0684b149cbf
Valentin <valio990210@gmail.com>
2023-01-28 09:59:35 +0200

    invite new players

--- Advanced/Classes/footballTeam.js
+++ Advanced/Classes/footballTeam.js
@@ -13,7 +13,7 @@ class footballTeam{
             
             let checkPlayerName = this.invitedPlayers.find(x => x.name == name)
 
-            if(!checkPlayer){
+            if(!checkPlayerName){
                 this.invitedPlayers.push({
                    name,
                    age,
@@ -21,10 +21,24 @@ class footballTeam{
                 })
             }
         })
+        let buff = [];
+        this.invitedPlayers.forEach(p => buff.push(p.name))
+        return `Successfully added ${buff.join(', ')}.`
+    }
+    
+
+    signContract(selectedPlayer){
+
+
+        selectedPlayer.forEach(sign =>{
+            let [name, playerOffer] = sign.split('/');
+        })
 
     }
-
 }
 
 let fTeam = new footballTeam("Barcelona", "Spain");
- console.log(fTeam.newAdditions(["Kylian Mbappé/23/160", "Lionel Messi/35/50", "Pau Torres/25/52"]));
+console.log(fTeam.newAdditions(["Kylian Mbappé/23/160", "Lionel Messi/35/50", "Pau Torres/25/52"]));
+console.log(fTeam.signContract("Lionel Messi/60"));
+console.log(fTeam.signContract("Kylian Mbappé/240"));
+console.log(fTeam.signContract("Barbukov/10"));
