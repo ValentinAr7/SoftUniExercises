7cb1fa56c6dd0ba9f101631760b38ee32d7492a0
Valentin <valio990210@gmail.com>
2023-01-28 09:26:34 +0200

    split the input

--- Advanced/Classes/footballTeam.js
+++ Advanced/Classes/footballTeam.js
@@ -1,4 +1,4 @@
-class FootballTeam(){
+class footballTeam{
     constructor(clubName, country){
         this.clubName = clubName;
         this.country = country;
@@ -6,5 +6,15 @@ class FootballTeam(){
 
     }
 
+    newAdditions(footballPlayers){
+
+        footballPlayers.forEach(player =>{
+            let [name, age, playerValue] = player.split('/')
+            
+        })
+    }
 
 }
+
+let fTeam = new footballTeam("Barcelona", "Spain");
+ console.log(fTeam.newAdditions(["Kylian Mbappé/23/160", "Lionel Messi/35/50", "Pau Torres/25/52"]));
