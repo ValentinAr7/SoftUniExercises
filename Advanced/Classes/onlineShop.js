156404da0578ec75960fa22e01e6c4e304a7dd79
Valentin <valio990210@gmail.com>
2023-01-30 08:47:20 +0200

    first task done

--- Advanced/Classes/onlineShop.js
+++ Advanced/Classes/onlineShop.js
@@ -1,13 +1,13 @@
 class OnlineShop{
     constructor(warehouseSpace){
         this.warehouseSpace = warehouseSpace;
-        this.products = products;
-        this.sales = sales;
+        this.products = [];
+        this.sales = [];
     }
 
     loadingStore(product, quantity, spaceRequired){
         
-        if(this.warehouseSpace > spaceRequired){
+        if(this.warehouseSpace < spaceRequired){
             throw new Error (`Not enough space in the warehouse.`)
         }
 
@@ -17,9 +17,14 @@ class OnlineShop{
         }
 
         this.products.push(productObj)
-        
+        this.warehouseSpace -= spaceRequired
 
+        return `The ${product} has been successfully delivered in the warehouse.`
     }
 
-
 }
+
+const myOnlineShop = new OnlineShop(500)
+console.log(myOnlineShop.loadingStore('headphones', 10, 200));
+console.log(myOnlineShop.loadingStore('laptop', 5, 200));
+console.log(myOnlineShop.loadingStore('TV', 40, 500));
