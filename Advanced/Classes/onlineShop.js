2a8b5c23978de6dfb1d9c9fe91d7b1935f6127b7
Valentin <valio990210@gmail.com>
2023-01-30 08:44:00 +0200

    throw error if space not enough

--- Advanced/Classes/onlineShop.js
+++ Advanced/Classes/onlineShop.js
@@ -3,6 +3,20 @@ class OnlineShop{
         this.warehouseSpace = warehouseSpace;
         this.products = products;
         this.sales = sales;
+    }
+
+    loadingStore(product, quantity, spaceRequired){
+        
+        if(this.warehouseSpace > spaceRequired){
+            throw new Error (`Not enough space in the warehouse.`)
+        }
+
+        let productObj = {
+            product,
+            quantity,
+        }
+
+        this.products.push(productObj)
         
 
     }
