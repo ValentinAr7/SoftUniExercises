143204afd9db5339b40725a5d4167835acdd4e91
Valentin <valio990210@gmail.com>
2023-01-30 10:25:58 +0200

    create elements

--- Exam Preparation/Hotel Reservation/01. Hotel reservation-Resources/app.js
+++ Exam Preparation/Hotel Reservation/01. Hotel reservation-Resources/app.js
@@ -8,13 +8,45 @@ function solve() {
     let checkOutElement = document.getElementById('date-out');
     let guestsNumberElement = document.getElementById('people-count')
 
+    let infoListElement = document.querySelector('.info-list')
+
+    let infoReservationElement = document.getElementById('info-reservations')
+
     let btnNextElement = document.getElementById('next-btn')
 
+    btnNextElement.addEventListener('click', (e) =>{
+        e.preventDefault()
+
+        
     let firstName = firstNameElement.value;
     let lastName= lastNameElement.value;
     let checkIn = checkInElement.value;
     let checkOut = checkOutElement.value;
     let guests = guestsNumberElement.value;
+
+    let liElement = document.createElement('li');
+
+    let articleElement = document.createElement('article');
+    let h3Element = document.createElement('h3');
+    let fromDateCell =document.createElement('p');
+    let toDateCell = document.createElement('p');
+    let forManyPeopleCell = document.createElement('p');
+
+    let btnEdit = document.createElement('button');
+    btnEdit.classList.add('edit-btn')
+    btnEdit.textContent = 'Edit'
+
+
+    let btnContinue = document.createElement('button');
+    btnContinue.classList.add('continue-btn');
+    btnContinue.textContent = 'Continue'
+
+
+
+    })
+
+
+
 
 
 }
