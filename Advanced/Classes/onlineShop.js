24db5a4c1708856f3ecdf3c17bb6b10c69c2f19b
Valentin <valio990210@gmail.com>
2023-01-30 08:57:53 +0200

    checks

--- Advanced/Classes/onlineShop.js
+++ Advanced/Classes/onlineShop.js
@@ -7,6 +7,9 @@ class OnlineShop {
 
     loadingStore(product, quantity, spaceRequired) {
 
+
+        quantity = Number(quantity)
+        spaceRequired = Number(spaceRequired)
         if (this.warehouseSpace < spaceRequired) {
             throw new Error(`Not enough space in the warehouse.`)
         }
@@ -22,9 +25,34 @@ class OnlineShop {
         return `The ${product} has been successfully delivered in the warehouse.`
     }
 
+    quantityCheck(product, minimalQuantity){
+
+        minimalQuantity = Number(minimalQuantity)
+
+        if(!this.products.find(p => p.product == product)){
+            throw new Error (`There is no ${product} in the warehouse.`)
+        }
+
+        if(minimalQuantity <= 0){
+            throw new Error (`The quantity cannot be zero or negative.`)
+        }
+
+        if(this.products.quantity >= minimalQuantity){
+            return `You have enough from product ${product}.`
+        }
+
+        this.products.quantity == minimalQuantity
+        let difference = this.products.quantity - minimalQuantity
+
+        return `You added ${difference} more from the ${product} products.`
+    }
 }
 
 const myOnlineShop = new OnlineShop(500)
 console.log(myOnlineShop.loadingStore('headphones', 10, 200));
 console.log(myOnlineShop.loadingStore('laptop', 5, 200));
-console.log(myOnlineShop.loadingStore('TV', 40, 500));
+
+console.log(myOnlineShop.quantityCheck('headphones', 10));
+console.log(myOnlineShop.quantityCheck('laptop', 10));
+console.log(myOnlineShop.quantityCheck('TV', 40,));
+
