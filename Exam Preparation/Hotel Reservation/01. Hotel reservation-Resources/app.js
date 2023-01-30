8e704c7b5c06a426337ba0d6f59e1a4d635dd00d
Valentin <valio990210@gmail.com>
2023-01-30 10:12:27 +0200

    get input data by id

--- Exam Preparation/Hotel Reservation/01. Hotel reservation-Resources/app.js
+++ Exam Preparation/Hotel Reservation/01. Hotel reservation-Resources/app.js
@@ -2,6 +2,12 @@ window.addEventListener('load', solve);
 
 function solve() {
 
+    let firstNameElement = document.getElementById('first-name');
+    let lastNameElement = document.getElementById('last-name');
+    let checkInElement = document.getElementById('date-in');
+    let checkOutElement = document.getElementById('date-out');
+    let guestsNumberElement = document.getElementById('people-count')
+
     }
 
 
