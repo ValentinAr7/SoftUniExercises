cb3960c14def202ac072d5e2db473319c537f455
Valentin <valio990210@gmail.com>
2023-01-30 10:04:42 +0200

    revision

--- Advanced/Classes/onlineShop.js
+++ Advanced/Classes/onlineShop.js
@@ -25,20 +25,20 @@ class OnlineShop {
         return `The ${product} has been successfully delivered in the warehouse.`
     }
 
-    quantityCheck(product, minimalQuantity){
+    quantityCheck(product, minimalQuantity) {
 
         minimalQuantity = Number(minimalQuantity)
 
         let foundProduct = this.products.find(p => p.product === product)
-        if(!foundProduct){
-            throw new Error (`There is no ${product} in the warehouse.`)
+        if (!foundProduct) {
+            throw new Error(`There is no ${product} in the warehouse.`)
         }
 
-        if(minimalQuantity <= 0){
-            throw new Error (`The quantity cannot be zero or negative.`)
+        if (minimalQuantity <= 0) {
+            throw new Error(`The quantity cannot be zero or negative.`)
         }
 
-        if(foundProduct.quantity >= minimalQuantity){
+        if (foundProduct.quantity >= minimalQuantity) {
             return `You have enough from product ${product}.`
         }
 
@@ -50,21 +50,38 @@ class OnlineShop {
 
     sellProduct(product) {
         const haveProduct = this.products.find(s => s.product === product)
-        if(!haveProduct){
-            throw new Error (`There is no ${product} in the warehouse.`)
+        if (!haveProduct) {
+            throw new Error(`There is no ${product} in the warehouse.`)
         }
 
-        let salesObj = {
+        let salesObj = this.sales.find(s => s.product === product) || {
             product,
-            sales: 1
+            sales: 0
         }
-        this.products.quantity--
+        haveProduct.quantity--
+        salesObj.sales++
         this.sales.push(salesObj)
 
         return `The ${product} has been successfully sold.`
     }
 
-}
+    revision() {
+        if (this.sales.length === 0) {
+            throw new Error(`There are no sales today!`)
+        }
+
+        let output = `You sold ${this.sales.length} products today!\nProducts in the warehouse:\n`;
+        for (let product of this.products) {
+          let sales = this.sales.find(s => s.product === product.product);
+          let quantityLeft = product.quantity - (sales ? sales.sales : 0);
+          output += `${product.product} - ${quantityLeft} more left\n`;
+        }
+        
+        return output;
+      }
+    }
+
+
 
 const myOnlineShop = new OnlineShop(500)
 console.log(myOnlineShop.loadingStore('headphones', 10, 200));
@@ -75,6 +92,6 @@ console.log(myOnlineShop.quantityCheck('laptop', 10));
 
 console.log(myOnlineShop.sellProduct('headphones'));
 console.log(myOnlineShop.sellProduct('laptop'));
-console.log(myOnlineShop.sellProduct('keyboard'));
+console.log(myOnlineShop.revision());
 
 
