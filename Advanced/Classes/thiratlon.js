c01fc1a152d5ab31c651378414734f7af3d764ac
Valentin <valio990210@gmail.com>
2023-02-01 11:44:22 +0200

    input 2 and completeness method

--- Advanced/Classes/thiratlon.js
+++ Advanced/Classes/thiratlon.js
@@ -13,15 +13,22 @@ class Triathlon {
 
             this.participants[participantName] = participantGender;
             //The line of code assigns a value (participantGender) to a property (participantName) of an object (this.participants). So, in this line, the value participantGender is being assigned to a property with the name participantName within the participants object. 
-            
+
             return `A new participant has been added - ${participantName}`;
           } else {
             return `${participantName} has already been added to the list`;
           }
         }
 
+        completeness (participantName, condition){
+            
+        }
+
 }
 const contest = new Triathlon("Dynamos");
 console.log(contest.addParticipant("Peter", "male"));
 console.log(contest.addParticipant("Sasha", "female"));
-console.log(contest.addParticipant("Peter", "male"));
+console.log(contest.addParticipant("George", "male"));
+console.log(contest.completeness("Peter", 100));
+console.log(contest.completeness("Sasha", 70));
+console.log(contest.completeness("George", 20));
