c420a80f6ca04cc9dba852843d84599b9ad63f0e
Valentin <valio990210@gmail.com>
2023-02-01 14:04:30 +0200

    check prices

--- Advanced/Classes/christmasDiner.js
+++ Advanced/Classes/christmasDiner.js
@@ -1,14 +1,57 @@
 class ChristmasDinner {
 
-    constructor(budget, dishes, products, guests){
+    constructor(budget){
         this.budget = budget
-        this.dishes = dishes
-        this.products = products
-        this.guests = guests
+        this.dishes = []
+        this.products = []
+        this.guests = {}
 
         if(this.budget < 0){
             throw new Error (`The budget cannot be a negative number`)
         }
+    }
+
+    shopping([product, price]){
+        if(this.budget < price){
+            throw new Error (`Not enough money to buy this product`)
+        } else {
+            this.products.push(product)
+            this.budget -= price
+            return `You have successfully bought ${product}!`
+        }
+
 
     }
+
+
 }
+
+let dinner = new ChristmasDinner(300);
+
+dinner.shopping(['Salt', 1]);
+dinner.shopping(['Beans', 3]);
+dinner.shopping(['Cabbage', 4]);
+dinner.shopping(['Rice', 2]);
+dinner.shopping(['Savory', 1]);
+dinner.shopping(['Peppers', 1]);
+dinner.shopping(['Fruits', 40]);
+dinner.shopping(['Honey', 10]);
+
+// dinner.recipes({
+//     recipeName: 'Oshav',
+//     productsList: ['Fruits', 'Honey']
+// });
+// dinner.recipes({
+//     recipeName: 'Folded cabbage leaves filled with rice',
+//     productsList: ['Cabbage', 'Rice', 'Salt', 'Savory']
+// });
+// dinner.recipes({
+//     recipeName: 'Peppers filled with beans',
+//     productsList: ['Beans', 'Peppers', 'Salt']
+// });
+
+// dinner.inviteGuests('Ivan', 'Oshav');
+// dinner.inviteGuests('Petar', 'Folded cabbage leaves filled with rice');
+// dinner.inviteGuests('Georgi', 'Peppers filled with beans');
+
+// console.log(dinner.showAttendance());
