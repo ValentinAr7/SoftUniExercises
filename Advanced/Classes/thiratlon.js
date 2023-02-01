d348cf4809a2fd3886d3f6113c25735991835f5b
Valentin <valio990210@gmail.com>
2023-02-01 11:42:47 +0200

    add new partcipant to the list

--- Advanced/Classes/thiratlon.js
+++ Advanced/Classes/thiratlon.js
@@ -9,16 +9,16 @@ class Triathlon {
 
     addParticipant (participantName, participantGender){
 
-        const existParticipant = this.participants.find(p => p.participantName = participantName)
-
-        let participant = {
-            participantName: participantGender
+        if (this.participants[participantName] === undefined) {
+            this.participants[participantName] = participantGender;
+            return `A new participant has been added - ${participantName}`;
+          } else {
+            return `${participantName} has already been added to the list`;
+          }
         }
-
-        if(!existParticipant){
-            this.participants.push(participant)
-            return `A new participant has been added - ${participantName}`
-        }
-    }
 
 }
+const contest = new Triathlon("Dynamos");
+console.log(contest.addParticipant("Peter", "male"));
+console.log(contest.addParticipant("Sasha", "female"));
+console.log(contest.addParticipant("Peter", "male"));
