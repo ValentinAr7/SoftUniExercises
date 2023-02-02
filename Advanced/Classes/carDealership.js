434d4370db3f26cb548d5c6382604dd3c41ce8e0
Valentin <valio990210@gmail.com>
2023-02-02 18:05:25 +0200

    constructor data

--- Advanced/Classes/carDealership.js
+++ Advanced/Classes/carDealership.js
@@ -1,6 +1,9 @@
 class CarDealership{
     constructor(name){
-        
+       this.name = name;
+       this.availableCars = [];
+       this.soldCars = [];
+       this.totalIncome = 0 
     }
 
 }
