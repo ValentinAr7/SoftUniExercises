aab420ac49338ccdf01720edcd3cdd871c72cb2e
Valentin <valio990210@gmail.com>
2023-02-02 18:09:34 +0200

    check for valid input

--- Advanced/Classes/carDealership.js
+++ Advanced/Classes/carDealership.js
@@ -7,7 +7,18 @@ class CarDealership{
     }
 
     addCar (model, horsepower, price, mileage){
-        
+
+        let horsepower = Number(horsepower)
+        let price = Number(price);
+        let mileage = Number(mileage)
+        if(model == '' || horsepower < 0 || price < 0 || mileage < 0){
+            throw new Error (`Invalid input!`)
+        }
     }
 
 }
+
+let dealership = new CarDealership('SoftAuto');
+console.log(dealership.addCar('Toyota Corolla', 100, 3500, 190000));
+console.log(dealership.addCar('Mercedes C63', 300, 29000, 187000));
+console.log(dealership.addCar('', 120, 4900, 240000));
