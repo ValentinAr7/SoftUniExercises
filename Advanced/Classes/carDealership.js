2b7671897a54d4abcbfe2b7bb71ffb8a858767a0
Valentin <valio990210@gmail.com>
2023-02-02 18:29:47 +0200

    sell cars method

--- Advanced/Classes/carDealership.js
+++ Advanced/Classes/carDealership.js
@@ -22,13 +22,18 @@ class CarDealership{
             mileage
         }
         this.availableCars.push(newCar)
-        return `"New car added: ${model} - ${horsepower} HP - ${(mileage).toFixed(2)} km - ${(price).toFixed(2)}$`
+        return `New car added: ${model} - ${horsepower} HP - ${(mileage).toFixed(2)} km - ${(price).toFixed(2)}$`
+    }
+
+    sellCar (model, desiredMileage) {
 
     }
 
 }
 
 let dealership = new CarDealership('SoftAuto');
-console.log(dealership.addCar('Toyota Corolla', 100, 3500, 190000));
-console.log(dealership.addCar('Mercedes C63', 300, 29000, 187000));
-console.log(dealership.addCar('', 120, 4900, 240000));
+dealership.addCar('Toyota Corolla', 100, 3500, 190000);
+dealership.addCar('Mercedes C63', 300, 29000, 187000);
+dealership.addCar('Audi A3', 120, 4900, 240000);
+console.log(dealership.sellCar('Toyota Corolla', 230000));
+console.log(dealership.sellCar('Mercedes C63', 110000));
