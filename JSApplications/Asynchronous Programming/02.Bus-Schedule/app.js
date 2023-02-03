b9cf3a9fd19202b30c4b6a4852f669cea77a1097
Valentin <valio990210@gmail.com>
2023-02-03 15:32:17 +0200

    get URL

--- JSApplications/Asynchronous Programming/02.Bus-Schedule/app.js
+++ JSApplications/Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,7 +1,16 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    async function depart() {
+        let infoElement = document.getElementById('info');
+        let btnDepartElement=document.getElementById('depart');
+        let btnArriveElement=document.getElementById('arrive');
+
+        btnDepartElement.addEventListener(click, (e) => {
+
+            let url = `http://localhost:3030/jsonstore/bus/schedule/${stopId}`
+
+            
+        })
     }
 
     function arrive() {
