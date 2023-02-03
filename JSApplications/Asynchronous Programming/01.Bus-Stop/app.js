f26bbb6689c206f78a79c1b9fbef8144f93d8c22
Valentin <valio990210@gmail.com>
2023-02-03 11:05:21 +0200

    get elements

--- JSApplications/Asynchronous Programming/01.Bus-Stop/app.js
+++ JSApplications/Asynchronous Programming/01.Bus-Stop/app.js
@@ -2,6 +2,11 @@ async function getInfo() {
 
     const stopInfoElement = document.getElementById('stopId');
     const stopId = stopInfoElement.value
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`
+    const stopNameElement = document.getElementById('stopName');
+    const busList = document.getElementById('buses');
+
+    busList.innerHTML = ''
 
 
 }
