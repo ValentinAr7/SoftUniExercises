340d64c471b5485fc8875d3ebfda583820ae15e0
Valentin <valio990210@gmail.com>
2023-02-03 15:33:43 +0200

    fetch data

--- JSApplications/Asynchronous Programming/02.Bus-Schedule/app.js
+++ JSApplications/Asynchronous Programming/02.Bus-Schedule/app.js
@@ -5,11 +5,17 @@ function solve() {
         let btnDepartElement=document.getElementById('depart');
         let btnArriveElement=document.getElementById('arrive');
 
+        let url = `http://localhost:3030/jsonstore/bus/schedule/${stopId}`
+
+
+        let response = await fetch(url);
+        let data = response.json()
+
         btnDepartElement.addEventListener(click, (e) => {
 
-            let url = `http://localhost:3030/jsonstore/bus/schedule/${stopId}`
 
             
+
         })
     }
 
