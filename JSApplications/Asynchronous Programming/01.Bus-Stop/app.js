4fa091a8ab9fea9f42a8722b34d5507ff83a6ce2
Valentin <valio990210@gmail.com>
2023-02-03 11:14:25 +0200

    comments

--- JSApplications/Asynchronous Programming/01.Bus-Stop/app.js
+++ JSApplications/Asynchronous Programming/01.Bus-Stop/app.js
@@ -1,15 +1,20 @@
 async function getInfo() {
-    const stopInfoElement = document.getElementById('stopId');
-    const stopId = stopInfoElement.value
-    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`
-    const stopNameElement = document.getElementById('stopName');
-    const busList = document.getElementById('buses');
-    busList.innerHTML = ''
-    const response = await fetch(url);
-    const data = await response.json();
+    const stopInfoElement = document.getElementById('stopId');  //get the element by ID
+    const stopId = stopInfoElement.value                        //store the value of the stopInfoElement
+    const url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`     //url address of stopIds
+    const stopNameElement = document.getElementById('stopName');            //get stopName element
+    const busList = document.getElementById('buses');                       //get busList elements
+   
+    busList.innerHTML = ''                  //reset the input value
 
-    stopNameElement.textContent = data.name;
-    Object.entries(data.buses).forEach((busNumber, timeArrive) => {
+    //Make a network request to the specified url. 
+    //The response from the server is being awaited and converted to JSON format using the .json().
+    const response = await fetch(url);          
+    const data = await response.json();       
+    
+    
+    stopNameElement.textContent = data.name;    
+    Object.entries(data.buses).forEach((busNumber, timeArrive) => {     //for each element print
         const li = document.createElement('li');
         li.textContent = `Bus ${busNumber} arrives in ${timeArrive} minutes`
         busList.appendChild(li)
