07861070ea4ba369de2d9ca0431f08686a0bf724
Valentin <valio990210@gmail.com>
2023-02-04 09:54:43 +0200

    get title and posts elements

--- JSApplications/Asynchronous Programming/06.Blog/app.js
+++ JSApplications/Asynchronous Programming/06.Blog/app.js
@@ -1,19 +1,27 @@
 function attachEvents() {
 document.getElementById('btnLoadPosts').addEventListener('click', getPosts)
+document.getElementById('btnViewPost').addEventListener('click', getComments)
 }
 
 async function getPosts(){
     const selectOp = document.getElementById('posts');
-    const url = `http://localhost:3030/jsonstore/blog/posts`
+    const url = "http://localhost:3030/jsonstore/blog/posts"
 
     const response = await fetch(url)
-    const data = response.json();
+    const data = await response.json();
 
     Object.values(data).forEach(post => {
         const op = document.createElement('option');
         op.value = post.id;
         op.textContent = post.title;
+        selectOp.appendChild(op)
     })
+
+    async function getComments(){
+        const postsUrl = 'http://localhost:3030/jsonstore/blog/posts'
+        const titleElement = 'http://localhost:3030/jsonstore/blog/comments'
+        
+    }
 }
 
 attachEvents();
