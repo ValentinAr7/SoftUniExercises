449d088c7ba14a8e3253c3758c2589f2810b0e7b
Valentin <valio990210@gmail.com>
2023-02-06 18:21:29 +0200

    words calculator

--- Code Wars/wordsCalculator.js
+++ Code Wars/wordsCalculator.js
@@ -1,12 +1,13 @@
-function high(x){
-    let score = 0;
-
-    for (let i = 0; i < x.length; i++) {
-      score += x.charCodeAt(i) - 96;
+function high(x) {
+    let words = x.split(" ");
+    let scores = [];
+    for (let i = 0; i < words.length; i++) {
+      let score = 0;
+      for (let j = 0; j < words[i].length; j++) {
+        score += words[i].charCodeAt(j) - 96;
+      }
+      scores.push(score);
     }
-
-
-    return score;
+    let maxScore = Math.max(...scores);
+    return words[scores.indexOf(maxScore)];
   }
-
-  high('man i need a taxi up to ubud')
