f37b95ef85523975e4831378bcd1be5b300322be
Valentin <valio990210@gmail.com>
2023-02-07 15:44:45 +0200

    post method

--- JSApplications/Remote Data and Authentication/app.js
+++ JSApplications/Remote Data and Authentication/app.js
@@ -5,4 +5,12 @@ async function onLogin(event){
 
     const formData = new FormData(event.target);
     const {email, password} = Object.fromEntries(formData.entries());
+
+    const response = await fetch('http://localhost:3030/users/login', {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({email, password})
+    })
 }
