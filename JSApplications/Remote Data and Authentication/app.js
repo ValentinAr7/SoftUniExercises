b7a6dc5478c52ee41bc83b903d978d424d824d22
Valentin <valio990210@gmail.com>
2023-02-07 15:42:57 +0200

    new formData

--- JSApplications/Remote Data and Authentication/app.js
+++ JSApplications/Remote Data and Authentication/app.js
@@ -2,4 +2,7 @@ document.getElementById('login-form').addEventListener('submit', onLogin);
 
 async function onLogin(event){
     event.preventDefault()
+
+    const formData = new FormData(event.target);
+    const {email, password} = Object.fromEntries(formData.entries());
 }
