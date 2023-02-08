2df794dff86b52965d6acf286fefdb7f5121dd43
Valentin <valio990210@gmail.com>
2023-02-08 12:15:22 +0200

    fetch data

--- JSApplications/Applications/01.Messenger/app.js
+++ JSApplications/Applications/01.Messenger/app.js
@@ -1,5 +1,13 @@
 function attachEvents() {
-    console.log('TODO...');
+
+    async function getAllMsg(){
+        const url = 'http://localhost:3030/jsonstore/messenger'
+        const response = await fetch(url)
+        const data = await response.json()
+
+        return data
+    }
+
 }
 
 attachEvents();
