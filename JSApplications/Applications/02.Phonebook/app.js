1596e91a3bd814ff46cc77aeb4326d7a692198d8
Valentin <valio990210@gmail.com>
2023-02-08 17:57:37 +0200

    handle newly created records

--- JSApplications/Applications/02.Phonebook/app.js
+++ JSApplications/Applications/02.Phonebook/app.js
@@ -1,22 +1,36 @@
 function attachEvents() {
 
     document.getElementById('btnLoad').addEventListener('click', onLoadAllRecords)
+    document.getElementById('btnCreate').addEventListener('click', handleCreateRec)
 
 }
 
+function handleCreateRec(){
+    const person = document.getElementById('person')
+    const phone = document.getElementById('phone')
+
+}
 function renderRecords(data) {
     const ul = document.getElementById('phonebook')
-
-
     Object.values(data).forEach(rec => {
-        const btnDelete = document.createElement('button')
-        btnDelete.textContent = 'Delete'
         const li = document.createElement('li');
         li.textContent = `${rec.person}: ${rec.phone}`
         li.setAttribute('data-id', rec._id)
-        ul.appendChild(btnDelete)
+
+        const btnDelete = document.createElement('button')
+        btnDelete.addEventListener('click', handleDelete)
+        btnDelete.textContent = 'Delete'
+        li.appendChild(btnDelete)
         ul.appendChild(li)
     })
+}
+//--------------------------------------------------------------------------
+function handleDelete(e) {
+    const li = e.target.parentElement;
+    const id = li.getAttribute('data-id')
+
+    onDeleteRecords(id);
+    li.remove()
 }
 
 
