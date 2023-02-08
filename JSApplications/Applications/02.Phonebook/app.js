f37dd5b4e5a613bf7456c0eb6002c650c8733eb6
Valentin <valio990210@gmail.com>
2023-02-08 18:24:39 +0200

    comments

--- JSApplications/Applications/02.Phonebook/app.js
+++ JSApplications/Applications/02.Phonebook/app.js
@@ -2,7 +2,6 @@ function attachEvents() {
 
     document.getElementById('btnLoad').addEventListener('click', onLoadAllRecords)
     document.getElementById('btnCreate').addEventListener('click', handleCreateRec)
-
 }
 
 //----------------------------------------------------------------------------
@@ -23,7 +22,7 @@ function renderRecords(data) {
     Object.values(data).forEach(rec => {
         const li = document.createElement('li');
         li.textContent = `${rec.person}: ${rec.phone}`
-        li.setAttribute('data-id', rec._id)
+        li.setAttribute('data-id', rec._id)     //set atribute to get the id of each phone number so it can be delete afterwards
 
         const btnDelete = document.createElement('button')
         btnDelete.addEventListener('click', handleDelete)
@@ -38,6 +37,7 @@ function handleDelete(e) {
     const id = li.getAttribute('data-id')
 
     onDeleteRecords(id);
+    //delete the whole record with matching id
     li.remove()
 }
 
