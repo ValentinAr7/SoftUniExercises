c7c895f454f846420c0396f7c85344ea6534cd5c
Valentin <valio990210@gmail.com>
2023-02-08 13:52:52 +0200

    fetch data from the server

--- JSApplications/Applications/02.Phonebook/app.js
+++ JSApplications/Applications/02.Phonebook/app.js
@@ -1,5 +1,16 @@
 function attachEvents() {
-    console.log('TODO...');
+
 }
+
+async function loadPhones() {
+
+    const url = 'http://localhost:3030/jsonstore/phonebook'
+    const response = await fetch(url);
+    const data = await response.json()
+
+    return data
+}
+
+
 
 attachEvents();
