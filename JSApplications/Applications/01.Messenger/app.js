cc5a3abe2f8336d17c9ebc30e4a6dc942ec338f2
Valentin <valio990210@gmail.com>
2023-02-08 12:30:36 +0200

    post method

--- JSApplications/Applications/01.Messenger/app.js
+++ JSApplications/Applications/01.Messenger/app.js
@@ -1,12 +1,26 @@
 function attachEvents() {
 
-    async function getAllMsg(){
-        const url = 'http://localhost:3030/jsonstore/messenger'
-        const response = await fetch(url)
-        const data = await response.json()
+}
 
-        return data
-    }
+
+async function getAllMsg(){
+    const url = 'http://localhost:3030/jsonstore/messenger'
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+}
+
+async function createMessage(){
+    const url = 'http://localhost:3030/jsonstore/messenger'
+    const response = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(body)
+    })
+
 
 }
 
