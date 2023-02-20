70c6fcf8752685df8b713420cc8ed7d916b82da2
ValentinAr7 <104101252+ValentinAr7@users.noreply.github.com>
2023-02-20 11:08:39 +0200

    .

--- Routing/src/app.js
+++ Routing/src/app.js
@@ -1,32 +1,34 @@
 const views = {
-    '/': ()=> '<h2>Home Page</h2>',
-    '/catalog': ()=> '<h2>Catalog</h2>',
-    '/about': ()=> '<h2>About Page</h2>'
+    '/': () => '<h2>Home Page</h2>',
+    '/catalog': () => '<h2>Catalog</h2>',
+    '/about': () => '<h2>About Page</h2>'
 }
 
 const main = document.querySelector('main');
 document.querySelector('nav').addEventListener('click', onNavigate);
-showView('/')
 
-function onNavigate(event){
-    if(event.target.tagName == 'A'){
+const startingView = window.location.pathname
+showView(startingView)
+window.addEventListener
+
+function onNavigate(event) {
+    if (event.target.tagName == 'A') {
         const name = new URL(event.target.href);
         const view = views[url.pathname];
-        if(typeof view == 'function'){
+        if (typeof view == 'function') {
             event.preventDefault()
             main.innerHTML = view()
-        history.pushState(null, '', url.pathname)
+            history.pushState(null, '', url.pathname)
         }
     }
 }
 
-function showView(){
+function showView() {
     const view = views[name];
-    if(typeof view == 'function'){
+    if (typeof view == 'function') {
         main.innerHTML = view();
         return true
     } else {
-
+        return false
     }
-    main.innerHTML = view();
 }
